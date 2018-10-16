ed566b54b641ab62023f9dd9a9fca8895611e611
tutok <walas.marcin@gmail.com>
2018-10-17 00:33:11 +0200

    feat(adieu-razzle): set clearConsole to true

--- examples/shop-with-blog/client/falcon-client.build.config.js
+++ examples/shop-with-blog/client/falcon-client.build.config.js
@@ -1,7 +1,7 @@
 const { razzlePluginFalconClient } = require('@deity/falcon-client');
 
 module.exports = {
-  clearConsole: false,
+  clearConsole: true,
   i18n: {
     resourcePackages: ['@deity/falcon-i18n'],
     filter: { lng: ['en'] }
