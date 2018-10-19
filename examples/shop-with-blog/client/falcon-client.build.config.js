b0d0f94a683c1684cb65bf2f74813f188ee27407
tutok <walas.marcin@gmail.com>
2018-10-19 19:26:14 +0200

    feat(adieu-razzle): removed falconClientPlugin

--- examples/shop-with-blog/client/falcon-client.build.config.js
+++ examples/shop-with-blog/client/falcon-client.build.config.js
@@ -1,10 +1,7 @@
-const { razzlePluginFalconClient } = require('@deity/falcon-client');
-
 module.exports = {
   clearConsole: true,
   i18n: {
     resourcePackages: ['@deity/falcon-i18n'],
     filter: { lng: ['en'] }
-  },
-  plugins: [razzlePluginFalconClient()]
+  }
 };
