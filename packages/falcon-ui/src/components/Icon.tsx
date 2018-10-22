c2f5fd62c7db5734fe8013c723337edd15a7a9ed
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-10-22 15:49:30 +0200

    docs(falcon-ui): add icon docs, rework icon component implementation

--- packages/falcon-ui/src/components/Icon.tsx
+++ packages/falcon-ui/src/components/Icon.tsx
@@ -2,25 +2,9 @@ import React from 'react';
 import { withCSSContext } from '@emotion/core';
 import { themed, PropsWithTheme, BaseProps, ThemedComponentProps } from '../theme';
 
-const IconRenderer = themed({
-  tag: 'svg',
-
-  defaultProps: {
-    // https://stackoverflow.com/questions/18646111/disable-onfocus-event-for-svg-element
-    focusable: 'false'
-  },
-
-  defaultTheme: {
-    icon: {
-      size: 'lg',
-      stroke: 'primary'
-    }
-  }
-});
-
 type IconProps = { src: string; fallback?: React.ReactNode } & ThemedComponentProps & BaseProps<'svg'>;
 
-export const Icon = withCSSContext((props: IconProps, context: PropsWithTheme) => {
+const IconRenderer = withCSSContext((props: IconProps, context: PropsWithTheme) => {
   if (!context.theme || !context.theme.icons) return null;
 
   const { icons } = context.theme;
@@ -30,7 +14,25 @@ export const Icon = withCSSContext((props: IconProps, context: PropsWithTheme) =
     return fallback || null;
   }
 
-  const { icon, ...otherProps } = icons[src];
+  const { icon: SVGICon, ...otherProps } = icons[src];
 
-  return <IconRenderer as={icon} {...otherProps as any} {...rest} />;
+  return <SVGICon {...otherProps} {...rest} />;
 }) as (props: IconProps) => JSX.Element;
+
+export const Icon = themed({
+  tag: IconRenderer,
+
+  defaultProps: {
+    // https://stackoverflow.com/questions/18646111/disable-onfocus-event-for-svg-element
+    focusable: 'false',
+    src: '',
+    fallback: undefined as React.ReactNode | undefined
+  },
+
+  defaultTheme: {
+    icon: {
+      size: 'lg',
+      stroke: 'primary'
+    }
+  }
+});
