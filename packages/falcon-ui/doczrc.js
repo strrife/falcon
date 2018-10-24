d22cbe6fe9973dd71d3f3b871e94c9ad1d6b0f69
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-10-24 13:51:01 +0200

    docs(falcon-ui): finals tweaks for components docs, update docz config

--- packages/falcon-ui/doczrc.js
+++ packages/falcon-ui/doczrc.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
 import doczPluginNetlify from 'docz-plugin-netlify';
+import * as path from 'path';
 
 module.exports = {
   typescript: true,
@@ -7,15 +8,37 @@ module.exports = {
   src: './src',
   wrapper: 'docs/Wrapper',
   title: 'Falcon UI',
+  description: 'Library of composable, themable, design-system-driven UI components for React',
+  repository: 'https://github.com/deity-io/falcon/tree/master/packages/falcon-ui',
   themeConfig: {
-    colors: {
-      background: '#fafafa'
-    },
     styles: {
-      playground: {
-        background: '#fff'
+      h1: {
+        fontSize: 48,
+        fontWeight: 300,
+        margin: '32px 0',
+        letterSpacing: 0,
+        '::before': {
+          display: 'none'
+        }
+      },
+      h2: {
+        fontSize: 39,
+        fontWeight: 400,
+        letterSpacing: 0,
+        '::before': {
+          display: 'none'
+        }
       }
     }
+  },
+
+  modifyBundlerConfig: config => {
+    config.resolve.alias = Object.assign({}, config.resolve.alias, {
+      '@deity/falcon-ui': path.resolve(__dirname, 'src'),
+      docs: path.resolve(__dirname, 'docs')
+    });
+
+    return config;
   },
   plugins: [doczPluginNetlify()]
 };
