d3e2989295592b3f4fe991cdfc1c639a508175c5
tutok <walas.marcin@gmail.com>
2018-10-25 15:22:04 +0200

    feat(adieu-razzle): removed cross env and set default env variables in tests

--- packages/falcon-client/build-utils/jest/index.js
+++ packages/falcon-client/build-utils/jest/index.js
@@ -1,13 +1,13 @@
 const jest = require('jest');
 
 module.exports = () => {
-  process.env.BABEL_ENV = 'test';
-  process.env.NODE_ENV = 'test';
-  process.env.PORT = 3000;
-  process.env.PUBLIC_PATH = '/';
-  process.env.BUILD_TARGET = '';
-  process.env.ASSETS_MANIFEST = 'assets/manifest.json';
-  process.env.PUBLIC_DIR = 'public/dir';
+  process.env.BABEL_ENV = process.env.BABEL_ENV || 'test';
+  process.env.NODE_ENV = process.env.NODE_ENV || 'test';
+  process.env.PORT = process.env.PORT || 3000;
+  process.env.PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
+  process.env.BUILD_TARGET = process.env.BUILD_TARGET || '';
+  process.env.ASSETS_MANIFEST = process.env.ASSETS_MANIFEST || 'assets/manifest.json';
+  process.env.PUBLIC_DIR = process.env.PUBLIC_DIR || 'public/dir';
 
   const paths = require('./../paths');
   const createConfig = require('./config/create');
