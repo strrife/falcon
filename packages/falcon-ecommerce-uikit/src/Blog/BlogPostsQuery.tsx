13a370aed6dc5db87634f05a0c3111da475209f5
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-10-30 15:19:50 +0100

    feat(blog): add blog posts query component

---
+++ packages/falcon-ecommerce-uikit/src/Blog/BlogPostsQuery.tsx
@@ -0,0 +1,69 @@
+import gql from 'graphql-tag';
+import { Query } from '../Query/Query';
+
+const GET_BLOG_POSTS = gql`
+  query BlogPosts {
+    blogPosts {
+      items {
+        title
+        date
+        slug
+        excerpt
+        image {
+          url
+          description
+        }
+      }
+      pagination {
+        currentPage
+        nextPage
+        prevPage
+        perPage
+        totalPages
+      }
+    }
+  }
+`;
+
+export type BlogPostExcerptType = {
+  title: string;
+  date: string;
+  slug: string;
+  excerpt: string;
+  image?: {
+    url: string;
+    description: string;
+  };
+};
+
+export type BlogPagination = {
+  currentPage: number;
+  nextPage?: number;
+  prevPage?: number;
+  perPage: number;
+  totalPages: number;
+};
+
+export type BlogPosts = {
+  blogPosts: {
+    items: BlogPostExcerptType;
+    pagination: BlogPagination;
+  };
+};
+
+function getTranslations(t: reactI18Next.TranslationFunction) {
+  return {
+    title: t('blog.title'),
+    readMore: t('blog.readMore')
+  };
+}
+
+export type BlogPostsTranslations = ReturnType<typeof getTranslations>;
+
+export class BlogPostsQuery extends Query<BlogPosts, {}, BlogPostsTranslations> {
+  static defaultProps = {
+    query: GET_BLOG_POSTS,
+    getTranslations,
+    translationsNamespaces: ['blog']
+  };
+}
