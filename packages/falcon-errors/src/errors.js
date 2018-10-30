42d141c2800831d02d27748ff510c8c2f5e2a810
tutok <walas.marcin@gmail.com>
2018-10-30 14:08:05 +0100

    feat(signin): added AuthenticationError into errors

--- packages/falcon-errors/src/errors.js
+++ packages/falcon-errors/src/errors.js
@@ -1,5 +1,7 @@
-const { ApolloError } = require('apollo-server-errors');
+const { ApolloError, AuthenticationError } = require('apollo-server-errors');
 const codes = require('./codes');
+
+module.exports.AuthenticationError = AuthenticationError;
 
 module.exports.EntityNotFoundError = class EntityNotFoundError extends ApolloError {
   constructor(message = 'Entity not found') {
