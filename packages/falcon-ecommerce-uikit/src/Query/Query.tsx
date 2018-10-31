62526477402702418c6ce4b385d593b0b32d25e7
tutok <walas.marcin@gmail.com>
2018-10-31 15:59:12 +0100

    feat(signin): added TODO

--- packages/falcon-ecommerce-uikit/src/Query/Query.tsx
+++ packages/falcon-ecommerce-uikit/src/Query/Query.tsx
@@ -37,6 +37,7 @@ export class Query<TData = any, TVariables = OperationVariables, TTranslations =
           if (error) {
             const errorCode = this.getErrorCode(error);
 
+            // TODO: check errorPolicy and if === 'all' then pass thru render props all extracted/formated errors with errorcodes instead of inline error message
             return (
               <p>
                 {`Error!: ${errorCode}`}
