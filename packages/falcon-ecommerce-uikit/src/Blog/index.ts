8bd2f5993b691103d98b351fc2ba6b301015fcb8
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-02 16:26:36 +0100

    feat(blog): add CmsContent component for displaying html delivered as string

--- packages/falcon-ecommerce-uikit/src/Blog/index.ts
+++ packages/falcon-ecommerce-uikit/src/Blog/index.ts
@@ -2,3 +2,6 @@ export * from './BlogPostsQuery';
 export * from './BlogPostsLayout';
 export * from './BlogPostExcerpt';
 export * from './BlogPostsPaginator';
+export * from './BlogPostQuery';
+export * from './BlogPost';
+export * from './CmsContent';
