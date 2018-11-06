670e7e7d046f660588e65e2b4608f6ec4a06efa1
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-06 10:09:57 +0100

    feat(blog): add ability to pass custom blogurl base in blogposts paginator component

--- packages/falcon-ecommerce-uikit/src/Blog/BlogPostsPaginator.tsx
+++ packages/falcon-ecommerce-uikit/src/Blog/BlogPostsPaginator.tsx
@@ -10,20 +10,39 @@ const blogPostsPaginatorLayout: DefaultThemeProps = {
   }
 };
 
-export const BlogPostsPaginator: React.SFC<{ pagination: BlogPagination; translations: BlogPostsTranslations }> = ({
-  pagination,
-  translations
-}) => (
+type BlogPostsPaginatorProps = {
+  pagination: BlogPagination;
+  translations: BlogPostsTranslations;
+  blogUrlBase: string;
+};
+
+export const BlogPostsPaginator: React.SFC<BlogPostsPaginatorProps> = ({ pagination, translations, blogUrlBase }) => (
   <Box defaultTheme={blogPostsPaginatorLayout} justifyContent={!pagination.prevPage ? 'flex-end' : 'space-between'}>
     {pagination.prevPage && (
-      <Link display="flex" lineHeight="small" fontSize="md" as={RouterLink} to={`/blog/${pagination.prevPage}`}>
+      <Link
+        display="flex"
+        lineHeight="small"
+        fontSize="md"
+        as={RouterLink}
+        to={`${blogUrlBase}/${pagination.prevPage}`}
+      >
         <Icon size="md" mr="xs" src="prevPage" /> {translations.newerEntries}
       </Link>
     )}
     {pagination.nextPage && (
-      <Link display="flex" lineHeight="small" fontSize="md" as={RouterLink} to={`/blog/${pagination.nextPage}`}>
+      <Link
+        display="flex"
+        lineHeight="small"
+        fontSize="md"
+        as={RouterLink}
+        to={`${blogUrlBase}/${pagination.nextPage}`}
+      >
         {translations.olderEntries} <Icon ml="xs" size="md" src="nextPage" />
       </Link>
     )}
   </Box>
 );
+
+BlogPostsPaginator.defaultProps = {
+  blogUrlBase: '/blog'
+};
