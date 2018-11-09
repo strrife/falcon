a30e19e19f846339c1615cbc2bdd2dddca68182f
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-09 11:49:19 +0100

    docs(falcon-errors): add note about falcon-errors not being transpiled by babel

--- packages/falcon-errors/index.js
+++ packages/falcon-errors/index.js
@@ -1,5 +1,7 @@
 const codes = require('./src/codes');
 const errors = require('./src/errors');
+// IMPORTANT: code in falcon-errors in not transpiled by babel currently
+// so we must stick to the syntax supported by browsers we support
 
 const codesAndErrors = { codes };
 // eslint-disable-next-line
