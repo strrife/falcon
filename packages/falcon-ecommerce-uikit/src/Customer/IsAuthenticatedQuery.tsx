25e36ae66a65abc782b7d794b1a5f98facb8b72f
tutok <walas.marcin@gmail.com>
2018-11-13 16:36:19 +0100

    feat(auth-route): added is authenticated query

---
+++ packages/falcon-ecommerce-uikit/src/Customer/IsAuthenticatedQuery.tsx
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+import { Query } from '../Query/Query';
+
+export const GET_IS_AUTHENTICATED = gql`
+  query IsAuthenticated {
+    customer {
+      id
+    }
+    config @client {
+      signInUrl
+    }
+  }
+`;
+
+export type IsAuthenticatedQueryData = {
+  customer: { id: number };
+  config: { signInUrl: string };
+};
+
+export class IsAuthenticatedQuery extends Query<IsAuthenticatedQueryData> {
+  static defaultProps = {
+    query: GET_IS_AUTHENTICATED
+  };
+  static propTypes = {
+    ...Query.propTypes
+  };
+}
