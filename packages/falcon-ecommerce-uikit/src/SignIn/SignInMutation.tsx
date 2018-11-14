8c3c435b485111ce9400e47f00e303daa8ec147d
tutok <walas.marcin@gmail.com>
2018-11-14 12:34:27 +0100

    feat(signin): fixed mutation name

--- packages/falcon-ecommerce-uikit/src/SignIn/SignInMutation.tsx
+++ packages/falcon-ecommerce-uikit/src/SignIn/SignInMutation.tsx
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 
 export const SIGN_IN_MUTATION = gql`
-  mutation signIn($input: SignIn!) {
+  mutation SignIn($input: SignIn!) {
     signIn(input: $input)
   }
 `;
