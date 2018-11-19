e08c9f65a75ae7c3dd294c70ea7191196539fac0
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-19 14:54:29 +0100

    feat(shop-and-blog): add Sidebar contents component that can show proper contents depending on contentType

--- examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarContents.js
+++ examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarContents.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { MiniCartQuery, MiniCart } from '@deity/falcon-ecommerce-uikit';
+import { Box } from '@deity/falcon-ui';
+import { MiniCartQuery, MiniAccountQuery, MiniCart, MiniAccount } from '@deity/falcon-ecommerce-uikit';
 
 export default ({ contentType }) => {
   if (!contentType) return null;
-
-  return <MiniCartQuery>{data => <MiniCart {...data} />}</MiniCartQuery>;
+  // using hidden attribute will cause react to consider rendering it as low priority
+  // (in ver >= 16.6)
+  return (
+    <React.Fragment>
+      <Box hidden={contentType !== 'cart'} css={{ height: '100%' }}>
+        <MiniCartQuery>{data => <MiniCart {...data} />}</MiniCartQuery>
+      </Box>
+      <div hidden={contentType !== 'signin'}>
+        <MiniAccountQuery>{data => <MiniAccount {...data} />}</MiniAccountQuery>
+      </div>
+    </React.Fragment>
+  );
 };
--- examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarQuery.js
+++ examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarQuery.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 import { Query } from '@deity/falcon-ecommerce-uikit';
 
 export const GET_SIDEBAR_STATE = gql`
-  query SIDEBAR_STATE {
+  query Sidebar {
     sidebar @client {
       contentType
       side
