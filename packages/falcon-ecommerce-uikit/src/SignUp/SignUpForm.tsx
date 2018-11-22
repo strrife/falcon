f0abd0bb95e0efa8c0ed783c539a231f82acf2b2
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-22 14:38:17 +0100

    feat(ecommerce-uikit): add barebones sign up form for testing basic sign up flow

--- packages/falcon-ecommerce-uikit/src/SignUp/SignUpForm.tsx
+++ packages/falcon-ecommerce-uikit/src/SignUp/SignUpForm.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
+import { Formik, Form, FormikProps, Field } from 'formik';
 import { Box, Icon, List, ListItem, Button, Text } from '@deity/falcon-ui';
+import { SignUpMutation, SignUpVariables } from './SignUpMutation';
 
 export const SignUpForm = () => (
   <Box>
     <Text>No account yet?</Text>
-    <Button type="submit" css={{ width: '100%' }}>
-      Create an account
-      <Icon src="buttonArrowRight" stroke="white" />
-    </Button>
+
     <Text fontWeight="bold"> Creating an account has many benefits: </Text>
+    <SignUpMutation>
+      {signUp => (
+        <Formik
+          initialValues={{} as SignUpVariables}
+          onSubmit={(values: SignUpVariables) => {
+            signUp({
+              variables: {
+                input: {
+                  ...values,
+                  autoSignIn: true
+                }
+              }
+            });
+          }}
+        >
+          {() => (
+            <Form>
+              <Field type="string" name="firstname" />
+              <Field type="string" name="lastname" />
+              <Field type="email" name="email" />
+              <Field type="password" name="password" />
+              <Field type="password" name="passwordConfirmation" />
+              <Button type="submit" css={{ width: '100%' }}>
+                Create an account
+                <Icon src="buttonArrowRight" stroke="white" />
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      )}
+    </SignUpMutation>
 
     <List>
       <ListItem>check out faster</ListItem>
