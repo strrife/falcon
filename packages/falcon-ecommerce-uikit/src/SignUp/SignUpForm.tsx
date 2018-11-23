18a80c9e630beb9ec2a4e93e9fd89c3e084bd228
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-23 17:32:57 +0100

    feat(ecommerce-uikit): add proper UI for sign up form, after successful signup shop sidebar with account info

--- packages/falcon-ecommerce-uikit/src/SignUp/SignUpForm.tsx
+++ packages/falcon-ecommerce-uikit/src/SignUp/SignUpForm.tsx
@@ -1,39 +1,76 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { Icon, Button } from '@deity/falcon-ui';
+import { Button, Box, DefaultThemeProps, Input } from '@deity/falcon-ui';
 import { SignUpMutation, SignUpVariables } from './SignUpMutation';
-import { Field } from './Field';
+import { FormInput } from '../FormInput';
+import { OpenSidebarMutation } from '../Sidebar';
+
+const signUpFormLayout: DefaultThemeProps = {
+  signUpFormLayout: {
+    display: 'grid',
+    gridGap: 'sm'
+  }
+};
 
 export const SignUpForm = () => (
-  <SignUpMutation>
-    {signUp => (
-      <Formik
-        initialValues={{} as SignUpVariables}
-        onSubmit={(values: SignUpVariables) => {
-          signUp({
+  <OpenSidebarMutation>
+    {openSidebarMutation => (
+      <SignUpMutation
+        onCompleted={() =>
+          openSidebarMutation({
             variables: {
-              input: {
-                ...values,
-                autoSignIn: true
-              }
+              contentType: 'signin'
             }
-          });
-        }}
+          })
+        }
       >
-        {() => (
-          <Form>
-            <Field labeli18n="First name" type="string" name="firstname" autoComplete="given-name" />
-            <Field type="string" name="lastname" autoComplete="family-name" />
-            <Field type="email" name="email" autoComplete="email" />
-            <Field type="password" name="password" autoComplete="off" />
-            <Field type="password" name="passwordConfirmation" autoComplete="off" />
-            <Button type="submit" css={{ width: '100%' }}>
-              Create an account
-              <Icon src="buttonArrowRight" stroke="white" />
-            </Button>
-          </Form>
+        {(signUp, { loading }) => (
+          <Formik
+            // initial values need to be set because of: https://github.com/jaredpalmer/formik/issues/738
+            initialValues={
+              {
+                firstname: '',
+                lastname: '',
+                email: '',
+                password: '',
+                passwordConfirmation: ''
+              } as SignUpVariables
+            }
+            onSubmit={(values: SignUpVariables) => {
+              signUp({
+                variables: {
+                  input: {
+                    ...values,
+                    autoSignIn: true
+                  }
+                }
+              });
+            }}
+          >
+            {() => (
+              <Box as={Form} defaultTheme={signUpFormLayout}>
+                <Input />
+                <FormInput label="First Name" type="text" required name="firstname" autoComplete="given-name" />
+                <FormInput label="Last Name" type="text" required name="lastname" autoComplete="family-name" />
+                <FormInput label="Email" type="email" required name="email" autoComplete="email" />
+                <FormInput label="Password" type="password" required name="password" autoComplete="off" />
+                <FormInput
+                  label="Confirm Password"
+                  type="password"
+                  required
+                  name="passwordConfirmation"
+                  autoComplete="off"
+                />
+                <Box justifySelf="center">
+                  <Button type="submit" variant={loading ? 'loader' : undefined}>
+                    Create an account
+                  </Button>
+                </Box>
+              </Box>
+            )}
+          </Formik>
         )}
-      </Formik>
+      </SignUpMutation>
     )}
-  </SignUpMutation>
+  </OpenSidebarMutation>
 );
