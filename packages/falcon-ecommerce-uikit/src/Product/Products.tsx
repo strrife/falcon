9141b7cd88d69d70f4dd4e8a5c91605295aef506
tutok <walas.marcin@gmail.com>
2018-11-26 13:56:09 +0100

    feat(translations): clean up

--- packages/falcon-ecommerce-uikit/src/Product/Products.tsx
+++ packages/falcon-ecommerce-uikit/src/Product/Products.tsx
@@ -4,42 +4,6 @@ import LazyLoad from 'react-lazyload';
 import { themed, Image, Text } from '@deity/falcon-ui';
 import { Price } from '../Locale';
 
-export const ProductCardLayout = themed({
-  tag: Link,
-  defaultTheme: {
-    card: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      color: 'secondaryText',
-
-      css: {
-        height: '100%',
-        textDecoration: 'none',
-        overflow: 'hidden',
-        cursor: 'pointer'
-      }
-    }
-  }
-});
-
-export const ProductListLayout = themed({
-  tag: 'ul',
-  defaultTheme: {
-    productListLayout: {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fill,minmax(300px,1fr))',
-      gridAutoRows: '280px',
-      gridGap: 'sm',
-      m: 'none',
-      p: 'none',
-      css: {
-        listStyle: 'none'
-      }
-    }
-  }
-});
-
 export const ProductsList: React.SFC<{ products: any[] }> = ({ products }) => (
   <ProductListLayout>
     {products.map((product: any) => (
@@ -59,3 +23,39 @@ export const ProductsList: React.SFC<{ products: any[] }> = ({ products }) => (
     ))}
   </ProductListLayout>
 );
+
+export const ProductListLayout = themed({
+  tag: 'ul',
+  defaultTheme: {
+    productListLayout: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill,minmax(300px,1fr))',
+      gridAutoRows: '280px',
+      gridGap: 'sm',
+      m: 'none',
+      p: 'none',
+      css: {
+        listStyle: 'none'
+      }
+    }
+  }
+});
+
+export const ProductCardLayout = themed({
+  tag: Link,
+  defaultTheme: {
+    card: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      color: 'secondaryText',
+
+      css: {
+        height: '100%',
+        textDecoration: 'none',
+        overflow: 'hidden',
+        cursor: 'pointer'
+      }
+    }
+  }
+});
