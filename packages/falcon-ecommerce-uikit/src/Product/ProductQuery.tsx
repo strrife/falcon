be0109cef2827ab866fb34dd701de0435e7be5b9
tutok <walas.marcin@gmail.com>
2018-11-26 14:11:36 +0100

    feat(translations): moved productsList query into coresponding file

--- packages/falcon-ecommerce-uikit/src/Product/ProductQuery.tsx
+++ packages/falcon-ecommerce-uikit/src/Product/ProductQuery.tsx
@@ -68,27 +68,3 @@ export class ProductQuery extends Query<any> {
     translationsNamespaces: ['shop']
   };
 }
-
-export const GET_PRODUCTS_LIST = gql`
-  query Products {
-    products {
-      items {
-        id
-        name
-        price
-        thumbnail
-        urlPath
-      }
-    }
-  }
-`;
-
-export type Products = {
-  items: any[];
-};
-
-export class ProductsListQuery extends Query<Products> {
-  static defaultProps = {
-    query: GET_PRODUCTS_LIST
-  };
-}
