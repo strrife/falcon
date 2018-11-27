9b8b4081fb4b9be7c9e6e9fdae38c4285c23d1e1
tutok <walas.marcin@gmail.com>
2018-11-27 09:08:39 +0100

    feat(translations): revert back fitering i18next store via used namespaces

--- packages/falcon-client/src/middlewares/routes/ssrMiddleware.js
+++ packages/falcon-client/src/middlewares/routes/ssrMiddleware.js
@@ -7,6 +7,7 @@ import { I18nextProvider } from 'react-i18next-with-context';
 import { AsyncComponentProvider, createAsyncContext } from 'react-async-component';
 import asyncBootstrapper from 'react-async-bootstrapper2';
 import HtmlHead from '../../components/HtmlHead';
+import { filterResourceStoreByNs } from '../../i18n/i18nServerFactory';
 
 /**
  * Server Side Rendering middleware.
@@ -23,12 +24,7 @@ export default ({ App }) => async (ctx, next) => {
   const markup = (
     <ApolloProvider client={client}>
       <AsyncComponentProvider asyncContext={asyncContext}>
-        <I18nextProvider
-          i18n={i18next}
-          reportNS={ns => {
-            i18nextUsedNamespaces.add(ns || i18next.options.defaultNS);
-          }}
-        >
+        <I18nextProvider i18n={i18next} reportNS={x => i18nextUsedNamespaces.add(x)}>
           <StaticRouter context={context} location={ctx.url}>
             <React.Fragment>
               <HtmlHead htmlLang={i18next.language} />
@@ -57,8 +53,7 @@ export default ({ App }) => async (ctx, next) => {
   ctx.state.asyncContext = asyncContext.getState();
   ctx.state.helmetContext = Helmet.renderStatic();
 
-  // filterResourceStoreByNs(i18next.services.resourceStore.data, i18nextUsedNamespaces);
-  ctx.state.i18nextFilteredStore = i18next.services.resourceStore.data;
+  ctx.state.i18nextFilteredStore = filterResourceStoreByNs(i18next.services.resourceStore.data, i18nextUsedNamespaces);
 
   return context.url ? ctx.redirect(context.url) : next();
 };
