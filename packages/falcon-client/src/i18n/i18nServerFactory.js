cf9091298b1c35f4478a4207e762600f8ccaaa74
tutok <walas.marcin@gmail.com>
2018-11-30 19:37:26 +0100

    feat(translations): also on server side we will resolve i18n from promise

--- packages/falcon-client/src/i18n/i18nServerFactory.js
+++ packages/falcon-client/src/i18n/i18nServerFactory.js
@@ -12,35 +12,40 @@ import Backend from 'i18next-sync-fs-backend';
 /**
  * i18next instance server side factory
  * @argument {Options} options - options
- * @returns {object} - next middleware or redirect
+ * @returns {Promise<i18next.i18n>} - Promise with initialized i18n instance
  */
-export default ({ lng = 'en', fallbackLng = 'en', whitelist = ['en'], debug = false, resources } = {}) => {
-  const defaultNS = 'translations';
+export default ({ lng = 'en', fallbackLng = 'en', whitelist = ['en'], debug = false, resources } = {}) =>
+  new Promise((resolve, reject) => {
+    const defaultNS = 'translations';
 
-  return i18next.use(Backend).init({
-    lng,
-    ns: [defaultNS],
-    fallbackLng,
-    whitelist,
-    defaultNS,
-    fallbackNS: defaultNS,
-    saveMissing: false,
-    initImmediate: false,
-    resources,
-    debug,
-    react: {
-      wait: true,
-      nsMode: 'fallback'
-    },
-    interpolation: {
-      escapeValue: false
-    },
-    backend: {
-      loadPath: 'build/i18n/{{lng}}/{{ns}}.json',
-      jsonIndent: 2
-    }
+    const instance = i18next.use(Backend);
+    instance.init(
+      {
+        lng,
+        ns: [defaultNS],
+        fallbackLng,
+        whitelist,
+        defaultNS,
+        fallbackNS: defaultNS,
+        saveMissing: false,
+        initImmediate: false,
+        resources,
+        debug,
+        react: {
+          wait: true,
+          nsMode: 'fallback'
+        },
+        interpolation: {
+          escapeValue: false
+        },
+        backend: {
+          loadPath: 'build/i18n/{{lng}}/{{ns}}.json',
+          jsonIndent: 2
+        }
+      },
+      error => (error ? reject(error) : resolve(instance))
+    );
   });
-};
 
 export function extractI18nextState(ctx) {
   if (ctx.i18next) {
