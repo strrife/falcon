5ca6bd104449004582109388b2c7248562240424
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-11-30 10:12:35 +0100

    feat(ecommerce-ukit): add proper handling of invalid or expired token provided on reset-email flow

--- packages/falcon-ecommerce-uikit/src/AccountRecovery/ResetPassword.tsx
+++ packages/falcon-ecommerce-uikit/src/AccountRecovery/ResetPassword.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Formik } from 'formik';
-import { Text, GridLayout, H2 } from '@deity/falcon-ui';
+import { Text, Button, GridLayout, H1, Link, FlexLayout, Box } from '@deity/falcon-ui';
 import { FormField, Form, FormSubmit, FormErrorSummary } from '../Forms';
 import { ResetCustomerPasswordMutation } from './AccountRecoveryMutations';
 import { ValidatePasswordTokenQuery } from './AccountRecoveryQueries';
+import { OpenSidebarMutation } from '../Sidebar';
 
 type ResetPasswordProps = {
   customerId: number;
@@ -11,13 +12,43 @@ type ResetPasswordProps = {
 };
 export const ResetPassword: React.SFC<ResetPasswordProps> = ({ customerId, resetToken }) => (
   <ValidatePasswordTokenQuery variables={{ id: customerId, token: resetToken }}>
-    {({ validatePasswordToken }) => (
-      <GridLayout>
-        <H2>Reset Password</H2>
-        <div>The reset password token you provided is invalid or has expired</div>
-        <div>Request another one</div>
-        <div> {JSON.stringify(validatePasswordToken)}</div>
-      </GridLayout>
-    )}
+    {({ validatePasswordToken }) => {
+      const tokenIsInvalid = !validatePasswordToken;
+
+      return (
+        <GridLayout gridGap="md" py="md">
+          <H1 justifySelf="center">Reset Password</H1>
+          {tokenIsInvalid && <InvalidToken />}
+        </GridLayout>
+      );
+    }}
   </ValidatePasswordTokenQuery>
 );
+
+export const InvalidToken: React.SFC = () => (
+  <OpenSidebarMutation>
+    {openSidebar => (
+      <>
+        <Text justifySelf="center" fontSize="md" color="error">
+          The reset password token you provided is invalid or has expired.
+        </Text>
+        <Box justifySelf="center">
+          <Button
+            onClick={() =>
+              openSidebar({
+                variables: {
+                  contentType: 'forgotPassword'
+                }
+              })
+            }
+          >
+            Request another one
+          </Button>
+        </Box>
+      </>
+    )}
+  </OpenSidebarMutation>
+);
+
+// zrobić otwieranie sidebar'a jako komponent??
+export const ResetPasswordForm: React.SFC = () => <div />;
