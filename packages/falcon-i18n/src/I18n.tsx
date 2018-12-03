dceae69cd6c305ae93bcb12976d7e8d7df112f73
tutok <walas.marcin@gmail.com>
2018-12-03 10:09:55 +0100

    feat(translations): added rerenders into I18n  when i18next will change

--- packages/falcon-i18n/src/I18n.tsx
+++ packages/falcon-i18n/src/I18n.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import i18next from 'i18next';
-import { I18nContext } from './context';
+import { I18nContext, I18nContextOptions } from './context';
 
 export type I18nRenderProps = {
   i18n: i18next.i18n;
@@ -15,9 +15,40 @@ export class I18n extends React.Component<I18nProps> {
     children: PropTypes.oneOfType([PropTypes.func]).isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    this.onI18nChanged = this.onI18nChanged.bind(this);
+  }
+
+  componentDidMount() {
+    this.options!.rerenderOn.forEach(x => this.i18n!.on(x, this.onI18nChanged));
+  }
+
+  componentWillUnmount() {
+    this.options!.rerenderOn.forEach(x => this.i18n!.off(x, this.onI18nChanged));
+  }
+
+  onI18nChanged() {
+    // eslint-disable-next-line react/no-unused-state
+    this.setState({ i18nChangedAt: new Date() });
+  }
+
+  i18n?: i18next.i18n = undefined;
+  options?: I18nContextOptions = undefined;
+
   render() {
     const { children } = this.props;
 
-    return <I18nContext.Consumer>{({ i18n, t }) => children({ i18n, t })}</I18nContext.Consumer>;
+    return (
+      <I18nContext.Consumer>
+        {({ i18n, t, options: contextOptions }) => {
+          this.i18n = i18n;
+          this.options = contextOptions;
+
+          return children({ i18n, t });
+        }}
+      </I18nContext.Consumer>
+    );
   }
 }
