638b4acaa55c7adc3f6464eb9d8b4bee5c03b663
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-12-03 09:24:35 +0100

    improvement(shop-and-blog): do not convert token to number when reading it from url query params

--- examples/shop-with-blog/client/src/pages/shop/ResetPassword.js
+++ examples/shop-with-blog/client/src/pages/shop/ResetPassword.js
@@ -4,7 +4,7 @@ import { ResetPassword } from '@deity/falcon-ecommerce-uikit';
 export default ({ location }) => {
   const queryParams = new URLSearchParams(location.search);
   const customerId = +queryParams.get('id') || 0;
-  const resetToken = +queryParams.get('token') || '';
+  const resetToken = queryParams.get('token') || '';
 
   return <ResetPassword customerId={customerId} resetToken={resetToken} />;
 };
