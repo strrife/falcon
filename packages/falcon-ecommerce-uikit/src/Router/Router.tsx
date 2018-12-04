e2dd41cbe0fda5e4c0817a42dd81191787731bf4
tutok <walas.marcin@gmail.com>
2018-12-04 14:57:03 +0100

    feat(auth-route): changes after review

--- packages/falcon-ecommerce-uikit/src/Router/Router.tsx
+++ packages/falcon-ecommerce-uikit/src/Router/Router.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import ReactRouterDom, { withRouter, RouteComponentProps } from 'react-router-dom';
+// eslint-disable-next-line
+import { History, Location } from 'history';
 
-const RenderRouterProps: React.SFC<RouteComponentProps> = props => {
+export type IRenderRouterProps = {
+  children: ({  }: ReactRouterRenderProps) => any;
+} & RouteComponentProps;
+
+export type ReactRouterRenderProps = {
+  match: ReactRouterDom.match;
+  location: Location;
+  history: History;
+};
+
+const RenderRouterProps: React.SFC<IRenderRouterProps> = props => {
   const { match, location, history, children } = props;
 
   return (children as Function)({ match, location, history });
--- packages/falcon-ecommerce-uikit/src/SignOut/SignOutLogic.tsx
+++ packages/falcon-ecommerce-uikit/src/SignOut/SignOutLogic.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { MutationFn, OperationVariables, MutationResult } from 'react-apollo';
-import { adopt } from 'react-adopt';
 import { Router } from './../Router';
 import { SignOutMutation } from './SignOutMutation';
 
@@ -10,17 +9,18 @@ export type SignOutLogicRenderProps = {
   signOutMutation: { signOut: MutationFn<any, OperationVariables>; result: MutationResult<any> };
 };
 
-export const SignOutLogic = adopt<SignOutLogicRenderProps>({
-  router: ({ render }) => <Router>{(router: any) => render && render({ ...router })}</Router>,
-  signOutMutation: ({ router, render }) => (
-    <SignOutMutation
-      update={(_cache, result) => {
-        if (!result.errors && result.data.signOut) {
-          router.history.push('/');
-        }
-      }}
-    >
-      {(signOut, result) => render && render({ signOut, result })}
-    </SignOutMutation>
-  )
-});
+export const SignOutLogic = (props: { children: any }) => (
+  <Router>
+    {({ history }) => (
+      <SignOutMutation
+        update={(_cache, result) => {
+          if (!result.errors && result.data.signOut) {
+            history.push('/');
+          }
+        }}
+      >
+        {(signOut, result) => props.children({ signOut, result })}
+      </SignOutMutation>
+    )}
+  </Router>
+);
