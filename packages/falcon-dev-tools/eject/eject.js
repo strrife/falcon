5880437a028f70152f59499a08b2dd9e6406f8d1
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-12-06 13:00:04 +0100

    refactor(eject): refactor eject implementation

--- packages/falcon-dev-tools/eject/eject.js
+++ packages/falcon-dev-tools/eject/eject.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-use-before-define, import/no-dynamic-require */
 const path = require('path');
 const fs = require('fs-extra');
 const klawSync = require('klaw-sync');
@@ -9,6 +10,7 @@ const recast = require('recast');
 const prettier = require('prettier');
 const install = require('install-packages');
 
+// as an improvement those options could be provided via command line perhaps
 const prettierOptions = {
   tabWidth: 2,
   singleQuote: true,
@@ -17,59 +19,24 @@ const prettierOptions = {
   endOfLine: 'lf'
 };
 
-const pluginRecast = () => ({
-  parserOverride(code, opts, parser) {
-    return recast.parse(code, {
-      ...opts,
-      parser: {
-        parse: source => parser(source, Object.assign({}, opts, { tokens: true }))
-      }
-    });
-  },
-  generatorOverride(ast, opts) {
-    return recast.print(ast, opts);
-  }
-});
-
-const onTsFiles = item => {
-  const ext = path.extname(item.path);
-
-  if (ext && ext !== '.ts' && ext !== '.tsx') return false;
-
-  return true;
-};
-
-const onlyAppJsFiles = targetDir => item => {
-  const ext = path.extname(item.path);
-
-  if (ext && ext !== '.js') return false;
-
-  return item.path.indexOf(targetDir) === -1;
-};
-
-const skipTypeDefinitionsFiles = src => !src.endsWith('d.ts');
-
-const bold = text => chalk.blue.underline.bold(text);
-
-const normalizeNewLines = src => src.replace(/\r?\n|\r/g, '\n');
-
 async function run() {
   const packageToEject = process.argv[2];
   const targetDir = process.argv[3];
   const targetDirFullPath = path.join(process.cwd(), targetDir);
   const appSrc = path.join(process.cwd(), 'src');
+  // assumption is that resolvedPackageDir points to dist folder that is one level deep inside root package dir
+  // and based on that assumption we can retreive package json and src dir
   const resolvedPackageDir = path.dirname(require.resolve(packageToEject));
   const resolvedPackagePackageJsonPath = path.join(resolvedPackageDir, '../package.json');
-  // eslint-disable-next-line
   const resolvedPackagePackageJsonDependencies = require(resolvedPackagePackageJsonPath).dependencies;
+  const packageSrcFiles = path.join(resolvedPackageDir, '../src');
 
   console.log(chalk.green(`Ejecting ${bold(packageToEject)} package into ${bold(targetDirFullPath)} ...`));
 
-  const packageSrcFiles = path.join(resolvedPackageDir, '../src');
-
+  // don't run any further if targetDir already exists, we don't want to overwrite anything
   if (fs.pathExistsSync(targetDirFullPath)) {
     console.log(
-      chalk.red(`Looks like ${bold(packageToEject)} has been already ejected, ${bold(targetDir)} dir already exists.`)
+      chalk.red(`Looks like ${bold(packageToEject)} has been already ejected ${bold(targetDir)} dir already exists.`)
     );
     process.exit(-1);
 
@@ -77,69 +44,138 @@ async function run() {
   }
 
   // copy source typescript files to target dir
-  await fs.copy(packageSrcFiles, targetDirFullPath, { filter: skipTypeDefinitionsFiles });
+  fs.copySync(packageSrcFiles, targetDirFullPath, { filter: filterOnlyTSSourceFiles });
 
-  const ejectedFilesMeta = klawSync(targetDirFullPath, { nodir: true, filter: onTsFiles });
+  // retreive all copied TS files metadata
+  const ejectedFilesMeta = klawSync(targetDirFullPath, { nodir: true });
+  // once files are copied then we transform each of them by removing TS types
+  // while preserving original formatting and changing extension to .js
+  convertToJs(ejectedFilesMeta);
 
-  // one files are copied transform each of, them removing TS types
-  // while preserving original formatting and rename to .js extension
+  // retreive all project  files that can have imports to package being ejected
+  const appFilesMeta = klawSync(appSrc, { nodir: true, filter: filterOnlyAppJsFiles(targetDir) });
 
-  for (let i = 0; i < ejectedFilesMeta.length; i++) {
-    const filePath = ejectedFilesMeta[i].path;
-    // eslint-disable-next-line
-    let fileContents = await fs.readFile(filePath, 'utf8');
-    // babel transforms Typescript to JS using recast to preserver as much original formatting as possible
+  // and then replace imports inside them
+  replaceImports(appFilesMeta, packageToEject, targetDir);
+
+  // as a final step install dependencies of package being ejected to project where it's ejected
+  // this logic fails when using yarn workspaces - https://github.com/yarnpkg/yarn/issues/4812
+  const dependenciesToInstall = Object.keys(resolvedPackagePackageJsonDependencies).map(
+    packageName => `${packageName}@${resolvedPackagePackageJsonDependencies[packageName]}`
+  );
+
+  console.log(chalk.green(`Installing ${bold(packageToEject)} package dependencies in the project...`));
+
+  try {
+    await install({
+      packages: dependenciesToInstall,
+      peerFilter: () => false
+    });
+  } catch (_ignored) {
+    console.log(
+      chalk.green(
+        `
+         It appears that you've run eject command while using yarn workspaces in your project,
+         hence intallation failed due to known yarn issue (https://github.com/yarnpkg/yarn/issues/4812).
+         Package has been ejected, but if you'd have troubles running the project after it anyway, try running yarn install in the root dir.`
+      )
+    );
+  }
+
+  console.log(chalk.green(`${bold(packageToEject)} package ejected!`));
+}
+
+run();
+
+function convertToJs(ejectedFilesMeta) {
+  ejectedFilesMeta.forEach(meta => {
+    const filePath = meta.path;
+
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    // babel transforms Typescript to JS using recast to preserve as much original formatting as possible
     const result = babel.transformSync(fileContents, {
       plugins: [['@babel/plugin-transform-typescript', { isTSX: true }], pluginRecast]
     });
 
     // run prettier on transformed JS as recast preserves formatting such as whitespaces, but formats bit diferently thant prettier
     const transformedContents = prettier.format(result.code, prettierOptions);
+
     // write transformed code to file
-    // eslint-disable-next-line
-    await fs.writeFile(filePath, normalizeNewLines(transformedContents), 'utf8');
-    // and rename file to .js extension
+    fs.writeFileSync(filePath, normalizeNewLines(transformedContents), 'utf8');
+
+    // finally rename file to .js extension
     const targetPathJs = filePath.replace(path.extname(filePath), '.js');
-    // eslint-disable-next-line
-    await fs.move(filePath, targetPathJs);
-  }
+    fs.moveSync(filePath, targetPathJs);
+  });
+}
 
-  // replace imports inside project src
-  const appFilesMeta = klawSync(appSrc, { nodir: true, filter: onlyAppJsFiles(targetDir) });
-
+function replaceImports(appFilesMeta, originalImport, replacementImport) {
   for (let i = 0; i < appFilesMeta.length; i++) {
     const filePath = appFilesMeta[i].path;
-    // eslint-disable-next-line
-    const fileContents = await fs.readFile(filePath, 'utf8');
+
+    const fileContents = fs.readFileSync(filePath, 'utf8');
     // run babel transforms that preserve formatting while replacing imports
     const result = babel.transformSync(fileContents, {
       plugins: [
         '@babel/plugin-syntax-jsx',
         '@babel/plugin-syntax-dynamic-import',
         '@babel/plugin-syntax-class-properties',
-        ['babel-plugin-transform-rename-import', { original: packageToEject, replacement: targetDir }],
+        ['babel-plugin-transform-rename-import', { original: originalImport, replacement: replacementImport }],
         pluginRecast
       ]
     });
 
     const transformedContents = prettier.format(result.code, prettierOptions);
 
-    // eslint-disable-next-line
-    await fs.writeFile(filePath, normalizeNewLines(transformedContents), 'utf8');
+    fs.writeFileSync(filePath, normalizeNewLines(transformedContents), 'utf8');
   }
-  const dependenciesToInstall = Object.keys(resolvedPackagePackageJsonDependencies).map(
-    packageName => `${packageName}@${resolvedPackagePackageJsonDependencies[packageName]}`
-  );
-  console.log(
-    chalk.green(`${bold(packageToEject)} package dependencies in project...\n${dependenciesToInstall.join('\n')}`)
-  );
-
-  await install({
-    packages: Object.keys(resolvedPackagePackageJsonDependencies),
-    peerFilter: () => false
-  });
-
-  console.log(chalk.green(`${bold(packageToEject)} package ejected!`));
 }
 
-run();
+// recast plugin overrides parser and generator of babel to use recast's ones
+// as recast preserves original file formatting much better (new lines, white spaces)
+function pluginRecast() {
+  return {
+    parserOverride(code, opts, parser) {
+      return recast.parse(code, {
+        ...opts,
+        parser: {
+          parse: source => parser(source, Object.assign({}, opts, { tokens: true }))
+        }
+      });
+    },
+    generatorOverride(ast, opts) {
+      return recast.print(ast, opts);
+    }
+  };
+}
+// it accepts two args (src, dest) that are strings
+// https://github.com/jprichardson/node-fs-extra/blob/master/docs/copy.md
+function filterOnlyTSSourceFiles(src) {
+  const ext = path.extname(src);
+
+  if (ext && ext !== '.ts' && ext !== '.tsx') return false;
+  // skip tests as well
+  if (src.indexOf('.test') !== -1) return false;
+
+  return true;
+}
+
+// it accepts single argument - designed to work with
+// https://github.com/manidlou/node-klaw-sync#klawsyncdirectory-options
+function filterOnlyAppJsFiles(targetDir) {
+  return item => {
+    const ext = path.extname(item.path);
+
+    if (ext && ext !== '.js') return false;
+
+    return item.path.indexOf(targetDir) === -1;
+  };
+}
+
+function bold(text) {
+  return chalk.blue.underline.bold(text);
+}
+
+function normalizeNewLines(src) {
+  return src.replace(/\r?\n|\r/g, '\n');
+}
