d36877472486a09c8ae48afa8cdc7d57dc135dc0
tutok <walas.marcin@gmail.com>
2018-12-10 21:19:19 +0100

    feat(translations): added Form component which will provide context for field ids and transaltions

--- packages/falcon-ecommerce-uikit/src/Forms/Form.tsx
+++ packages/falcon-ecommerce-uikit/src/Forms/Form.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Form as FormikForm } from 'formik';
-import { DefaultThemeProps, ThemedComponentProps, Box } from '@deity/falcon-ui';
+import { Form as FormikForm, FormikFormProps } from 'formik';
+import { ThemedComponentProps, Box } from '@deity/falcon-ui';
 
-const formLayout: DefaultThemeProps = {
-  formLayout: {
-    display: 'grid',
-    gridGap: 'sm'
-  }
+export type FormContextValue = {
+  id: number | string;
+  name: string;
 };
 
-export const Form: React.SFC<ThemedComponentProps> = props => (
-  <Box {...props} as={FormikForm as any} defaultTheme={formLayout} />
-);
+export const FormContext = React.createContext<FormContextValue>({
+  id: '',
+  name: ''
+});
+
+export type FormProps = { id?: number | string; name: string } & FormikFormProps & ThemedComponentProps;
+
+export const Form: React.SFC<FormProps> = props => {
+  const { id, name } = props;
+
+  return (
+    <FormContext.Provider value={{ id: id || name, name }}>
+      <Box {...props} as={FormikForm as any} defaultTheme={{ formLayout: { display: 'grid', gridGap: 'sm' } }} />
+    </FormContext.Provider>
+  );
+};
