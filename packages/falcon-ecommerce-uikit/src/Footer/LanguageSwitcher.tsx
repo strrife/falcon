6a31ef19b9f3574325df74af639ed19853c109b1
tutok <walas.marcin@gmail.com>
2018-12-10 20:03:50 +0100

    feat(translations): added cimode language into list

--- packages/falcon-ecommerce-uikit/src/Footer/LanguageSwitcher.tsx
+++ packages/falcon-ecommerce-uikit/src/Footer/LanguageSwitcher.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Dropdown, DropdownLabel, DropdownMenu, DropdownMenuItem } from '@deity/falcon-ui';
 import { LanguageItem } from './FooterQuery';
 
+const addCIModeLanguage = (languages: LanguageItem[]) => {
+  if (process.env.NODE_ENV === 'development') {
+    if (!languages.find(x => x.code === 'cimode')) {
+      languages.unshift({ code: 'cimode', name: 'CI mode', active: false });
+    }
+  }
+
+  return languages;
+};
+
 type LanguageSwitcherProps = {
   languages: LanguageItem[];
   onChange?: (x: LanguageItem) => any;
 };
 
 export const LanguageSwitcher: React.SFC<LanguageSwitcherProps> = ({ languages, onChange }) => {
+  languages = addCIModeLanguage(languages);
   const activeLanguage = languages.filter(lang => lang.active)[0];
 
   return (
