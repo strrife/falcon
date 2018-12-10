4572386573ab62d95dfd3a79b000de0937690a5c
Oleksandr Bordun <bordun.alexandr@gmail.com>
2018-12-10 16:03:35 +0100

    fix(tests): Providing tests for Cache and Extension classes

--- packages/falcon-server-env/src/cache/Cache.test.ts
+++ packages/falcon-server-env/src/cache/Cache.test.ts
@@ -3,25 +3,63 @@ import Cache from './Cache';
 
 describe('Cache', () => {
   let provider: KeyValueCache;
+  let cache: Cache;
+
   beforeEach(() => {
     provider = {
       get: jest.fn(),
       set: jest.fn(),
       delete: jest.fn()
     } as KeyValueCache;
+    cache = new Cache(provider);
   });
 
-  it('should properly pass key to "set" method', async () => {
-    await provider.set('key', 'value');
-    expect(provider.set).toBeCalledWith('key', 'value');
+  it('Should properly pass arguments to "set" method', async () => {
+    await cache.set('key', 'value');
+    expect(provider.set).toBeCalledWith('key', 'value', undefined);
     provider.set.mockClear();
 
-    await provider.set('key', 'value', { ttl: 1 });
+    await cache.set('key', 'value', { ttl: 1 });
     expect(provider.set).toBeCalledWith('key', 'value', { ttl: 1 });
   });
 
-  it('should properly pass key to "delete" method', async () => {
-    await provider.delete('foo');
+  it('Should properly pass key to "delete" method', async () => {
+    await cache.delete('foo');
     expect(provider.delete).toBeCalledWith('foo');
+  });
+
+  it('Should properly pass a string key to "get" method', async () => {
+    const value: string | undefined = await cache.get('key');
+    expect(provider.get).toBeCalledWith('key');
+  });
+
+  it('Should properly handle a callback option to fill the cache for "get" method', async () => {
+    provider.get = jest.fn(() => undefined);
+    const value: string | undefined = await cache.get({
+      key: 'key',
+      callback: () => 'new_value',
+      options: {
+        ttl: 1
+      }
+    });
+
+    expect(provider.get).toBeCalledWith('key');
+    expect(provider.set).toBeCalledWith('key', 'new_value', { ttl: 1 });
+  });
+
+  it('Should be able to define TTL dynamically from callback for "get" method', async () => {
+    provider.get = jest.fn(() => undefined);
+    const value: string | undefined = await cache.get({
+      key: 'key',
+      callback: () => ({
+        value: 'value',
+        options: {
+          ttl: 10
+        }
+      })
+    });
+
+    expect(provider.get).toBeCalledWith('key');
+    expect(provider.set).toBeCalledWith('key', 'value', { ttl: 10 });
   });
 });
--- packages/falcon-server-env/src/models/Extension.test.ts
+++ packages/falcon-server-env/src/models/Extension.test.ts
@@ -1,9 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'jest-extended';
+import * as Logger from '@deity/falcon-logger';
 import { GraphQLResolveInfo } from 'graphql';
 import Extension from './Extension';
 import ApiDataSource from './ApiDataSource';
 import { FetchUrlResult } from '../types';
+
+global.__SERVER__ = true; // eslint-disable-line no-underscore-dangle
+
+Logger.setLogLevel('off');
 
 class CustomExtension extends Extension {
   getFetchUrlPriority(url: string): number {
@@ -30,5 +35,50 @@ describe('Extension', () => {
   it('Should create an instance of Extension', async () => {
     expect(ext.config).toEqual({});
     expect(ext.name).toBe('CustomExtension');
+  });
+
+  it('Should properly bind GQL types to the API Provider instance', async () => {
+    const typeDefs = `
+    extend type Query {
+      foo: Number
+      missingResolver: Int
+    }
+    extend type Mutation {
+      bar: Number
+    }
+    `;
+    ext.config.api = 'api';
+
+    const { resolvers } = await ext.getGraphQLConfig(typeDefs);
+    expect(Object.keys(resolvers)).toEqual(['Query', 'Mutation']);
+    expect(Object.keys(resolvers.Query)).toEqual(['foo', 'missingResolver']);
+    expect(Object.keys(resolvers.Mutation)).toEqual(['bar']);
+
+    const api: CustomApiDataSource = new CustomApiDataSource({});
+    api.foo = jest.fn(() => 'foo');
+    api.bar = jest.fn(() => 'bar');
+    api.initialize({ context: {} });
+    const context = {
+      dataSources: {
+        api
+      }
+    };
+
+    const resultFoo: string = await resolvers.Query.foo({}, {}, context);
+    expect(resultFoo).toBe('foo');
+    const resultBar: string = await resolvers.Mutation.bar({}, {}, context);
+    expect(resultBar).toBe('bar');
+
+    try {
+      await resolvers.Query.missingResolver({}, {}, context);
+      expect(false).toBeTruthy();
+    } catch (error) {
+      expect(error.message).toBe('CustomExtension: api.missingResolver() resolver method is not defined!');
+    }
+  });
+
+  it('Should not fail if "typeDefs" won\'t be passed to getGraphQLConfig() method', async () => {
+    const gqlConfig = await ext.getGraphQLConfig();
+    expect(gqlConfig.resolvers).toBeEmpty();
   });
 });
