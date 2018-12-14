355c93a4e39ba903d9f7186276fe94f6a97c67ab
tutok <walas.marcin@gmail.com>
2018-12-14 20:10:34 +0100

    feat(translations): aligned versions

--- packages/falcon-i18n/src/I18n.tsx
+++ packages/falcon-i18n/src/I18n.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import i18next, { TranslationFunction } from 'i18next';
+import i18next from 'i18next';
 import { I18nContext } from './context';
 
 export type I18nProps = {
-  children: (t: TranslationFunction, i18n: i18next.i18n) => any;
+  children: (t: i18next.TranslationFunction, i18n: i18next.i18n) => any;
 };
 export class I18n extends React.Component<I18nProps> {
   static propTypes = {
