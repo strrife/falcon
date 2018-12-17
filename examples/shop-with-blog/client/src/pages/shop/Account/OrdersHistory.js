4a73ec23e4e9f4f1d0a54fb7a807bd6c55c3b442
tutok <walas.marcin@gmail.com>
2018-12-17 16:36:52 +0100

    feat(account-pages): added translation title for ordersHistory

--- examples/shop-with-blog/client/src/pages/shop/Account/OrdersHistory.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/OrdersHistory.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { H1, Box } from '@deity/falcon-ui';
-import { Order } from '@deity/falcon-ecommerce-uikit';
+import { T } from '@deity/falcon-i18n';
+// import { OrdersListQuery, OrdersList } from '@deity/falcon-ecommerce-uikit';
 
 const OrdersHistory = () => (
   <Box>
-    <H1>Orders History</H1>
-    <Order />
+    <H1>
+      <T id="ordersList.title" />
+    </H1>
+    {/* <OrdersListQuery>{orders => <OrdersList {...orders} />}</OrdersListQuery> */}
   </Box>
 );
 
