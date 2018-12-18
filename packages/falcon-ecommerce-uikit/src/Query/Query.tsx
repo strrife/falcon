6e075b8d752578ad8ebb8b413cdbff55e75b81c9
tutok <walas.marcin@gmail.com>
2018-12-18 11:45:08 +0100

    feat(account-pages): improved types on generic Query component

--- packages/falcon-ecommerce-uikit/src/Query/Query.tsx
+++ packages/falcon-ecommerce-uikit/src/Query/Query.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Query as ApolloQuery, OperationVariables, QueryProps, QueryResult } from 'react-apollo';
+import { Query as ApolloQuery, OperationVariables, QueryProps as ApolloQueryProps, QueryResult } from 'react-apollo';
 import { NetworkStatus, ApolloError } from 'apollo-client';
 import { Loader } from './Loader';
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
+export type ApolloFetchMore<TData, TVariables> = QueryResult<TData, TVariables>['fetchMore'];
+export type FetchMore<TData, TVariables> = (data: TData, fetchMore: ApolloFetchMore<TData, TVariables>) => any;
+
+export type QueryRenderProps<TData> = TData & { networkStatus: NetworkStatus; fetchMore: (() => any) | undefined };
+
+export type QueryProps<TData, TVariables> = Omit<ApolloQueryProps<TData, TVariables>, 'children'> & {
+  children: (result: QueryRenderProps<TData>) => React.ReactNode;
+  fetchMore?: FetchMore<TData, TVariables>;
+};
+
 export class Query<TData = any, TVariables = OperationVariables> extends React.Component<
-  Omit<QueryProps<TData, TVariables>, 'children'> & {
-    children: (result: TData) => React.ReactNode;
-  } & {
-    fetchMore?: (data: TData, fetchMore: QueryResult<TData, TVariables>['fetchMore']) => any;
-  }
+  QueryProps<TData, TVariables>
 > {
   static propTypes = {
     ...ApolloQuery.propTypes
@@ -51,13 +57,11 @@ export class Query<TData = any, TVariables = OperationVariables> extends React.C
             return <Loader />;
           }
 
-          const props = {
-            ...(data as any),
+          return children({
+            ...data!,
             networkStatus,
             fetchMore: fetchMore ? () => fetchMore(data!, apolloFetchMore) : undefined
-          };
-
-          return children(props);
+          });
         }}
       </ApolloQuery>
     );
