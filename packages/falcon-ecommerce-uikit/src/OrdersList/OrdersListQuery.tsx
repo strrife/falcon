a824612e846349ab3eaddbad748b8200b0a72cc7
tutok <walas.marcin@gmail.com>
2018-12-18 11:47:17 +0100

    feat(account-pages): configured fetchmore more on OrdersListQuery

--- packages/falcon-ecommerce-uikit/src/OrdersList/OrdersListQuery.tsx
+++ packages/falcon-ecommerce-uikit/src/OrdersList/OrdersListQuery.tsx
@@ -1,9 +1,9 @@
 import gql from 'graphql-tag';
-import { Query, ShopPageQuery } from '../Query';
+import { Query, FetchMore, ShopPageQuery, Pagination } from '../Query';
 import { Order } from './../Order';
 
 export const GET_ORDERS_LIST = gql`
-  query Orders($page: Int = 1, $perPage: Int = 10) {
+  query Orders($page: Int = 1, $perPage: Int = 2) {
     orders(query: { page: $page, perPage: $perPage }) {
       items {
         incrementId
@@ -14,16 +14,47 @@ export const GET_ORDERS_LIST = gql`
         grandTotal
         orderCurrencyCode
       }
+      pagination {
+        currentPage
+        totalItems
+        nextPage
+      }
     }
   }
 `;
 
 export type OrdersData = {
-  items: Order[];
+  orders: {
+    items: Order[];
+    pagination: Pagination;
+  };
 };
+
+const fetchMore: FetchMore<OrdersData, ShopPageQuery> = (data, apolloFetchMore) =>
+  apolloFetchMore({
+    variables: { page: data.orders.pagination.nextPage },
+    updateQuery: (prev, { fetchMoreResult }) => {
+      if (!fetchMoreResult) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        ...{
+          orders: {
+            ...prev.orders,
+            items: [...prev.orders.items, ...fetchMoreResult.orders.items],
+            pagination: { ...fetchMoreResult.orders.pagination }
+          }
+        }
+      };
+    }
+  });
 
 export class OrdersListQuery extends Query<OrdersData, ShopPageQuery> {
   static defaultProps = {
-    query: GET_ORDERS_LIST
+    query: GET_ORDERS_LIST,
+    fetchMore,
+    notifyOnNetworkStatusChange: true
   };
 }
