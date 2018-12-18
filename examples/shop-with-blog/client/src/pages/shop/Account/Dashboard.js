fe85e548da35c4abb8031dc6fa2b9da766ec5deb
tutok <walas.marcin@gmail.com>
2018-12-18 19:02:35 +0100

    feat(account-pages): added recent order into dashboard

--- examples/shop-with-blog/client/src/pages/shop/Account/Dashboard.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/Dashboard.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { Box, Icon, Button, H1 } from '@deity/falcon-ui';
-import { SignOutMutation } from '@deity/falcon-ecommerce-uikit';
+import { Link as RouterLink } from 'react-router-dom';
+import { T } from '@deity/falcon-i18n';
+import { Box, H1, H2, Text, Link, FlexLayout } from '@deity/falcon-ui';
+import { CustomerQuery, OrdersListQuery, NoOrders, OrdersList } from '@deity/falcon-ecommerce-uikit';
 
 const Dashboard = () => (
   <Box>
-    <H1>Dashboard</H1>
+    <H1>
+      <T id="dashboard.title" />
+    </H1>
     <Box>
-      <SignOutMutation>
-        {(signOut, { loading }) => (
-          <Button mt="md" disabled={loading} onClick={() => signOut()}>
-            Logout
-            <Icon
-              src={loading ? 'loader' : 'logOut'}
-              stroke="white"
-              fill={loading ? 'white' : 'transparent'}
-              size="md"
-              ml="xs"
-            />
-          </Button>
+      <H2>
+        <T id="dashboard.personalInformation" />
+      </H2>
+      <CustomerQuery>
+        {({ customer }) => (
+          <Box>
+            <Text>{`${customer.firstname} ${customer.lastname}`}</Text>
+            <Text> {customer.email}</Text>
+          </Box>
         )}
-      </SignOutMutation>
+      </CustomerQuery>
+    </Box>
+
+    <Box mt="lg">
+      <OrdersListQuery variables={{ perPage: 1, page: 1 }}>
+        {({ orders: { items } }) => (
+          <>
+            <FlexLayout justifyContent="flex-start" alignItems="baseline">
+              <H2 mr="md">
+                <T id="dashboard.recentOrder" />
+              </H2>
+              <Link as={RouterLink} to="/account/orders">
+                <T id="dashboard.viewAllOrders" />
+              </Link>
+            </FlexLayout>
+            <Box>{items.length ? <OrdersList items={items} /> : <NoOrders />}</Box>
+          </>
+        )}
+      </OrdersListQuery>
     </Box>
   </Box>
 );
