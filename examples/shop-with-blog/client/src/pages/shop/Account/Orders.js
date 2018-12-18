56bcec7378ca4ef117fefbbd0399547c19265a2a
tutok <walas.marcin@gmail.com>
2018-12-18 18:40:05 +0100

    feat(account-pages): displayed NoOrders if it is a case

--- examples/shop-with-blog/client/src/pages/shop/Account/Orders.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/Orders.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NetworkStatus } from 'apollo-client';
 import { H1, Box, Button, FlexLayout } from '@deity/falcon-ui';
 import { T } from '@deity/falcon-i18n';
-import { OrdersListQuery, OrdersList } from '@deity/falcon-ecommerce-uikit';
+import { OrdersListQuery, OrdersList, NoOrders } from '@deity/falcon-ecommerce-uikit';
 
 const Orders = () => (
   <Box>
@@ -10,22 +10,26 @@ const Orders = () => (
       <T id="ordersList.title" />
     </H1>
     <OrdersListQuery>
-      {({ orders: { items, pagination }, fetchMore, networkStatus }) => (
-        <>
-          <OrdersList items={items} />
-          {pagination.nextPage && (
-            <FlexLayout justifyContent="center">
-              <Button
-                onClick={fetchMore}
-                variant={networkStatus === NetworkStatus.fetchMore ? 'loader' : 'secondary'}
-                height="xl"
-              >
-                <T id="ordersList.showMore" />
-              </Button>
-            </FlexLayout>
-          )}
-        </>
-      )}
+      {({ orders: { items, pagination }, fetchMore, networkStatus }) =>
+        items.length ? (
+          <>
+            <OrdersList items={items} />
+            {pagination.nextPage && (
+              <FlexLayout justifyContent="center">
+                <Button
+                  onClick={fetchMore}
+                  variant={networkStatus === NetworkStatus.fetchMore ? 'loader' : 'secondary'}
+                  height="xl"
+                >
+                  <T id="ordersList.showMore" />
+                </Button>
+              </FlexLayout>
+            )}
+          </>
+        ) : (
+          <NoOrders />
+        )
+      }
     </OrdersListQuery>
   </Box>
 );
