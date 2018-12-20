10c96d4a843aa20dafa29e0b695c29b0f8da42a9
tutok <walas.marcin@gmail.com>
2018-12-20 12:04:55 +0100

    feat(account-pages): displaying addresses in address book

--- examples/shop-with-blog/client/src/pages/shop/Account/AddressBook.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/AddressBook.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import { H1, Box } from '@deity/falcon-ui';
+import { T } from '@deity/falcon-i18n';
+import { H1, H2, Box, Divider } from '@deity/falcon-ui';
+import {
+  AddressesListQuery,
+  AddressCardLayout,
+  AddressDetails,
+  AddressesListLayout
+} from '@deity/falcon-ecommerce-uikit';
 
 const AddressBook = () => (
   <Box>
-    <H1>Address Book</H1>
+    <H1>
+      <T id="addressBook.title" />
+    </H1>
+    <AddressesListQuery>
+      {({ addresses: { items } }) => {
+        const defaultBilling = items.find(x => x.defaultBilling) || {};
+        const defaultShipping = items.find(x => x.defaultShipping) || {};
+        const restAddresses = items.filter(x => !x.defaultBilling && !x.defaultShipping) || [];
+
+        return (
+          <>
+            {(defaultBilling || defaultShipping) && (
+              <>
+                <AddressesListLayout>
+                  {defaultBilling && (
+                    <AddressCardLayout>
+                      <H2>
+                        <T id="addressBook.defaultBilling" />
+                      </H2>
+                      <AddressDetails {...defaultBilling} />
+                      <Box>Edit | Delete</Box>
+                    </AddressCardLayout>
+                  )}
+                  {defaultShipping && (
+                    <AddressCardLayout>
+                      <H2>
+                        <T id="addressBook.defaultShipping" />
+                      </H2>
+                      <AddressDetails {...defaultShipping} />
+                      <Box>Edit | Delete</Box>
+                    </AddressCardLayout>
+                  )}
+                </AddressesListLayout>
+                <Divider my="lg" />
+              </>
+            )}
+            <AddressesListLayout>
+              {restAddresses.map(x => (
+                <AddressCardLayout>
+                  <AddressDetails {...x} />
+                  <Box>Edit | Delete</Box>
+                </AddressCardLayout>
+              ))}
+            </AddressesListLayout>
+          </>
+        );
+      }}
+    </AddressesListQuery>
   </Box>
 );
 
