33f71ab79c33a5e810655c7b7cb269d6cf7cdb63
tutok <walas.marcin@gmail.com>
2018-12-27 17:28:38 +0100

    feat(account-pages): simplified AddressPicker component

--- examples/shop-with-blog/client/src/pages/shop/Checkout/AddressPicker.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/AddressPicker.js
@@ -1,34 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, Dropdown, DropdownLabel, DropdownMenu, DropdownMenuItem } from '@deity/falcon-ui';
+import { Dropdown, DropdownLabel, DropdownMenu, DropdownMenuItem } from '@deity/falcon-ui';
+import { addressToString } from '@deity/falcon-ecommerce-uikit';
 
-const renderAddress = address => (
-  <Text>
-    {address.firstname}, {address.lastname}, {address.street}, {address.city}, {address.telephone}
-  </Text>
-);
-
-const renderAddressById = (items, id) => {
-  const addr = items.find(item => item.id === id);
-  if (addr) {
-    return renderAddress(addr);
-  }
-  return <Text>Other</Text>;
+const AddressPicker = ({ addresses, selectedAddressId, onChange }) => {
+  const selected = addresses.find(item => item.id === selectedAddressId);
+  return (
+    <Dropdown key={selectedAddressId || 'none'} onChange={onChange}>
+      <DropdownLabel>{selected ? addressToString(selected) : 'Other'}</DropdownLabel>
+      <DropdownMenu>
+        {addresses.map(item => (
+          <DropdownMenuItem value={item.id} key={item.id}>
+            {addressToString(item)}
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuItem value={-1}>Other</DropdownMenuItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
 };
-
-const AddressPicker = ({ addresses, selectedAddressId, onChange }) => (
-  <Dropdown key={selectedAddressId || 'none'} onChange={onChange}>
-    <DropdownLabel>{renderAddressById(addresses, selectedAddressId)}</DropdownLabel>
-    <DropdownMenu>
-      {addresses.map(item => (
-        <DropdownMenuItem value={item.id} key={item.id}>
-          {renderAddress(item)}
-        </DropdownMenuItem>
-      ))}
-      <DropdownMenuItem value={-1}>Other</DropdownMenuItem>
-    </DropdownMenu>
-  </Dropdown>
-);
 
 AddressPicker.propTypes = {
   addresses: PropTypes.arrayOf(PropTypes.shape({})),
