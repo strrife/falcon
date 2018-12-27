d4cc47b33b7313b0b09cddd2bf71173f4d518dba
tutok <walas.marcin@gmail.com>
2018-12-27 12:52:15 +0100

    feat(account-pages): improved way of displaying info about editing address

--- examples/shop-with-blog/client/src/pages/shop/Account/EditAddress.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/EditAddress.js
@@ -19,160 +19,169 @@ const EditAddress = ({ match, history }) => {
 
   return (
     <AddressQuery variables={{ id }}>
-      {({ address }) => {
-        const titleContext =
-          (address.defaultBilling && 'billing') || (address.defaultBilling && 'shipping') || undefined;
+      {({ address }) => (
+        <Box>
+          <H1>
+            <T id="editAddress.title" />
+          </H1>
 
-        return (
-          <Box>
-            <H1>
-              <T id="editAddress.title" context={titleContext} />
-            </H1>
-            <EditAddressMutation>
-              {(editAddress, { loading, error }) => (
-                <Formik
-                  initialValues={{
-                    firstname: address.firstname,
-                    lastname: address.lastname,
-                    street: address.street[0],
-                    postcode: address.postcode,
-                    city: address.city,
-                    countryId: address.countryId,
-                    company: address.company,
-                    telephone: address.telephone,
-                    defaultBilling: address.defaultBilling,
-                    defaultShipping: address.defaultShipping
-                  }}
-                  onSubmit={values =>
-                    editAddress({
-                      variables: {
-                        input: {
-                          id,
-                          firstname: values.firstname,
-                          lastname: values.lastname,
-                          street: [values.street],
-                          postcode: values.postcode,
-                          city: values.city,
-                          countryId: values.countryId,
-                          company: values.company,
-                          telephone: values.telephone,
-                          defaultBilling: values.defaultBilling,
-                          defaultShipping: values.defaultShipping
-                        }
+          {(address.defaultBilling || address.defaultShipping) && (
+            <Text>
+              <T
+                id="editAddress.defaultAddressLabel"
+                context={[
+                  address.defaultBilling ? 'billing' : undefined,
+                  address.defaultShipping ? 'shipping' : undefined
+                ]
+                  .filter(x => x)
+                  .join('&')}
+              />
+            </Text>
+          )}
+          <EditAddressMutation>
+            {(editAddress, { loading, error }) => (
+              <Formik
+                initialValues={{
+                  firstname: address.firstname,
+                  lastname: address.lastname,
+                  street: address.street[0],
+                  postcode: address.postcode,
+                  city: address.city,
+                  countryId: address.countryId,
+                  company: address.company,
+                  telephone: address.telephone,
+                  defaultBilling: address.defaultBilling,
+                  defaultShipping: address.defaultShipping
+                }}
+                onSubmit={values =>
+                  editAddress({
+                    variables: {
+                      input: {
+                        id,
+                        firstname: values.firstname,
+                        lastname: values.lastname,
+                        street: [values.street],
+                        postcode: values.postcode,
+                        city: values.city,
+                        countryId: values.countryId,
+                        company: values.company,
+                        telephone: values.telephone,
+                        defaultBilling: values.defaultBilling,
+                        defaultShipping: values.defaultShipping
                       }
-                    }).then(() => history.push('/account/address-book'))
-                  }
-                >
-                  {() => (
-                    <Form id={id} i18nId="editAddress" defaultTheme={addressFormLayout}>
-                      <FormField name="company" gridArea={AddressFormLayoutArea.company} />
-                      <FormField name="firstname" required gridArea={AddressFormLayoutArea.firstName} />
-                      <FormField name="lastname" required gridArea={AddressFormLayoutArea.lastname} />
-                      <FormField name="telephone" gridArea={AddressFormLayoutArea.telephone} />
+                    }
+                  }).then(() => history.push('/account/address-book'))
+                }
+              >
+                {() => (
+                  <Form id={id} i18nId="editAddress" defaultTheme={addressFormLayout}>
+                    <FormField name="company" gridArea={AddressFormLayoutArea.company} />
+                    <FormField name="firstname" required gridArea={AddressFormLayoutArea.firstName} />
+                    <FormField name="lastname" required gridArea={AddressFormLayoutArea.lastname} />
+                    <FormField name="telephone" gridArea={AddressFormLayoutArea.telephone} />
 
-                      <FormField name="street" required gridArea={AddressFormLayoutArea.street} />
-                      <FormField name="postcode" required gridArea={AddressFormLayoutArea.postcode} />
-                      <FormField name="city" required gridArea={AddressFormLayoutArea.city} />
-                      <Field
-                        name="countryId"
-                        render={({ field, form }) => (
-                          <Box gridArea={AddressFormLayoutArea.country}>
-                            <Label htmlFor={`${id}-${field.name}`}>Country *</Label>
-                            <CountriesQuery passLoading>
-                              {({ countries = { items: [] } }) => (
-                                <CountrySelector
-                                  items={countries.items}
-                                  value={field.value}
-                                  onChange={x => form.setFieldValue(field.name, x)}
-                                />
-                              )}
-                            </CountriesQuery>
-                            <ErrorMessage
-                              name={field.name}
-                              render={msg => (
-                                <Text fontSize="xxs" color="error">
-                                  {msg}
-                                </Text>
-                              )}
-                            />
-                          </Box>
-                        )}
-                      />
-
-                      <Box
-                        gridArea={AddressFormLayoutArea.address}
-                        display="grid"
-                        gridGap="sm"
-                        css={{ alignContent: 'start' }}
-                      >
-                        {address.defaultBilling === false && (
-                          <Field
-                            name="defaultBilling"
-                            render={({ field, form }) => (
-                              <Box>
-                                <FlexLayout mb="md">
-                                  <Checkbox
-                                    id={`${id}-${field.name}`}
-                                    size="sm"
-                                    checked={field.value}
-                                    onChange={e => form.setFieldValue(field.name, e.target.checked)}
-                                  />
-                                  <Label htmlFor={`${id}-${field.name}`}>defaultBilling</Label>
-                                </FlexLayout>
-                                <ErrorMessage
-                                  name={field.name}
-                                  render={msg => (
-                                    <Text fontSize="xxs" color="error">
-                                      {msg}
-                                    </Text>
-                                  )}
-                                />
-                              </Box>
+                    <FormField name="street" required gridArea={AddressFormLayoutArea.street} />
+                    <FormField name="postcode" required gridArea={AddressFormLayoutArea.postcode} />
+                    <FormField name="city" required gridArea={AddressFormLayoutArea.city} />
+                    <Field
+                      name="countryId"
+                      render={({ field, form }) => (
+                        <Box gridArea={AddressFormLayoutArea.country}>
+                          <Label htmlFor={`${id}-${field.name}`}>Country *</Label>
+                          <CountriesQuery passLoading>
+                            {({ countries = { items: [] } }) => (
+                              <CountrySelector
+                                items={countries.items}
+                                value={field.value}
+                                onChange={x => form.setFieldValue(field.name, x)}
+                              />
+                            )}
+                          </CountriesQuery>
+                          <ErrorMessage
+                            name={field.name}
+                            render={msg => (
+                              <Text fontSize="xxs" color="error">
+                                {msg}
+                              </Text>
                             )}
                           />
-                        )}
-                        {address.defaultShipping === false && (
-                          <Field
-                            name="defaultShipping"
-                            render={({ field, form }) => (
-                              <Box>
-                                <FlexLayout mb="md">
-                                  <Checkbox
-                                    id={`${id}-${field.name}`}
-                                    size="sm"
-                                    checked={field.value}
-                                    onChange={e => form.setFieldValue(field.name, e.target.checked)}
-                                  />
-                                  <Label htmlFor={`${id}-${field.name}`}>defaultShipping</Label>
-                                </FlexLayout>
-                                <ErrorMessage
-                                  name={field.name}
-                                  render={msg => (
-                                    <Text fontSize="xxs" color="error">
-                                      {msg}
-                                    </Text>
-                                  )}
-                                />
-                              </Box>
-                            )}
-                          />
-                        )}
-                      </Box>
+                        </Box>
+                      )}
+                    />
 
-                      <FlexLayout justifyContent="space-between" alignItems="center" mt="md">
-                        <Button type="submit" variant={loading ? 'loader' : undefined}>
-                          <T id="editAddress.submitButton" />
-                        </Button>
-                      </FlexLayout>
-                      <FormErrorSummary errors={error && [error.message]} />
-                    </Form>
-                  )}
-                </Formik>
-              )}
-            </EditAddressMutation>
-          </Box>
-        );
-      }}
+                    <Box
+                      gridArea={AddressFormLayoutArea.address}
+                      display="grid"
+                      gridGap="sm"
+                      css={{ alignContent: 'start' }}
+                    >
+                      {address.defaultBilling === false && (
+                        <Field
+                          name="defaultBilling"
+                          render={({ field, form }) => (
+                            <Box>
+                              <FlexLayout mb="md">
+                                <Checkbox
+                                  id={`${id}-${field.name}`}
+                                  size="sm"
+                                  checked={field.value}
+                                  onChange={e => form.setFieldValue(field.name, e.target.checked)}
+                                />
+                                <Label htmlFor={`${id}-${field.name}`}>defaultBilling</Label>
+                              </FlexLayout>
+                              <ErrorMessage
+                                name={field.name}
+                                render={msg => (
+                                  <Text fontSize="xxs" color="error">
+                                    {msg}
+                                  </Text>
+                                )}
+                              />
+                            </Box>
+                          )}
+                        />
+                      )}
+                      {address.defaultShipping === false && (
+                        <Field
+                          name="defaultShipping"
+                          render={({ field, form }) => (
+                            <Box>
+                              <FlexLayout mb="md">
+                                <Checkbox
+                                  id={`${id}-${field.name}`}
+                                  size="sm"
+                                  checked={field.value}
+                                  onChange={e => form.setFieldValue(field.name, e.target.checked)}
+                                />
+                                <Label htmlFor={`${id}-${field.name}`}>defaultShipping</Label>
+                              </FlexLayout>
+                              <ErrorMessage
+                                name={field.name}
+                                render={msg => (
+                                  <Text fontSize="xxs" color="error">
+                                    {msg}
+                                  </Text>
+                                )}
+                              />
+                            </Box>
+                          )}
+                        />
+                      )}
+                    </Box>
+
+                    <FlexLayout justifyContent="space-between" alignItems="center" mt="md">
+                      <Button type="submit" variant={loading ? 'loader' : undefined}>
+                        <T id="editAddress.submitButton" />
+                      </Button>
+                    </FlexLayout>
+                    <FormErrorSummary errors={error && [error.message]} />
+                  </Form>
+                )}
+              </Formik>
+            )}
+          </EditAddressMutation>
+        </Box>
+      )}
     </AddressQuery>
   );
 };
