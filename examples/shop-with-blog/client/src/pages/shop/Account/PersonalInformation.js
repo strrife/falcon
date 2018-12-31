23edfa829584d118ee256348d5097aa92c4e6028
tutok <walas.marcin@gmail.com>
2018-12-31 11:15:56 +0100

    feat(account-pages): added edit customer form on Personal Information

--- examples/shop-with-blog/client/src/pages/shop/Account/PersonalInformation.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/PersonalInformation.js
@@ -1,9 +1,53 @@
 import React from 'react';
-import { H1, Box } from '@deity/falcon-ui';
+import { Formik } from 'formik';
+import { T } from '@deity/falcon-i18n';
+import { H1, Box, FlexLayout, Button } from '@deity/falcon-ui';
+import { FormField, Form, FormErrorSummary, CustomerQuery, EditCustomerMutation } from '@deity/falcon-ecommerce-uikit';
 
 const PersonalInformation = () => (
   <Box>
     <H1>Personal Information</H1>
+    <CustomerQuery>
+      {({ customer }) => (
+        <EditCustomerMutation>
+          {(editCustomer, { loading, error }) => (
+            <Formik
+              initialValues={{
+                firstname: customer.firstname,
+                lastname: customer.lastname,
+                email: customer.email
+              }}
+              onSubmit={values =>
+                editCustomer({
+                  variables: {
+                    input: {
+                      websiteId: customer.websiteId,
+                      firstname: values.firstname,
+                      lastname: values.lastname,
+                      email: values.email
+                    }
+                  }
+                })
+              }
+            >
+              {() => (
+                <Form id="edit-customer" i18nId="editCustomer">
+                  <FormField name="firstname" />
+                  <FormField name="lastname" />
+                  <FormField name="email" />
+                  <FlexLayout justifyContent="space-between" alignItems="center" mt="md">
+                    <Button type="submit" variant={loading ? 'loader' : undefined}>
+                      <T id="editCustomer.submitButton" />
+                    </Button>
+                  </FlexLayout>
+                  <FormErrorSummary errors={error && [error.message]} />
+                </Form>
+              )}
+            </Formik>
+          )}
+        </EditCustomerMutation>
+      )}
+    </CustomerQuery>
   </Box>
 );
 
