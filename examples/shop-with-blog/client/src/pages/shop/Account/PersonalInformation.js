65d8b4b9c207054c69b22a3119b664843345445b
tutok <walas.marcin@gmail.com>
2018-12-31 14:57:46 +0100

    feat(account-pages): added personal information page

--- examples/shop-with-blog/client/src/pages/shop/Account/PersonalInformation.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/PersonalInformation.js
@@ -1,53 +1,64 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { T } from '@deity/falcon-i18n';
-import { H1, Box, FlexLayout, Button } from '@deity/falcon-ui';
-import { FormField, Form, FormErrorSummary, CustomerQuery, EditCustomerMutation } from '@deity/falcon-ecommerce-uikit';
+import { H1, Box, FlexLayout, GridLayout, Button } from '@deity/falcon-ui';
+import {
+  FormField,
+  Form,
+  FormErrorSummary,
+  CustomerQuery,
+  EditCustomerMutation,
+  TwoColumnsLayout,
+  TwoColumnsLayoutArea
+} from '@deity/falcon-ecommerce-uikit';
 
 const PersonalInformation = () => (
   <Box>
     <H1>Personal Information</H1>
-    <CustomerQuery>
-      {({ customer }) => (
-        <EditCustomerMutation>
-          {(editCustomer, { loading, error }) => (
-            <Formik
-              initialValues={{
-                firstname: customer.firstname,
-                lastname: customer.lastname,
-                email: customer.email
-              }}
-              onSubmit={values =>
-                editCustomer({
-                  variables: {
-                    input: {
-                      websiteId: customer.websiteId,
-                      firstname: values.firstname,
-                      lastname: values.lastname,
-                      email: values.email
+    <TwoColumnsLayout mt="lg">
+      <CustomerQuery>
+        {({ customer }) => (
+          <EditCustomerMutation>
+            {(editCustomer, { loading, error }) => (
+              <Formik
+                initialValues={{
+                  firstname: customer.firstname,
+                  lastname: customer.lastname,
+                  email: customer.email
+                }}
+                onSubmit={values =>
+                  editCustomer({
+                    variables: {
+                      input: {
+                        websiteId: customer.websiteId,
+                        firstname: values.firstname,
+                        lastname: values.lastname,
+                        email: values.email
+                      }
                     }
-                  }
-                })
-              }
-            >
-              {() => (
-                <Form id="edit-customer" i18nId="editCustomer">
-                  <FormField name="firstname" />
-                  <FormField name="lastname" />
-                  <FormField name="email" />
-                  <FlexLayout justifyContent="space-between" alignItems="center" mt="md">
-                    <Button type="submit" variant={loading ? 'loader' : undefined}>
-                      <T id="editCustomer.submitButton" />
-                    </Button>
-                  </FlexLayout>
-                  <FormErrorSummary errors={error && [error.message]} />
-                </Form>
-              )}
-            </Formik>
-          )}
-        </EditCustomerMutation>
-      )}
-    </CustomerQuery>
+                  })
+                }
+              >
+                {() => (
+                  <GridLayout as={Form} id="edit-customer" i18nId="editCustomer" gridArea={TwoColumnsLayoutArea.left}>
+                    <FormField name="firstname" />
+                    <FormField name="lastname" />
+                    <FormField name="email" />
+                    <FlexLayout justifyContent="space-between" alignItems="center" mt="md">
+                      <T id="editCustomer.changePassword" />
+                      <Button type="submit" variant={loading ? 'loader' : undefined}>
+                        <T id="editCustomer.submitButton" />
+                      </Button>
+                    </FlexLayout>
+                    <FormErrorSummary errors={error && [error.message]} />
+                  </GridLayout>
+                )}
+              </Formik>
+            )}
+          </EditCustomerMutation>
+        )}
+      </CustomerQuery>
+    </TwoColumnsLayout>
   </Box>
 );
 
