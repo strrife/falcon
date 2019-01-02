cd45105fdc537bd404df4a61f792ed9605bc2778
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2019-01-02 11:18:46 +0100

    wip

--- examples/shop-with-blog/server/src/MagentoSearch.js
+++ examples/shop-with-blog/server/src/MagentoSearch.js
@@ -8,7 +8,8 @@ const {
   // delegateToSchema,
   // FilterRootFields,
   RenameTypes,
-  RenameRootFields
+  RenameRootFields,
+  WrapQuery
 } = require('graphql-tools');
 
 class MagentoSearch extends Extension {
@@ -33,6 +34,7 @@ class MagentoSearch extends Extension {
       resolvers: {
         Query: {
           async myStoreConfig(parent, params, context, info) {
+            debugger;
             const result = await info.mergeInfo.delegateToSchema({
               // schema: remoteSchema,
               schema: transformedSchema,
@@ -46,28 +48,62 @@ class MagentoSearch extends Extension {
             return result;
           },
 
+          // async search2(parent, params, context, info) {
+          //   // console.log('-------------> search resolver', info);
+          //   debugger;
+          //   const result = await info.mergeInfo.delegateToSchema({
+          //     // schema: remoteSchema,
+          //     schema: transformedSchema,
+          //     operation: 'query',
+          //     fieldName: 'Magento_products',
+          //     // fieldName: 'products',
+          //     args: {
+          //       search: params.query
+          //     },
+          //     context,
+          //     info
+          //     // transforms: [
+          //     //   new RenameTypes(name => `Magento_${name}`),
+          //     //   new RenameRootFields((operation, name) => `Magento_${name}`)
+          //     // ]
+          //   });
+
+          //   console.log('result', result);
+          //   return result;
+          // },
+
           async search(parent, params, context, info) {
             // console.log('-------------> search resolver', info);
             debugger;
-            const result = await info.mergeInfo
-              .delegateToSchema({
-                // schema: remoteSchema,
-                schema: transformedSchema,
-                operation: 'query',
-                fieldName: 'Magento_products',
-                // fieldName: 'products',
-                args: {
-                  search: params.query
-                },
-                context,
-                info
-                // transforms: [
+            const result = await info.mergeInfo.delegateToSchema({
+              // schema: remoteSchema,
+              schema: transformedSchema,
+              operation: 'query',
+              fieldName: 'Magento_products',
+              // fieldName: 'products',
+              args: {
+                search: params.query
+              },
+              context,
+              info,
+              transforms: [
+                new WrapQuery(
+                  ['Magento_products'],
+                  subtree => {
+                    debugger;
+                    return subtree.selections[0].selectionSet;
+                  },
+                  res => {
+                    debugger;
+                    return {
+                      products: res
+                    };
+                  }
+                )
                 //   new RenameTypes(name => `Magento_${name}`),
                 //   new RenameRootFields((operation, name) => `Magento_${name}`)
-                // ]
-              })
-              .then(res => console.log('res', res), rej => console.log('rej', rej))
-              .catch(ex => console.log('ex', ex));
+              ]
+            });
 
             console.log('result', result);
             return result;
