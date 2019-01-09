e2f4b0d6c655de3e51bda6e1aeb96607e87cd971
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2019-01-09 17:21:14 +0100

    feat: bind sort options so category listing works correctly when user changes sort options

--- examples/shop-with-blog/client/src/pages/shop/Category.js
+++ examples/shop-with-blog/client/src/pages/shop/Category.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import gql from 'graphql-tag';
-import { CategoryProductsQuery, Category, Query } from '@deity/falcon-ecommerce-uikit';
-
-const sortOrdersQuery = gql`
-  query {
-    sortOrders @client
-  }
-`;
-
-class SortOrdersProvider extends React.Component {
-  state = {
-    active: null
-  };
-
-  setSortOrder = active => this.setState({ active });
-
-  render() {
-    return (
-      <Query query={sortOrdersQuery}>
-        {({ sortOrders }) =>
-          this.props.children({
-            availableSortOrders: sortOrders,
-            activeSortOrder: this.state.active || sortOrders[0],
-            setSortOrder: this.setSortOrder
-          })
-        }
-      </Query>
-    );
-  }
-}
+import { SortOrdersProvider, CategoryProductsQuery, Category } from '@deity/falcon-ecommerce-uikit';
 
 const CategoryPage = ({ id }) => (
   <SortOrdersProvider>
