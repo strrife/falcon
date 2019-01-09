ac80d2a19fc6ed51782f1a7e916ff7ae29d968b6
tutok <walas.marcin@gmail.com>
2019-01-09 14:31:52 +0100

    feat(falcon-scss): passing options into plugins

--- packages/falcon-client/build-utils/webpack/config/runPlugin.js
+++ packages/falcon-client/build-utils/webpack/config/runPlugin.js
@@ -1,16 +1,18 @@
-function runPlugin(plugin, config, { target, dev }, webpack) {
+// / { target, dev }
+
+function runPlugin(plugin, config, options, webpack) {
   if (typeof plugin === 'string') {
     // Apply the plugin with default options if passing only a string
-    return runPlugin({ name: plugin }, config, { target, dev }, webpack);
+    return runPlugin({ name: plugin }, config, options, webpack);
   }
 
   if (typeof plugin === 'function') {
-    return plugin(config, { target, dev }, webpack);
+    return plugin(config, options, webpack);
   }
 
   if (typeof plugin.func === 'function') {
     // Used for writing plugin tests
-    return plugin.func(config, { target, dev }, webpack, plugin.options);
+    return plugin.func(config, options, webpack, plugin.options);
   }
 
   const completePluginName = `razzle-plugin-${plugin.name}`;
@@ -22,7 +24,7 @@ function runPlugin(plugin, config, { target, dev }, webpack) {
     throw new Error(`Unable to find '${completePluginName}`);
   }
 
-  return razzlePlugin(config, { target, dev }, webpack, plugin.options);
+  return razzlePlugin(config, options, webpack, plugin.options);
 }
 
 module.exports = runPlugin;
