cdafdc48e8eee0945e13b1e9eeee04cc771fdcf5
tutok <walas.marcin@gmail.com>
2019-01-10 10:56:14 +0100

    feat(falcon-scss): updated minicssextractplugin coniguration according to CRA

--- packages/falcon-client/build-utils/webpack/config/create.js
+++ packages/falcon-client/build-utils/webpack/config/create.js
@@ -472,11 +472,8 @@ module.exports = (target = 'web', options, buildConfig) => {
         new webpack.DefinePlugin(clientEnv.stringified),
         // Extract our CSS into a files.
         new MiniCssExtractPlugin({
-          filename: 'static/css/bundle.[contenthash:8].css',
-          // allChunks: true because we want all css to be included in the main
-          // css bundle when doing code splitting to avoid FOUC:
-          // https://github.com/facebook/create-react-app/issues/2415
-          allChunks: true
+          filename: 'static/css/[name].[contenthash:8].css',
+          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
         }),
         new webpack.HashedModuleIdsPlugin(),
         new webpack.optimize.AggressiveMergingPlugin()
