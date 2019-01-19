f00130465c9d1c18db3ef01bc534dd1c73c9a5c5
Oleksandr Bordun <bordun.alexandr@gmail.com>
2019-01-19 11:43:28 +0100

    feature(example/client): showing PaymentSelector

--- examples/shop-with-blog/client/src/pages/shop/Checkout/PaymentMethodSection.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/PaymentMethodSection.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, FlexLayout, Label, Details, DetailsContent, Text, Radio, Button } from '@deity/falcon-ui';
+import { Details, DetailsContent, Text, Button } from '@deity/falcon-ui';
+import loadable from 'src/components/loadable';
 import SectionHeader from './CheckoutSectionHeader';
 import ErrorList from '../components/ErrorList';
 
-// we have to filter the methods until we have implementation for all of them
-const ALLOWED_PAYMENT_METHODS = ['checkmo'];
-
-const PaymentSelector = ({ availablePaymentMethods = [], onPaymentSelected }) => {
-  const paymentMethods = availablePaymentMethods.filter(option => ALLOWED_PAYMENT_METHODS.includes(option.code));
-
-  return (
-    <Box my="md">
-      {paymentMethods.map(x => (
-        <FlexLayout key={x.code}>
-          <Radio size="sm" name="payment" id={`opt-${x.code}`} value={x.code} onChange={() => onPaymentSelected(x)} />
-          <Label mx="sm" flex="1" htmlFor={`opt-${x.code}`}>
-            {x.title}
-          </Label>
-        </FlexLayout>
-      ))}
-    </Box>
-  );
-};
-
-PaymentSelector.propTypes = {
-  availablePaymentMethods: PropTypes.arrayOf(PropTypes.shape({})),
-  onPaymentSelected: PropTypes.func
-};
+const PaymentSelector = loadable(() => import(/* webpackChunkName: "shop/payments" */ './PaymentSelector'));
 
 class PaymentSection extends React.Component {
   state = {
@@ -41,7 +19,7 @@ class PaymentSection extends React.Component {
   };
 
   render() {
-    const { open, selectedPayment, onEditRequested, availablePaymentMethods, errors } = this.props;
+    const { addressInput, open, selectedPayment, onEditRequested, availablePaymentMethods, errors } = this.props;
     let header;
     if (!open && selectedPayment) {
       header = (
@@ -61,10 +39,13 @@ class PaymentSection extends React.Component {
       <Details open={open}>
         {header}
         <DetailsContent>
-          <PaymentSelector
-            availablePaymentMethods={availablePaymentMethods}
-            onPaymentSelected={this.onPaymentSelected}
-          />
+          {availablePaymentMethods.length && (
+            <PaymentSelector
+              addressInput={addressInput}
+              availableMethods={availablePaymentMethods}
+              onPaymentSelected={this.onPaymentSelected}
+            />
+          )}
           <ErrorList errors={errors} />
           <Button disabled={!this.state.selectedPayment} onClick={this.submitPayment}>
             Continue
@@ -76,6 +57,8 @@ class PaymentSection extends React.Component {
 }
 
 PaymentSection.propTypes = {
+  // Billing address form
+  addressInput: PropTypes.element.isRequired,
   // flag that indicates if the section is currently open
   open: PropTypes.bool,
   // all available payment methods
