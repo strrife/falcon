8819029e357013b5807556970381ef1c2fb75406
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2019-01-22 01:12:21 +0100

    fix: prevent sidebar from animation between sides when "side" property changes (this is temporary workaround, we need to find a better fix for that)

--- examples/shop-with-blog/client/src/pages/shop/components/Sidebar/Sidebar.js
+++ examples/shop-with-blog/client/src/pages/shop/components/Sidebar/Sidebar.js
@@ -3,17 +3,36 @@ import PropTypes from 'prop-types';
 import { Loader } from '@deity/falcon-ecommerce-uikit';
 import { Sidebar as SidebarLayout, Portal, Backdrop, Icon, Box } from '@deity/falcon-ui';
 
-export const Sidebar = ({ ready, close, isOpen, side, children }) => (
-  <React.Fragment>
-    <SidebarLayout as={Portal} visible={isOpen} side={side}>
-      <Box position="relative" flex={1}>
-        <Icon src="close" stroke="black" position="absolute" right={0} top={0} onClick={close} />
-        {ready ? children() : <Loader />}
-      </Box>
-    </SidebarLayout>
-    <Backdrop as={Portal} visible={isOpen} onClick={close} />
-  </React.Fragment>
-);
+export class Sidebar extends React.Component {
+  state = {
+    initialized: false
+  };
+
+  componentDidMount() {
+    // set initialized to true after 500ms so sidebars do not flash when mounting
+    setTimeout(() => this.setState({ initialized: true }), 500);
+  }
+
+  render() {
+    const { ready, close, isOpen, children, side } = this.props;
+    return (
+      <React.Fragment>
+        <SidebarLayout
+          as={Portal}
+          visible={isOpen}
+          side={side}
+          css={{ visibility: this.state.initialized ? 'visible' : 'hidden' }}
+        >
+          <Box position="relative" flex={1}>
+            <Icon src="close" stroke="black" position="absolute" right={0} top={0} onClick={close} />
+            {ready ? children() : <Loader />}
+          </Box>
+        </SidebarLayout>
+        <Backdrop as={Portal} visible={isOpen} onClick={close} />
+      </React.Fragment>
+    );
+  }
+}
 
 Sidebar.propTypes = {
   ready: PropTypes.bool.isRequired,
