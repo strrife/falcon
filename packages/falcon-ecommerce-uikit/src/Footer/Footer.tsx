5fb603bffa95f1db06437190d8fe11183f85dc11
tutok <walas.marcin@gmail.com>
2019-01-24 16:55:18 +0100

    feat(dynamic-menu): reworked footer

--- packages/falcon-ecommerce-uikit/src/Footer/Footer.tsx
+++ packages/falcon-ecommerce-uikit/src/Footer/Footer.tsx
@@ -5,7 +5,6 @@ import { T, I18n } from '@deity/falcon-i18n';
 import { LanguageSwitcher } from './LanguageSwitcher';
 import { FooterData } from './FooterQuery';
 import { Newsletter } from './Newsletter';
-import { MenuItem } from '../Menu';
 
 export const CopyrightLayout = themed({
   tag: Box,
@@ -21,39 +20,104 @@ export const CopyrightLayout = themed({
   }
 });
 
-const footerSectionsTheme: DefaultThemeProps = {
-  footerSectionLayout: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit,minmax(200px,1fr))',
-    gridGap: 'sm',
-    bgFullWidth: 'secondaryLight',
-    py: 'sm',
-    css: {
-      justifyItems: {
-        xs: 'center',
-        md: 'center'
+export const FooterSectionsLayout = themed({
+  tag: Box,
+  defaultTheme: {
+    footerSectionsLayout: {
+      display: 'flex',
+      bgFullWidth: 'secondaryLight',
+      css: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        justifyItems: 'center',
+        flexWrap: 'wrap'
       }
     }
   }
-};
+});
 
-export const FooterSections: React.SFC<{ sections: MenuItem[] }> = ({ sections }) => (
-  <Box defaultTheme={footerSectionsTheme}>
-    {sections.map(section => (
-      <Box key={section.name} css={{ minWidth: 200 }}>
-        <H4 fontWeight="bold">{section.name}</H4>
-        <List>
-          {section.children.map(item => (
-            <ListItem p="xs" key={item.name}>
-              <Link as={RouterLink} to={item.urlPath}>
-                {item.name}
-              </Link>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    ))}
-  </Box>
+export const FooterSectionLayout = themed({
+  tag: Box,
+  defaultTheme: {
+    footerSection: {
+      p: 'md',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: {
+        md: 'flex-start',
+        xs: 'flex-start'
+      },
+      css: {
+        minWidth: 250,
+        textAlign: {
+          md: 'unset',
+          xs: 'center'
+        },
+        alignItems: {
+          md: 'unset',
+          xs: 'center'
+        }
+      }
+    }
+  }
+});
+
+export const SitemapLink: React.SFC<{ to: string }> = ({ to, children }) => (
+  <ListItem p="xs">
+    <Link as={RouterLink} to={to}>
+      {children}
+    </Link>
+  </ListItem>
+);
+
+export const Sitemap: React.SFC = () => (
+  <FooterSectionsLayout>
+    <FooterSectionLayout>
+      <H4 fontWeight="bold">
+        <T id="sitemap.customerService" />
+      </H4>
+      <List>
+        <SitemapLink to="/">
+          <T id="sitemap.trackOrderLink" />
+        </SitemapLink>
+        <SitemapLink to="/">
+          <T id="sitemap.returnPolicyLink" />
+        </SitemapLink>
+        <SitemapLink to="/">
+          <T id="sitemap.faqsLink" />
+        </SitemapLink>
+      </List>
+    </FooterSectionLayout>
+    <FooterSectionLayout>
+      <H4 fontWeight="bold">
+        <T id="sitemap.aboutUs" />
+      </H4>
+      <List>
+        <SitemapLink to="/">
+          <T id="sitemap.aboutUsLink" />
+        </SitemapLink>
+        <SitemapLink to="/blog">
+          <T id="sitemap.blogLink" />
+        </SitemapLink>
+        <SitemapLink to="/">
+          <T id="sitemap.jobsLink" />
+        </SitemapLink>
+      </List>
+    </FooterSectionLayout>
+    <FooterSectionLayout>
+      <H4 fontWeight="bold">
+        <T id="sitemap.terms" />
+      </H4>
+      <List>
+        <SitemapLink to="/">
+          <T id="sitemap.cookiesLink" />
+        </SitemapLink>
+        <SitemapLink to="/">
+          <T id="sitemap.termsLink" />
+        </SitemapLink>
+      </List>
+    </FooterSectionLayout>
+  </FooterSectionsLayout>
 );
 
 const languageSectionTheme: DefaultThemeProps = {
@@ -78,15 +142,10 @@ export const FooterLayout = themed({
   }
 });
 
-export const Footer: React.SFC<FooterData> = ({
-  config: {
-    menus: { footer },
-    languages
-  }
-}) => (
+export const Footer: React.SFC<FooterData> = ({ config: { languages } }) => (
   <FooterLayout as="footer">
     <Newsletter />
-    <FooterSections sections={footer} />
+    <Sitemap />
     <Box defaultTheme={languageSectionTheme}>
       <I18n>
         {(_t, i18n) => <LanguageSwitcher languages={languages} onChange={x => i18n.changeLanguage(x.code)} />}
