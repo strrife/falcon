1093b845fe69f19756d96ec80ff04e1a59309688
tutok <walas.marcin@gmail.com>
2019-02-01 14:36:49 +0100

    feat(offline): added strategy to restore apollo cache depends on intenret connection

--- packages/falcon-client/src/client.js
+++ packages/falcon-client/src/client.js
@@ -5,6 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { loadableReady } from '@loadable/component';
 import { I18nProvider } from '@deity/falcon-i18n';
+import { CachePersistor } from 'apollo-cache-persist';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import { ApolloClient, apolloStateToObject } from './service';
 import HtmlHead from './components/HtmlHead';
 import App, { clientApolloSchema } from './clientApp';
@@ -16,38 +19,66 @@ const apolloInitialState = window.__APOLLO_STATE__ || {};
 const i18nextState = window.I18NEXT_STATE || {};
 
 const config = apolloStateToObject(apolloInitialState, '$ROOT_QUERY.config');
-const apolloClient = new ApolloClient({
-  isBrowser: true,
-  clientState: clientApolloSchema,
-  initialState: apolloInitialState,
-  apolloClientConfig: config.apolloClient
+
+const cacheKey = '@deity/falcon-client/apollo-cache';
+const cache = new InMemoryCache({ addTypename: false }); // .restore(apolloInitialState);
+const persistor = new CachePersistor({
+  cache,
+  storage: window.localStorage,
+  key: cacheKey,
+  debounce: 1000,
+  debug: true
 });
 
-i18nFactory({ ...config.i18n, lng: i18nextState.language }).then(i18next => {
-  const markup = (
-    <ApolloProvider client={apolloClient}>
-      <I18nProvider i18n={i18next}>
-        <BrowserRouter>
-          <React.Fragment>
-            <HtmlHead htmlLang={i18nextState.language || config.i18n.lng} />
-            <App />
-          </React.Fragment>
-        </BrowserRouter>
-      </I18nProvider>
-    </ApolloProvider>
-  );
-  const renderApp = config.serverSideRendering ? hydrate : render;
+const restoreCache = cachePersistor => {
+  const { navigator } = window;
+  const online = navigator && navigator.onLine;
 
-  loadableReady(() => {
-    renderApp(markup, document.getElementById('root'));
-  });
-
-  if (process.env.NODE_ENV === 'production') {
-    register('/sw.js');
-  } else {
-    unregisterAll();
+  if (online) {
+    cachePersistor.purge().then(() => {
+      cache.restore(apolloInitialState);
+      return Promise.resolve();
+    });
   }
-});
+
+  return cachePersistor.restore();
+};
+
+const renderApp = config.serverSideRendering ? hydrate : render;
+
+loadableReady()
+  .then(() => restoreCache(persistor))
+  .then(() => i18nFactory({ ...config.i18n, lng: i18nextState.language }))
+  .then(i18next => {
+    const apolloClient = new ApolloClient({
+      isBrowser: true,
+      clientState: clientApolloSchema,
+      initialState: apolloInitialState,
+      apolloClientConfig: config.apolloClient,
+      cache
+    });
+
+    const markup = (
+      <ApolloProvider client={apolloClient}>
+        <I18nProvider i18n={i18next}>
+          <BrowserRouter>
+            <React.Fragment>
+              <HtmlHead htmlLang={i18nextState.language || config.i18n.lng} />
+              <App />
+            </React.Fragment>
+          </BrowserRouter>
+        </I18nProvider>
+      </ApolloProvider>
+    );
+    renderApp(markup, document.getElementById('root'));
+  })
+  .then(() => {
+    if (process.env.NODE_ENV === 'production') {
+      register('/sw.js');
+    } else {
+      unregisterAll();
+    }
+  });
 
 if (module.hot) {
   module.hot.accept();
