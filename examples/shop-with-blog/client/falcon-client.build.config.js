5612c9a3948f4a96a5b07c55ca541d16420b6bde
tutok <walas.marcin@gmail.com>
2019-03-01 10:34:03 +0100

    feat(module-replacement): cleanup

--- examples/shop-with-blog/client/falcon-client.build.config.js
+++ examples/shop-with-blog/client/falcon-client.build.config.js
@@ -4,8 +4,5 @@ module.exports = {
   i18n: {
     resourcePackages: ['@deity/falcon-i18n']
   },
-  moduleOverride: {
-    '@deity/falcon-ui/dist/components/Text': '@deity/falcon-ecommerce-uikit/dist/Footer/Text',
-    '@deity/falcon-ecommerce-uikit/dist/Footer/Text': 'src/components/RedText'
-  }
+  moduleOverride: {}
 };
