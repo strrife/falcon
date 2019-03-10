02ec2fbc4da0ee66fbddc6c88f985b9099457092
Borales <bordun.alexandr@gmail.com>
2019-03-10 23:49:02 +0100

    feat(falcon-client): proxing "/graphql" endpoint to the Falcon-Server, passing proper headers from the client

---
+++ packages/falcon-client/src/middlewares/graphqlProxyMiddleware.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+// GraphQL Proxy to Falcon-Server
+export default serverUri => async ctx => {
+  const { request, originalUrl } = ctx;
+  const { method, header } = request;
+
+  const result = await fetch(`${serverUri}${originalUrl}`, {
+    method,
+    headers: header,
+    body: method === 'POST' ? ctx.req : undefined
+  });
+
+  result.headers.forEach((value, name) => {
+    ctx.set(name, value);
+  });
+
+  // https://github.com/bitinn/node-fetch/blob/master/src/headers.js#L120
+  // node-fetch returns `set-cookie` headers concatenated with ", " (which is invalid)
+  // for this reason we get a list of "raw" headers and set them to the response
+  ctx.set('set-cookie', result.headers.raw()['set-cookie']);
+
+  ctx.status = result.status;
+  ctx.body = result.body;
+};
