7b0f20f4eafb8d33376fc667873cc380898cb07d
tutok <walas.marcin@gmail.com>
2019-03-12 13:23:25 +0100

    feat(autocomplete): improved select apperance in ie10

--- packages/falcon-ui/src/components/Select.tsx
+++ packages/falcon-ui/src/components/Select.tsx
@@ -27,7 +27,10 @@ export const Select = themed({
         background: `url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJmZWF0aGVyIGZlYXRoZXItY2hldnJvbi1kb3duIj48cG9seWxpbmUgcG9pbnRzPSI2IDkgMTIgMTUgMTggOSI+PC9wb2x5bGluZT48L3N2Zz4=) 99% 50% no-repeat;`,
         WebkitAppearance: 'none',
         MozAppearance: 'none',
-        appearance: 'none'
+        appearance: 'none',
+        '::-ms-expand': {
+          display: 'none'
+        }
       })
     }
   }
