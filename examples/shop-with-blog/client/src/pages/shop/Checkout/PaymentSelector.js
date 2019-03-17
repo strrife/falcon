33b6327f522a599cfa3dacb4edf55184716bedb1
Borales <bordun.alexandr@gmail.com>
2019-03-17 22:25:20 +0100

    chore: relocating "falcon-proxy-endpoints" package

--- examples/shop-with-blog/client/src/pages/shop/Checkout/PaymentSelector.js
+++ examples/shop-with-blog/client/src/pages/shop/Checkout/PaymentSelector.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
 import { Box, FlexLayout, Label, Radio, Image } from '@deity/falcon-ui';
-import { Loader, GET_CONFIG } from '@deity/falcon-ecommerce-uikit';
+import { ConfigQuery } from '@deity/falcon-ecommerce-uikit';
 import { Payment, PlainPayment } from '@deity/falcon-payment-plugin';
 import AdyenCCPlugin from '@deity/falcon-adyen-plugin';
 import PayPalExpressPlugin from '@deity/falcon-paypal-plugin';
@@ -18,40 +17,39 @@ const PluginTemplate = ({ creditCardInput }) => (
   </React.Fragment>
 );
 
-class PaymentSelector extends React.PureComponent {
+export default class PaymentSelector extends React.PureComponent {
   render() {
-    const { paymentsConfigQuery, availableMethods = [], onPaymentSelected } = this.props;
-    if (paymentsConfigQuery.loading) {
-      return <Loader />;
-    }
-    const { getConfig: paymentsConfig } = paymentsConfigQuery;
-
+    const { availableMethods = [], onPaymentSelected } = this.props;
     return (
-      <Box my="md">
-        <Payment
-          creditCardInput={CreditCard}
-          template={PluginTemplate}
-          onPaymentSelected={onPaymentSelected}
-          plugins={{
-            adyen_cc: AdyenCCPlugin,
-            paypal_express: PayPalExpressPlugin,
-            checkmo: PlainPayment
-          }}
-          methods={availableMethods}
-          paymentsConfig={paymentsConfig}
-        >
-          {({ code, title, onSelect, icon, pluginComponent }) => (
-            <FlexLayout key={code} my="xs" css={{ alignItems: 'center' }}>
-              <Radio size="sm" name="payment" id={`opt-${code}`} value={code} onChange={onSelect} />
-              <Label mx="sm" flex="1" htmlFor={`opt-${code}`}>
-                {icon && <Image src={icon} mr="xs" mb="xs" css={{ verticalAlign: 'middle' }} />}
-                {title}
-              </Label>
-              {pluginComponent}
-            </FlexLayout>
-          )}
-        </Payment>
-      </Box>
+      <ConfigQuery variables={{ key: 'plugins.payments' }}>
+        {({ getConfig: paymentsConfig }) => (
+          <Box my="md">
+            <Payment
+              creditCardInput={CreditCard}
+              template={PluginTemplate}
+              onPaymentSelected={onPaymentSelected}
+              plugins={{
+                adyen_cc: AdyenCCPlugin,
+                paypal_express: PayPalExpressPlugin,
+                checkmo: PlainPayment
+              }}
+              methods={availableMethods}
+              paymentsConfig={paymentsConfig}
+            >
+              {({ code, title, onSelect, icon, pluginComponent }) => (
+                <FlexLayout key={code} my="xs" css={{ alignItems: 'center' }}>
+                  <Radio size="sm" name="payment" id={`opt-${code}`} value={code} onChange={onSelect} />
+                  <Label mx="sm" flex="1" htmlFor={`opt-${code}`}>
+                    {icon && <Image src={icon} mr="xs" mb="xs" css={{ verticalAlign: 'middle' }} />}
+                    {title}
+                  </Label>
+                  {pluginComponent}
+                </FlexLayout>
+              )}
+            </Payment>
+          </Box>
+        )}
+      </ConfigQuery>
     );
   }
 }
@@ -61,12 +59,3 @@ PaymentSelector.propTypes = {
   onPaymentSelected: PropTypes.func,
   paymentsConfigQuery: PropTypes.shape({})
 };
-
-export default graphql(GET_CONFIG, {
-  name: 'paymentsConfigQuery',
-  options: {
-    variables: {
-      key: 'plugins.payments'
-    }
-  }
-})(PaymentSelector);
--- packages/falcon-proxy-endpoints/README.md
+++
@@ -1,7 +0,0 @@
-# Falcon Proxy Endpoints (Falcon-Server)
-
-## Config values
-
-- `protocol` and `host` - set your target server you want to proxy your requests to
-- `entries` - list of API REST endpoints patterns
-- `orderIdSessionPath` - object path to get an orderId from the session (default `magento2.orderId`)
