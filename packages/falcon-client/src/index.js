0e3b889584208ab0582d999138c681c0b7965991
tutok <walas.marcin@gmail.com>
2019-03-19 16:57:07 +0100

    feat(port): cleanup

--- packages/falcon-client/src/index.js
+++ packages/falcon-client/src/index.js
@@ -32,10 +32,7 @@ function falconWebServer() {
 const server = falconWebServer();
 let currentWebServerHandler = server.callback();
 
-// Use `app#callback()` method here instead of directly
-// passing `app` as an argument to `createServer` (or use `app#listen()` instead)
-// @see https://github.com/koajs/koa/blob/master/docs/api/index.md#appcallback
-const httpServer = http.createServer(currentWebServerHandler);
+const httpServer = http.createServer(currentWebServerHandler); // https://github.com/koajs/koa/blob/master/docs/api/index.md#appcallback
 httpServer.listen(server.port, error => {
   if (error) {
     Logger.error(error);
