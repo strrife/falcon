fb2c8585cab8f05fcfd8b62e4a579534d56678fe
tutok <walas.marcin@gmail.com>
2019-03-26 13:56:07 +0100

    feat(filters): updated reexport path

--- packages/falcon-ecommerce-uikit/src/Search/index.ts
+++ packages/falcon-ecommerce-uikit/src/Search/index.ts
@@ -2,4 +2,4 @@ export * from './SearchContext';
 export * from './SearchProvider';
 export * from './searchStateFromURL';
 export * from './searchStateToURL';
-export * from './types.d';
+export * from './types';
