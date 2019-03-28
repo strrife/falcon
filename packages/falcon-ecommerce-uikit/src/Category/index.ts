09ff1527cc13e6b12e3315e8dae19cdb42b34abf
tutok <walas.marcin@gmail.com>
2019-03-28 10:36:51 +0100

    feat(filters): looks like some modules were not exported

--- packages/falcon-ecommerce-uikit/src/Category/index.ts
+++ packages/falcon-ecommerce-uikit/src/Category/index.ts
@@ -2,3 +2,5 @@ export * from './CategoryProductsQuery';
 export * from './Category';
 export * from './SortOrdersQuery';
 export * from './SortOrderDropdown';
+export * from './ShowingOutOf';
+export * from './ShowMore';
