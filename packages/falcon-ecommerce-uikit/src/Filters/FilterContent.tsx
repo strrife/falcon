cb1ca4e4e0ec9ae8eddff864c05c4f9f8dce8c58
tutok <walas.marcin@gmail.com>
2019-03-29 14:56:03 +0100

    feat(filters): removed unecessary code

--- packages/falcon-ecommerce-uikit/src/Filters/FilterContent.tsx
+++ packages/falcon-ecommerce-uikit/src/Filters/FilterContent.tsx
@@ -5,49 +5,6 @@ import { FilterItem, FilterItemLayout } from './FilterItem';
 import { SelectedFilterItem } from './FilterItem';
 import { ColorTile } from './ColorTile';
 
-const filterContentTheme = {
-  filterContent: {}
-};
-
-type FilterContentProps = {
-  setFilter: (name: string, value: string[], operator?: FilterOperator) => void;
-  selected: string[];
-  aggregation: FilterData;
-  singleMode?: boolean;
-};
-
-export const FilterContent: React.SFC<FilterContentProps> = ({ aggregation, selected = [], setFilter, singleMode }) => {
-  const updateNormalFilter = (value: string) => {
-    if (singleMode) {
-      return setFilter(aggregation.field, [value]);
-    }
-
-    const nextSelected = selected;
-    if (!nextSelected.includes(value)) {
-      nextSelected.push(value);
-    }
-
-    setFilter(aggregation.field, nextSelected);
-  };
-
-  const updatePriceFilter = (value: string) => {
-    const [from, to] = value.split('-');
-    setFilter(aggregation.field, [from, to], 'range');
-  };
-
-  const updateFilter = aggregation.field === 'price' ? updatePriceFilter : updateNormalFilter;
-
-  return (
-    <Box defaultTheme={filterContentTheme}>
-      <List>
-        {aggregation.options.map(item => (
-          <FilterItem key={item.title} item={item} onClick={() => updateFilter(item.value)} />
-        ))}
-      </List>
-    </Box>
-  );
-};
-
 export const FilterItemsList = themed({
   tag: List,
   defaultTheme: {
