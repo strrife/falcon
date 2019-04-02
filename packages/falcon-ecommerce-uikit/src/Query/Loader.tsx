d475961577f1377cc0cc315f2ad396ae900d7f38
tutok <walas.marcin@gmail.com>
2019-04-02 17:55:38 +0200

    feat(filters): needs to add zindex into loader

--- packages/falcon-ecommerce-uikit/src/Query/Loader.tsx
+++ packages/falcon-ecommerce-uikit/src/Query/Loader.tsx
@@ -24,7 +24,8 @@ export const Loader = themed({
           css: {
             background: 'rgba(255, 255, 255, 0.7)',
             height: '100%',
-            width: '100%'
+            width: '100%',
+            zIndex: 1
           }
         }
       }
