17d27c63f69a017d8f64f49cecc5863ad7b2e7c9
tutok <walas.marcin@gmail.com>
2019-04-15 19:26:42 +0200

    feat(scripts): extracted esm script, latter maybe I will convert it to use babel natively

---
+++ packages/falcon-dev-tools/falcon-scripts/scripts/build-esm.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const spawn = require('cross-spawn');
+const Logger = require('@deity/falcon-logger');
+
+module.exports = async ({ packagePath }) => {
+  Logger.log('building esm...');
+
+  const babelConfigPath = path.relative(packagePath, require.resolve('../src/build-package/babel.config.js'));
+
+  const result = spawn.sync(`babel src -d dist -x .ts,.tsx -s --config-file ${babelConfigPath}`, [], {
+    stdio: 'inherit'
+  });
+
+  if (result.status !== 0) {
+    throw result;
+  }
+};
