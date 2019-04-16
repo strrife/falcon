843659fb43d345c81271ef20947c593a051cf21f
tutok <walas.marcin@gmail.com>
2019-04-16 11:57:20 +0200

    feat(scripts): need to specify absolute patch to babel config

--- packages/falcon-dev-tools/falcon-scripts/scripts/build-esm.js
+++ packages/falcon-dev-tools/falcon-scripts/scripts/build-esm.js
@@ -1,13 +1,12 @@
-const path = require('path');
 const spawn = require('cross-spawn');
 const Logger = require('@deity/falcon-logger');
 
-module.exports = async ({ packagePath }) => {
+module.exports = async () => {
   Logger.log('building esm...');
 
-  const babelConfigPath = path.relative(packagePath, require.resolve('../babel.config.js'));
+  const babelConfigPath = require.resolve('../babel.config.js');
 
-  const result = spawn.sync(`babel src -d dist -x .ts,.tsx -s --config-file ${babelConfigPath}`, [], {
+  const result = spawn.sync(`babel src -d dist -x .js,.jsx,.ts,.tsx -s --config-file ${babelConfigPath}`, [], {
     stdio: 'inherit'
   });
 
