83de1c35f06e241fe653bfb230e73034cfb5cacf
tutok <walas.marcin@gmail.com>
2019-04-16 12:26:50 +0200

    feat(scripts): used glob to retrieve index file for cjs build

--- packages/falcon-dev-tools/falcon-scripts/scripts/build-cjs.js
+++ packages/falcon-dev-tools/falcon-scripts/scripts/build-cjs.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const glob = require('glob');
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
@@ -18,9 +19,13 @@ module.exports = async ({ packagePath }) => {
 
   process.env.ROLLUP = true;
 
+  const inputFiles = glob.sync(`${path.join(packagePath, 'src', 'index')}@(*.js|*.jsx|*.ts|*.tsx)`);
+  if (inputFiles.length !== 1) {
+    throw new Error('Directory "/src" should contain single "index" file!');
+  }
+
   // eslint-disable-next-line
   const packageJson = require(path.join(packagePath, './package.json'));
-
   const externals = [
     ...Object.keys(packageJson.dependencies || {}),
     ...Object.keys(packageJson.peerDependencies || {})
@@ -28,7 +33,7 @@ module.exports = async ({ packagePath }) => {
   const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 
   const inputOptions = {
-    input: path.join(packagePath, 'src/index.ts'),
+    input: inputFiles[0],
     external: makeExternalPredicate(externals),
     plugins: [
       nodeResolve({
