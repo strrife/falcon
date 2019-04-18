389e88d4489303e8a273e5e4ee6ff7a8946ef99e
Borales <bordun.alexandr@gmail.com>
2019-04-18 17:45:21 +0200

    feature(falcon-server): Building cache tags from schema

--- packages/falcon-server/src/schemaDirectives/CacheDirective.js
+++ packages/falcon-server/src/schemaDirectives/CacheDirective.js
@@ -4,6 +4,9 @@ const crypto = require('crypto');
 
 // Default cache TTL (10 minutes)
 const DEFAULT_TTL = 10;
+const TAG_SEPARATOR = ':';
+const PATH_SEPARATOR = '.';
+const PARENT_KEYWORD = '$parent';
 
 /**
  * `@cache` directive
@@ -74,9 +77,106 @@ module.exports = class CacheDirective extends SchemaDirectiveVisitor {
         options: {
           ttl: ttl * 60 // minutes to seconds
         },
-        callback: resolver
+        callback: async () => {
+          const result = await resolver();
+          return thisDirective.handleCacheCallbackResponse(field, result, parent, context, info);
+        }
       });
     };
+  }
+
+  handleCacheCallbackResponse(field, result, parent, context, info) {
+    const { idPath = [] } = this.args;
+    const { name: returnTypeName } = this.getRealType(info.returnType);
+    const tags = [returnTypeName];
+
+    const entityIdFieldName = this.getCacheIdFieldName(info.returnType);
+    // Checking if Type is "self-cacheable" - it contains ID field and its value
+    if (entityIdFieldName) {
+      tags.push(...this.getCacheTags(returnTypeName, this.getFieldValue(result, entityIdFieldName)));
+    }
+
+    idPath.forEach(idPathEntry => {
+      tags.push(...this.getTagsForIdPath(idPathEntry, result, info, parent));
+    });
+
+    return result;
+  }
+
+  getTagsForIdPath(idPath, result, info, parent) {
+    const [rootPath, ...pathParts] = idPath.split(PATH_SEPARATOR);
+    const valueToCheck = rootPath === PARENT_KEYWORD ? parent : result;
+    const typeToCheck = this.getRealType(rootPath === PARENT_KEYWORD ? info.parentType : info.returnType);
+    if (rootPath !== PARENT_KEYWORD) {
+      // Put first path section back to "pathParts" for non-parent entries
+      pathParts.unshift(rootPath);
+    }
+
+    return this.getTagsForField(valueToCheck, typeToCheck, pathParts);
+  }
+
+  getTagsForField(value, typeField, idPath = []) {
+    if (!idPath.length) {
+      const fieldName = this.getCacheIdFieldName(typeField);
+      if (fieldName) {
+        return this.getCacheTags(typeField, this.getFieldValue(value, fieldName));
+      }
+    }
+
+    const [currentPath, ...restIdPath] = idPath;
+    const { _fields: fields } = typeField;
+    let { name: typeName } = typeField;
+    let fieldValue = this.getFieldValue(value, currentPath);
+
+    // Keep looking for nested ID path
+    if (currentPath && restIdPath.length) {
+      return this.getTagsForField(fieldValue, this.getRealType(fields[currentPath].type), restIdPath);
+    }
+    if (Array.isArray(fieldValue)) {
+      const currentType = this.getRealType(fields[currentPath].type);
+      typeName = currentType.name;
+      const currentCacheIdFieldName = this.getCacheIdFieldName(currentType);
+      fieldValue = this.getFieldValue(fieldValue, currentCacheIdFieldName);
+    }
+
+    return [typeName, ...this.getCacheTags(typeName, fieldValue)];
+  }
+
+  /**
+   * Get a field name with a `ID` type
+   * @param {object} objectType GQL Object Type
+   * @return {string|undefined} Name of the field
+   */
+  getCacheIdFieldName(objectType) {
+    const { _fields: fields } = this.getRealType(objectType);
+
+    return Object.keys(fields).find(fieldName => {
+      const { [fieldName]: fieldType } = fields;
+      const { name } = this.getRealType(fieldType.type);
+      return name === 'ID';
+    });
+  }
+
+  getFieldValue(valueToCheck, fieldName) {
+    if (Array.isArray(valueToCheck)) {
+      return valueToCheck.map(item => this.getFieldValue(item, fieldName));
+    }
+
+    return fieldName in valueToCheck ? valueToCheck[fieldName] : undefined;
+  }
+
+  /**
+   * Concatenates type with ID to generate a tag
+   * @param {string} entityName Entity Type name
+   * @param {string|string[]} entityId Entity ID or list of IDs
+   * @return {string[]} Concatenated cache-tag array of strings
+   */
+  getCacheTags(entityName, entityId) {
+    if (!entityId) {
+      return [];
+    }
+    const ids = Array.isArray(entityId) ? entityId : [entityId];
+    return ids.map(id => `${entityName}${TAG_SEPARATOR}${id}`);
   }
 
   /**
@@ -110,6 +210,16 @@ module.exports = class CacheDirective extends SchemaDirectiveVisitor {
     if (prev) {
       keys.unshift(this.getOperationPath(prev));
     }
-    return keys.join('.');
+    return keys.join(PATH_SEPARATOR);
+  }
+
+  /**
+   * @private
+   * @param {object} type GQL Object type
+   * @return {object} "real" object type
+   */
+  getRealType(type) {
+    const realType = type.ofType || type;
+    return realType.ofType ? this.getRealType(realType) : type;
   }
 };
