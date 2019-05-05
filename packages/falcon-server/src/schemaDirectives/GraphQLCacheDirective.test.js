b06a049201a90519af271791a50ec9efbdf9d59f
Borales <bordun.alexandr@gmail.com>
2019-05-05 22:18:06 +0300

    test(falcon-server): Adding more tests for GraphQLCacheDirective

--- packages/falcon-server/src/schemaDirectives/GraphQLCacheDirective.test.js
+++ packages/falcon-server/src/schemaDirectives/GraphQLCacheDirective.test.js
@@ -3,6 +3,8 @@ const { graphql } = require('graphql');
 const { makeExecutableSchema, addResolveFunctionsToSchema } = require('graphql-tools');
 const GraphQLCacheDirective = require('./GraphQLCacheDirective');
 
+const directiveDefinition = `directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION`;
+
 const config = {
   cache: {
     resolvers: {
@@ -41,7 +43,7 @@ describe('@cache directive', () => {
   it('Should properly resolve value and return a cached value for further calls', async () => {
     let callCount = 0;
     const typeDefs = `
-      directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION
+      ${directiveDefinition}
       type Query {
         foo: Foo @cache
       }
@@ -72,7 +74,7 @@ describe('@cache directive', () => {
 
   it('Should throw an error while trying to cache a scalar type', async () => {
     const typeDefs = `
-      directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION
+      ${directiveDefinition}
       type Query {
         foo: String @cache
       }
@@ -86,7 +88,7 @@ describe('@cache directive', () => {
 
   it('Should be able to handle dynamically added resolvers', async () => {
     const typeDefs = `
-      directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION
+      ${directiveDefinition}
       type Query {
         foo: Foo @cache
       }
@@ -123,7 +125,7 @@ describe('@cache directive', () => {
     it('Should properly extract tags for object type', async () => {
       const cacheSetSpy = jest.spyOn(cache, 'set');
       const typeDefs = `
-        directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION
+        ${directiveDefinition}
         type Query {
           foo: Foo @cache
         }
@@ -152,17 +154,115 @@ describe('@cache directive', () => {
       });
     });
 
+    it('Should be able to extract tags from a preprocessed value (for Cache component)', async () => {
+      const cacheSetSpy = jest.spyOn(cache, 'set');
+      const customTtl = 100;
+      const typeDefs = `
+        ${directiveDefinition}
+        type Query {
+          foo: Foo @cache
+        }
+        type Foo {
+          id: ID!
+          name: String
+        }
+      `;
+      const resolvers = {
+        Query: {
+          foo: () => ({
+            value: {
+              id: '1',
+              name: 'foo'
+            },
+            options: {
+              ttl: customTtl
+            }
+          })
+        }
+      };
+      const query = `query {
+        foo {
+          id
+          name
+        }
+      }`;
+      const expected = {
+        foo: {
+          id: '1',
+          name: 'foo'
+        }
+      };
+
+      const { data } = await run(typeDefs, resolvers, query, { cache, config });
+      expect(data).toEqual(expected);
+      expect(cacheSetSpy).toHaveBeenCalledWith(expect.anything(), expected.foo, {
+        tags: ['Foo', 'Foo:1'],
+        ttl: customTtl
+      });
+    });
+
+    it('Should handle falsy and disabled cache properly', async () => {
+      const cacheSetSpy = jest.spyOn(cache, 'set');
+      const typeDefs = `
+        ${directiveDefinition}
+        type Query {
+          foo: Foo @cache
+        }
+        type Foo {
+          id: ID!
+          name: String
+        }
+      `;
+      const typeDefsNonCached = `
+        ${directiveDefinition}
+        type Query {
+          foo: Foo @cache(ttl: 0)
+        }
+        type Foo {
+          id: ID!
+          name: String
+        }
+      `;
+      const resolvers = {
+        Query: {
+          foo: () => ({
+            id: '1',
+            name: 'foo2'
+          })
+        }
+      };
+      const query = `query {
+        foo {
+          id
+          name
+        }
+      }`;
+      const expected = {
+        foo: {
+          id: '1',
+          name: 'foo2'
+        }
+      };
+
+      const { data } = await run(typeDefsNonCached, resolvers, query, { cache, config });
+      expect(data).toEqual(expected);
+      expect(cacheSetSpy).not.toHaveBeenCalled();
+      const { data: data2 } = await run(typeDefs, resolvers, query, { cache });
+      expect(data2).toEqual(expected);
+      expect(cacheSetSpy).not.toHaveBeenCalled();
+    });
+
     it('Should be able to extract tags for the nested item list', async () => {
       const cacheSetSpy = jest.spyOn(cache, 'set');
       const typeDefs = `
-        directive @cache(ttl: Int, idPath: [String]) on FIELD_DEFINITION
+        ${directiveDefinition}
         type Query {
           foo: Foo
         }
         type Foo {
           id: ID!
           name: String
-          list: [Bar] @cache(ttl: 1, idPath: ["$parent"])
+          list: [Bar]! @cache(ttl: 1, idPath: ["$parent"])
           barList: BarList @cache(ttl: 1, idPath: ["$parent", "items"])
         }
         type Bar {
