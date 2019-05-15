aac2ba26db721fedbf401bf7d0e7a59b8d1ba77c
tutok <walas.marcin@gmail.com>
2019-05-15 13:42:32 +0200

    feat(auth): on the end TARGET is sufficient

--- packages/falcon-dev-tools/falcon-scripts/src/babel/babel.config.js
+++ packages/falcon-dev-tools/falcon-scripts/src/babel/babel.config.js
@@ -1,9 +1,8 @@
 const { NODE_ENV, TARGET, ROLLUP } = process.env;
 
-const targetNode = TARGET === 'NODE';
-const envTest = NODE_ENV === 'test';
+const targetNode = TARGET === 'NODE' || NODE_ENV === 'test';
 const rollupCjsBuild = ROLLUP !== undefined;
-const useESModules = !(rollupCjsBuild || targetNode || envTest);
+const useESModules = !(rollupCjsBuild || targetNode);
 
 module.exports = {
   presets: [
@@ -23,12 +22,12 @@ module.exports = {
     require.resolve('@babel/plugin-proposal-class-properties'),
     [require.resolve('@babel/plugin-transform-runtime'), { useESModules }],
     [require.resolve('@babel/plugin-proposal-object-rest-spread'), { loose: true, useBuiltIns: true }],
-    ...(targetNode || envTest
+    ...(targetNode
       ? [
           require.resolve('babel-plugin-dynamic-import-node'),
-          [require.resolve('@babel/plugin-transform-modules-commonjs'), { loose: true }]
+          [require.resolve('@babel/plugin-transform-modules-commonjs'), { loose: true }],
+          require.resolve('@babel/plugin-transform-react-jsx-source')
         ]
-      : [require.resolve('babel-plugin-annotate-pure-calls')]),
-    ...(envTest ? [require.resolve('@babel/plugin-transform-react-jsx-source')] : [])
+      : [require.resolve('babel-plugin-annotate-pure-calls')])
   ].filter(Boolean)
 };
