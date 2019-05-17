d793b959e986d63079b12017f084de5729b3be36
Borales <bordun.alexandr@gmail.com>
2019-05-17 12:32:46 +0200

    fix(falcon-server): Separating testing utils to a separate file, adding GraphQLCacheInvalidatorDirective tests

--- packages/falcon-server/src/schemaDirectives/GraphQLCacheInvalidatorDirective.js
+++ packages/falcon-server/src/schemaDirectives/GraphQLCacheInvalidatorDirective.js
@@ -1,6 +1,6 @@
 const { SchemaDirectiveVisitor } = require('graphql-tools');
 const { defaultFieldResolver } = require('graphql');
-const { extractTagsForIdPath } = require('../graphqlUtils');
+const { extractTagsForIdPath, getTagsForField } = require('../graphqlUtils');
 
 /**
  * @typedef {import('graphql').GraphQLType} GraphQLType
@@ -54,13 +54,18 @@ module.exports = class GraphQLCacheInvalidatorDirective extends SchemaDirectiveV
    * @param {IdPathEntry[]} idPath List of idPath entries to invalidate
    * @return {Function} Resolver function
    */
-  getResolverWithCacheInvalidator(resolve, idPath) {
+  getResolverWithCacheInvalidator(resolve, idPath = []) {
     const thisDirective = this;
     return async function fieldResolver(parent, params, context, info) {
       const resolver = async () => resolve.call(this, parent, params, context, info);
       const { config: { cache: { resolvers: { invalidation = false } = {} } = {} } = {} } = context;
       const result = await resolver();
       if (invalidation) {
+        const rootTags = getTagsForField(result, info.returnType);
+        if (rootTags) {
+          await context.cache.delete(rootTags);
+        }
+
         await Promise.all(
           idPath.map(idPathEntry => thisDirective.invalidateCacheByResult(result, idPathEntry, parent, context, info))
         );
---
+++ packages/falcon-server/src/schemaDirectives/GraphQLCacheInvalidatorDirective.test.js
@@ -0,0 +1,101 @@
+const { Cache, InMemoryLRUCache } = require('@deity/falcon-server-env');
+const GraphQLCacheInvalidatorDirective = require('./GraphQLCacheInvalidatorDirective');
+const { buildSchemaAndRunQuery } = require('../utils/testing');
+
+const directiveDefinition = `directive @cacheInvalidator(idPath: [IdPathEntryInput]) on FIELD_DEFINITION
+
+input IdPathEntryInput {
+  type: String
+  path: String!
+}`;
+
+const config = {
+  cache: {
+    resolvers: {
+      invalidation: true
+    }
+  }
+};
+
+const schemaDirectives = {
+  cacheInvalidator: GraphQLCacheInvalidatorDirective
+};
+
+describe('@cacheInvalidator directive', () => {
+  let cacheProvider;
+  let cache;
+
+  beforeEach(() => {
+    cacheProvider = new InMemoryLRUCache();
+    cache = new Cache(cacheProvider);
+  });
+
+  it('Should properly resolve value and flush the cache by tags', async () => {
+    const cacheDeleteSpy = jest.spyOn(cache, 'delete');
+    const typeDefs = `
+      ${directiveDefinition}
+      type Query {
+        foo: Foo @cacheInvalidator
+      }
+      type Foo {
+        id: ID!
+        name: String
+      }
+    `;
+
+    const resolvers = {
+      Query: {
+        foo: () => ({
+          id: '1',
+          name: 'foo'
+        })
+      }
+    };
+    const query = `query { foo { name } }`;
+    const expected = { foo: { name: 'foo' } };
+
+    const { data } = await buildSchemaAndRunQuery(typeDefs, resolvers, query, { cache, config }, schemaDirectives);
+    expect(data).toEqual(expected);
+    expect(cacheDeleteSpy).toHaveBeenCalledWith(['Foo:1']);
+  });
+
+  it('Should invalidate cache by tags with a custom Type', async () => {
+    const cacheDeleteSpy = jest.spyOn(cache, 'delete');
+    const typeDefs = `
+      ${directiveDefinition}
+      type Query {
+        foo: Foo @cacheInvalidator(idPath: [{ path: "items", type: "Bar" }])
+      }
+      type Foo {
+        items: [FooItem]
+      }
+      type FooItem {
+        id: ID!
+        name: String
+      }
+    `;
+
+    const resolvers = {
+      Query: {
+        foo: () => ({
+          items: [
+            {
+              id: '1',
+              name: 'foo1'
+            },
+            {
+              id: '2',
+              name: 'foo2'
+            }
+          ]
+        })
+      }
+    };
+    const query = `query { foo { items { name } } }`;
+    const expected = { foo: { items: [{ name: 'foo1' }, { name: 'foo2' }] } };
+
+    const { data } = await buildSchemaAndRunQuery(typeDefs, resolvers, query, { cache, config }, schemaDirectives);
+    expect(data).toEqual(expected);
+    expect(cacheDeleteSpy).toHaveBeenCalledWith(['Bar:1', 'Bar:2']);
+  });
+});
