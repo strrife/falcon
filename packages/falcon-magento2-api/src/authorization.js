a4298cd272489cbb2096ac6a3de776a31df3e9e8
tutok <walas.marcin@gmail.com>
2019-05-20 13:47:23 +0200

    feat(oauth): extracted oauth authorization into separate class

--- packages/falcon-magento2-api/src/authorization.js
+++ packages/falcon-magento2-api/src/authorization.js
@@ -1,3 +1,5 @@
+const OAuth = require('oauth');
+
 /**
  * Request authorization scope
  */
@@ -38,9 +40,60 @@ function isIntegrationAuthTypeSupported(authType) {
   return Object.keys(IntegrationAuthType).some(x => IntegrationAuthType[x] === authType);
 }
 
+/**
+ * oAuth 1 based request authorization
+ */
+class OAuth1Auth {
+  /**
+   * @param {Object} config configuration
+   * @param {string} config.consumerKey consumer key
+   * @param {string} config.consumerSecret consumer secret
+   * @param {string} config.accessToken access token
+   * @param {string} config.accessTokenSecret access token secret
+   * @param {string} config.requestTokenUrl request token URL
+   * @param {string} config.accessTokenUrl access token URL
+   * @param {Object} options options
+   * @param {Function} options.resolveURL Apollo `RESTDataSource.resolverURL`
+   */
+  constructor(config, options) {
+    this.config = config;
+    this.options = options || {};
+  }
+
+  initialize() {
+    this.oAuthClient = new OAuth.OAuth(
+      this.config.requestTokenUrl,
+      this.config.accessTokenUrl,
+      this.config.consumerKey,
+      this.config.consumerSecret,
+      '1',
+      null,
+      'HMAC-SHA1'
+    );
+
+    // TODO: make full handshake
+  }
+
+  async authorize(request) {
+    const url = this.options.resolveURL ? await this.options.resolveURL(request) : request.path;
+    request.params.forEach((value, key) => url.searchParams.append(key, value));
+
+    const authorizationHeader = this.oAuthClient.authHeader(
+      url.toString(),
+      this.config.accessToken,
+      this.config.accessTokenSecret,
+      request.method
+    );
+    request.headers.set('Authorization', authorizationHeader);
+
+    return request;
+  }
+}
+
 module.exports = {
   AuthScope,
   IntegrationAuthType,
   getDefaultAuthScope,
-  isIntegrationAuthTypeSupported
+  isIntegrationAuthTypeSupported,
+  OAuth1Auth
 };
