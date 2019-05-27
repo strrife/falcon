07dbe75234c4edc0878835a30375ea3d8f27d51a
tutok <walas.marcin@gmail.com>
2019-05-27 16:04:44 +0200

    feat(custom-sw): removed file which does not make sense

--- packages/falcon-client/src/serviceWorker/sw.js
+++ packages/falcon-client/src/serviceWorker/sw.js
@@ -1,1 +1,20 @@
-import './pre-cache';
+/* global workbox, importScripts */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+workbox.core.setCacheNameDetails({ prefix: '@deity' });
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.suppressWarnings();
+
+const ENTRIES = [];
+workbox.precaching.precacheAndRoute(ENTRIES, {});
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.mode === 'navigate',
+  ({ url }) => fetch(url.href).catch(() => caches.match('/app-shell'))
+);
+
+workbox.routing.registerRoute('/', workbox.strategies.networkFirst(), 'GET');
