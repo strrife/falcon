cf98796c28fffb1c1ad27591fc21180abe8afbdc
tutok <walas.marcin@gmail.com>
2019-05-30 13:37:45 +0200

    feat(perf-optimizations): found that totalRow does not use custom currency

--- packages/falcon-ecommerce-uikit/src/Checkout/TotalRow.tsx
+++ packages/falcon-ecommerce-uikit/src/Checkout/TotalRow.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Box, Text } from '@deity/falcon-ui';
-import { Price } from './../Locale';
+import { Price } from '../Locale';
 
 export type TotalRowProps = {
   title: string;
   value: number;
-  currency?: string;
 };
-export const TotalRow: React.SFC<TotalRowProps> = ({ title, value, currency, ...props }) => (
+export const TotalRow: React.SFC<TotalRowProps> = ({ title, value, ...props }) => (
   <Box display="flex" {...props}>
     <Text flex="1">{title}</Text>
-    <Price value={value} currency={currency} />
+    <Price value={value} />
   </Box>
 );
