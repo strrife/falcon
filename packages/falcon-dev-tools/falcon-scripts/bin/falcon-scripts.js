3736efb9f0edcc6c98dc545d64ddbf7329327abc
tutok <walas.marcin@gmail.com>
2019-05-31 10:58:40 +0200

    feat(magento-2-api-ts): watch need to be also target aware

--- packages/falcon-dev-tools/falcon-scripts/bin/falcon-scripts.js
+++ packages/falcon-dev-tools/falcon-scripts/bin/falcon-scripts.js
@@ -12,22 +12,26 @@ const Logger = require('@deity/falcon-logger');
 (async () => {
   const script = process.argv[2];
   const args = process.argv.slice(3) || [];
+
+  const target =
+    (args.find(x => x.startsWith('--target=')) || '')
+      .split('=')
+      .pop()
+      .toUpperCase() || undefined;
+  if (target) {
+    process.env.TARGET = target;
+  }
+
   const packagePath = process.cwd();
 
   try {
     switch (script) {
       case 'build': {
-        const target =
-          (args.find(x => x.startsWith('--target=')) || '')
-            .split('=')
-            .pop()
-            .toUpperCase() || undefined;
-
         const buildEsm = require('../src/build-esm');
         const buildTsDeclarations = require('../src/build-tsDeclarations');
         const buildCjs = require('../src/build-cjs');
 
-        buildEsm({ packagePath, target });
+        buildEsm();
         buildTsDeclarations({ packagePath });
         if (target !== 'NODE') {
           await buildCjs({ packagePath, target });
