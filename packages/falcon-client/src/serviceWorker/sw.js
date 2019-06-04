dcfdad58bb77e7cb538984d0346a5bef332ebc34
tutok <walas.marcin@gmail.com>
2019-06-04 10:14:01 +0200

    feat(ssr-until-sw): migrates sw.js

--- packages/falcon-client/src/serviceWorker/sw.js
+++ packages/falcon-client/src/serviceWorker/sw.js
@@ -1,17 +1,20 @@
 /* global workbox, importScripts */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({ prefix: '@deity' });
 
-workbox.skipWaiting();
-workbox.clientsClaim();
-
-workbox.precaching.suppressWarnings();
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 const ENTRIES = [];
 workbox.precaching.precacheAndRoute(ENTRIES, {});
 
-workbox.routing.registerRoute(({ event }) => event.request.mode === 'navigate', () => caches.match('/app-shell'));
+// workbox.routing.registerRoute(({ event }) => event.request.mode === 'navigate', () => caches.match('/app-shell'));
+// workbox.routing.registerRoute('/', workbox.strategies.networkFirst(), 'GET');
 
-workbox.routing.registerRoute('/', workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('app-shell'));
+
+// workbox.routing.setCatchHandler(({ url, event, params }) => {
+//   // In the case of any of routes throwing an error, capture and degrade gracefully
+// });
