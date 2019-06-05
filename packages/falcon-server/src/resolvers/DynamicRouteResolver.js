d0b635866353892dce5a12061651d9f4e05911ab
Borales <bordun.alexandr@gmail.com>
2019-06-06 00:17:58 +0200

    fix(falcon-server-env): Fixed Cache key checks

--- packages/falcon-server/src/resolvers/DynamicRouteResolver.js
+++ packages/falcon-server/src/resolvers/DynamicRouteResolver.js
@@ -87,9 +87,7 @@ module.exports = class DynamicRouteResolver {
     /* eslint-enable no-await-in-loop */
     /* eslint-enable no-restricted-syntax */
 
-    if (response) {
-      response.path = path;
-    }
+    response.path = path;
 
     return response;
   }
