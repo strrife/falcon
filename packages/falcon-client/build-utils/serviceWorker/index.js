4406667fd3aecfed21f44bb5375b159a25c0f673
tutok <walas.marcin@gmail.com>
2019-06-10 08:13:52 +0200

    feat(ssr-until-sw): addseparate SW build method for watch - right now not implemented

--- packages/falcon-client/build-utils/serviceWorker/index.js
+++ packages/falcon-client/build-utils/serviceWorker/index.js
@@ -32,6 +32,63 @@ module.exports.build = async buildConfig => {
         re({
           patterns: [
             { test: 'process.env.NODE_ENV', replace: JSON.stringify('development' || NODE_ENV) },
+            { test: 'const CONFIG = {};', replace: `const CONFIG = ${JSON.stringify(buildConfig, null, 2)};` },
+            { test: 'const ENTRIES = [];', replace: `const ENTRIES = ${JSON.stringify(manifestEntries, null, 2)};` }
+          ]
+        })
+      ].map(x => x)
+    };
+
+    const outputOptions = {
+      file: path.join(SW_DIR, 'sw.js'),
+      format: 'iife',
+      sourcemap: !IS_PROD,
+      compact: IS_PROD
+    };
+
+    const bundle = await rollup.rollup(inputOptions);
+    await bundle.write(outputOptions);
+
+    Logger.log('Service Worker compiled.\n');
+  } catch (error) {
+    Logger.error(chalk.red(`Failed to compile Service Worker\n${input}`));
+    Logger.error(error);
+    Logger.log();
+
+    process.exit(1);
+  }
+};
+
+/**
+ * //TODO: implement real watching
+ * @param {import('../webpack/tools').FalconSWBuildConfig} buildConfig
+ */
+module.exports.watch = async buildConfig => {
+  Logger.log('Compiling Service Worker...');
+
+  const { NODE_ENV } = process.env;
+  const IS_PROD = NODE_ENV === 'production';
+  const SW_DIR = IS_PROD ? paths.appBuildPublic : paths.appBuild;
+
+  if (buildConfig.precache) {
+    Logger.warn('Precache is not supported while DEVELOPMENT SERVER, so it will be ignored');
+    buildConfig.precache = false;
+  }
+
+  const input = fs.existsSync(paths.appSwJs) ? paths.appSwJs : paths.ownSwJs;
+
+  try {
+    const manifestEntries = [];
+
+    const inputOptions = {
+      input,
+      plugins: [
+        alias({ 'app-path': paths.appPath }),
+        resolve(),
+        re({
+          patterns: [
+            { test: 'process.env.NODE_ENV', replace: JSON.stringify('development' || NODE_ENV) },
+            { test: 'const CONFIG = {};', replace: `const CONFIG = ${JSON.stringify(buildConfig, null, 2)};` },
             { test: 'const ENTRIES = [];', replace: `const ENTRIES = ${JSON.stringify(manifestEntries, null, 2)};` }
           ]
         })
