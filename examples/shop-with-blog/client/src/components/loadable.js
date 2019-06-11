8cb2df1fc9a906c30a2f520e3e9fab124a8c0f46
tutok <walas.marcin@gmail.com>
2019-06-11 13:35:38 +0200

    feat(ssr-until-sw): cleanup

--- examples/shop-with-blog/client/src/components/loadable.js
+++ examples/shop-with-blog/client/src/components/loadable.js
@@ -5,9 +5,10 @@ import { Loader } from '@deity/falcon-ecommerce-uikit';
 /**
  * loadable define code splitting point with lazy `import`, it needs to be exported as `default` and imported via `loadable` name to be compatible with SSR
  * @see https://github.com/smooth-code/loadable-components/issues/100#issuecomment-441192303
- * @param {() => )Promise<any>} component module which should be lazy imported
+ * @param {() => Promise<any>} component module which should be lazy imported
  * @returns {any} content of lazy imported module
  */
+
 export default component =>
   loadable(component, {
     fallback: <Loader />
