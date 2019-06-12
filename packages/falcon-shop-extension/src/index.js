73cdabfacf46b6b346618527da0f2efa8cc77c1d
Borales <bordun.alexandr@gmail.com>
2019-06-12 14:51:49 +0200

    feat(extensions): Simplifying native extensions

--- packages/falcon-shop-extension/src/index.js
+++ packages/falcon-shop-extension/src/index.js
@@ -1,31 +1,14 @@
-const { Extension } = require('@deity/falcon-server-env');
-const { resolve } = require('path');
-const typeDefs = require('fs').readFileSync(resolve(__dirname, 'schema.graphql'), 'utf8');
-
 /**
- * Extension that implements shop features
+ * Shop Extension
  */
-class Shop extends Extension {
-  async getGraphQLConfig() {
-    const gqlConfig = await super.getGraphQLConfig(typeDefs);
-
-    Object.assign(gqlConfig.resolvers, {
-      PlaceOrderResult: {
-        __resolveType: obj => (obj.url ? 'PlaceOrder3dSecureResult' : 'PlaceOrderSuccessfulResult')
-      },
-      BackendConfig: {
-        // Returning an empty object to make ShopConfig resolvers work
-        shop: () => ({})
-      },
-      ShopConfig: {
-        activeCurrency: (_, __, context) => this.getApiSession(context).currency,
-        activeStore: (_, __, context) => this.getApiSession(context).storeCode
-      }
-    });
-
-    return gqlConfig;
+module.exports = () => ({
+  resolvers: {
+    PlaceOrderResult: {
+      __resolveType: ({ url }) => (url ? 'PlaceOrder3dSecureResult' : 'PlaceOrderSuccessfulResult')
+    },
+    BackendConfig: {
+      // Returning an empty object to make ShopConfig resolvers work
+      shop: () => ({})
+    }
   }
-}
-
-module.exports = Shop;
-module.exports.Schema = typeDefs;
+});
