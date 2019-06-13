2c1e4e0388de2e24084e08ec3b2adec8a9cb212b
tutok <walas.marcin@gmail.com>
2019-06-13 17:25:45 +0200

    feat(ts-errors): simplified way in which we print ts errros

--- packages/falcon-dev-tools/falcon-scripts/src/build-dts.js
+++ packages/falcon-dev-tools/falcon-scripts/src/build-dts.js
@@ -18,62 +18,19 @@ class FormatHost {
 }
 
 function tsc(fileNames, options) {
-  const prettyErrors = true;
-  const formatHost = new FormatHost();
-
   const program = ts.createProgram(fileNames, options);
   const emitResult = program.emit();
   let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
 
   allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
 
-  allDiagnostics
-    .map(item => {
-      const entry = {
-        flatMessage: ts.flattenDiagnosticMessageText(item.messageText, '\n'),
-        formatted: ts.formatDiagnosticsWithColorAndContext(allDiagnostics, formatHost),
-        category: item.category,
-        code: item.code
-      };
+  if (allDiagnostics.some(x => x.category === 1)) {
+    Logger.error(ts.formatDiagnosticsWithColorAndContext(allDiagnostics, new FormatHost()));
 
-      if (item.file && item.start !== undefined) {
-        const { line, character } = item.file.getLineAndCharacterOfPosition(item.start);
-        entry.fileLine = `${item.file.fileName}(${line + 1},${character + 1})`;
-      }
+    throw new Error();
+  }
 
-      return entry;
-    })
-    .forEach(item => {
-      let category = '';
-      let loggerMethod = 'info';
-
-      switch (item.category) {
-        case 1 /* Error */:
-          loggerMethod = 'error';
-          category = 'error';
-          break;
-        case 3 /* Message */:
-          loggerMethod = 'info';
-          break;
-
-        case 0 /* Warning */:
-        case 2 /* Suggestion */:
-        default:
-          loggerMethod = 'warn';
-          category = 'warning';
-          break;
-      }
-
-      const type = `${item.type} `;
-
-      if (prettyErrors) {
-        Logger[loggerMethod](`${item.formatted}`);
-      } else if (item.fileLine !== undefined) {
-        Logger[loggerMethod](`${item.fileLine}: ${type}${category} TS${item.code}: ${item.flatMessage}`);
-      } else {
-        Logger[loggerMethod](`${type}${category} TS${item.code}: ${item.flatMessage}`);
-      }
-    });
+  Logger.info(ts.formatDiagnosticsWithColorAndContext(allDiagnostics, new FormatHost()));
 }
 
 module.exports = ({ packagePath }) => {
