99c8f0d49557c4daf600f24a8e7ad10b86952487
tutok <walas.marcin@gmail.com>
2019-06-13 17:04:05 +0200

    feat(ts-errors): rendering  ts errors

--- packages/falcon-dev-tools/falcon-scripts/src/build-tsDeclarations.js
+++ packages/falcon-dev-tools/falcon-scripts/src/build-tsDeclarations.js
@@ -3,14 +3,81 @@ const glob = require('glob');
 const ts = require('typescript');
 const Logger = require('@deity/falcon-logger');
 
-function tsc(fileNames, options) {
-  const program = ts.createProgram(fileNames, options);
+class FormatHost {
+  getCurrentDirectory() {
+    return ts.sys.getCurrentDirectory();
+  }
 
-  program.emit();
+  getCanonicalFileName(fileName) {
+    return path.normalize(fileName);
+  }
+
+  getNewLine() {
+    return ts.sys.newLine;
+  }
+}
+
+function tsc(fileNames, options) {
+  const prettyErrors = true;
+  const formatHost = new FormatHost();
+
+  const program = ts.createProgram(fileNames, options);
+  const emitResult = program.emit();
+  let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+  allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
+
+  allDiagnostics
+    .map(item => {
+      const entry = {
+        flatMessage: ts.flattenDiagnosticMessageText(item.messageText, '\n'),
+        formatted: ts.formatDiagnosticsWithColorAndContext(allDiagnostics, formatHost),
+        category: item.category,
+        code: item.code
+      };
+
+      if (item.file && item.start !== undefined) {
+        const { line, character } = item.file.getLineAndCharacterOfPosition(item.start);
+        entry.fileLine = `${item.file.fileName}(${line + 1},${character + 1})`;
+      }
+
+      return entry;
+    })
+    .forEach(item => {
+      let category = '';
+      let loggerMethod = 'info';
+
+      switch (item.category) {
+        case 1 /* Error */:
+          loggerMethod = 'error';
+          category = 'error';
+          break;
+        case 3 /* Message */:
+          loggerMethod = 'info';
+          break;
+
+        case 0 /* Warning */:
+        case 2 /* Suggestion */:
+        default:
+          loggerMethod = 'warn';
+          category = 'warning';
+          break;
+      }
+
+      const type = `${item.type} `;
+
+      if (prettyErrors) {
+        Logger[loggerMethod](`${item.formatted}`);
+      } else if (item.fileLine !== undefined) {
+        Logger[loggerMethod](`${item.fileLine}: ${type}${category} TS${item.code}: ${item.flatMessage}`);
+      } else {
+        Logger[loggerMethod](`${type}${category} TS${item.code}: ${item.flatMessage}`);
+      }
+    });
 }
 
 module.exports = ({ packagePath }) => {
-  Logger.log(`building ts declarations...`);
+  Logger.log(`building d.ts...`);
 
   const files = glob.sync(`${path.join(packagePath, 'src')}/*(*.ts|*.tsx)`);
 
@@ -23,7 +90,7 @@ module.exports = ({ packagePath }) => {
     allowSyntheticDefaultImports: true,
     esModuleInterop: true,
     jsx: ts.JsxEmit.React,
-    strict: true,
+    alwaysStrict: true,
     noFallthroughCasesInSwitch: true,
     noImplicitReturns: true,
     noUnusedParameters: true,
