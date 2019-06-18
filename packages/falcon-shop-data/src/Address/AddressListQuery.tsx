f59d6f9c893ffb3bf959ee048ac2152662d195c9
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2019-06-18 14:41:35 +0200

    refactor(uikit): fix types for adresses query

--- packages/falcon-shop-data/src/Address/AddressListQuery.tsx
+++ packages/falcon-shop-data/src/Address/AddressListQuery.tsx
@@ -2,12 +2,6 @@ import gql from 'graphql-tag';
 import { Address } from '@deity/falcon-shop-extension';
 import { Query } from '../Query';
 
-export type AddressListResponse = {
-  addresses: {
-    items: Address[];
-  };
-};
-
 export const GET_ADDRESS_LIST = gql`
   query Addresses {
     addresses {
@@ -29,6 +23,26 @@ export const GET_ADDRESS_LIST = gql`
     }
   }
 `;
+export type AddressListResponse = {
+  addresses: {
+    items: Pick<
+      Address,
+      | 'id'
+      | 'firstname'
+      | 'lastname'
+      | 'telephone'
+      | 'street'
+      | 'city'
+      | 'postcode'
+      | 'region'
+      | 'regionId'
+      | 'countryId'
+      | 'company'
+      | 'defaultBilling'
+      | 'defaultShipping'
+    >[];
+  };
+};
 
 export class AddressListQuery extends Query<AddressListResponse> {
   static defaultProps = {
