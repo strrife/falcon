a0b09797b3b593cd48408824439079aaa04b61de
tutok <walas.marcin@gmail.com>
2019-06-24 10:56:46 +0200

    feat(fs-improvements): used paths

--- packages/falcon-dev-tools/falcon-scripts/src/build-cjs.js
+++ packages/falcon-dev-tools/falcon-scripts/src/build-cjs.js
@@ -5,6 +5,7 @@ const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const Logger = require('@deity/falcon-logger');
+const { paths } = require('./tools');
 
 const makeExternalPredicate = externalsArr => {
   if (externalsArr.length === 0) {
@@ -14,19 +15,19 @@ const makeExternalPredicate = externalsArr => {
   return id => externalPattern.test(id);
 };
 
-module.exports.pkg = async ({ packagePath }) => {
+module.exports.pkg = async () => {
   Logger.log('building cjs...');
 
   process.env.ROLLUP = true;
   // process.env.TARGET = 'NODE';
 
-  const inputFiles = glob.sync(`${path.join(packagePath, 'src', 'index')}@(*.js|*.jsx|*.ts|*.tsx)`);
+  const inputFiles = glob.sync(`${path.join(paths.pkgSrc, 'index')}@(*.js|*.jsx|*.ts|*.tsx)`);
   if (inputFiles.length !== 1) {
-    throw new Error('Directory "/src" should contain single "index" file!');
+    throw new Error(`Directory "${paths.pkgSrc}" should contain single "index" file!`);
   }
 
   // eslint-disable-next-line
-  const packageJson = require(path.join(packagePath, './package.json'));
+  const packageJson = require(paths.pkgPackageJson);
   const externals = [
     ...Object.keys(packageJson.dependencies || {}),
     ...Object.keys(packageJson.peerDependencies || {})
@@ -41,12 +42,12 @@ module.exports.pkg = async ({ packagePath }) => {
       nodeResolve({
         extensions
       }),
+      commonjs(),
       babel({
         extensions,
         runtimeHelpers: true,
         ...require('./babel/babel.config')
-      }),
-      commonjs()
+      })
     ]
   };
   const outputOptions = { file: packageJson.main, format, sourcemap: true };
@@ -55,23 +56,22 @@ module.exports.pkg = async ({ packagePath }) => {
   await bundle.write(outputOptions);
 };
 
-module.exports.cli = async ({ packagePath }) => {
+module.exports.cli = async () => {
   console.log('building cli...');
 
   process.env.ROLLUP = true;
   // process.env.TARGET = 'NODE';
 
   // eslint-disable-next-line
-  const packageJson = require(path.join(packagePath, './package.json'));
+  const packageJson = require(paths.pkgPackageJson);
 
-  const directory = path.join('src', 'cli');
-  const inputFiles = glob.sync(`${path.join(packagePath, directory, 'index')}@(*.js|*.jsx|*.ts|*.tsx)`);
+  const inputFiles = glob.sync(`${path.join(paths.pkgBinSrc, 'index')}@(*.js|*.jsx|*.ts|*.tsx)`);
   if (inputFiles.length === 0) {
     // nothing to compile
     return;
   }
   if (inputFiles.length !== 1) {
-    throw new Error(`'Directory '${directory}' should contain single 'index.*' file!`);
+    throw new Error(`'Directory '${paths.pkgBinSrc}' should contain single 'index.*' file!`);
   }
 
   const externals = [
@@ -96,7 +96,7 @@ module.exports.cli = async ({ packagePath }) => {
       commonjs()
     ]
   };
-  const outputOptions = { file: 'dist/cli/index.js', format, sourcemap: 'inline' };
+  const outputOptions = { file: 'dist/cli/index.js', format, sourcemap: true };
 
   const bundle = await rollup.rollup(inputOptions);
   await bundle.write(outputOptions, {});
