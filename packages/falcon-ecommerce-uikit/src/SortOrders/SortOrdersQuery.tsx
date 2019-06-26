db17841e51fa71f497d04dd0dc224f101a349e40
tutok <walas.marcin@gmail.com>
2019-06-26 11:22:23 +0200

    feat(front-kit-2): removed SortOrdersQuery from ecommerce uikit

--- packages/falcon-ecommerce-uikit/src/SortOrders/SortOrdersQuery.tsx
+++ packages/falcon-ecommerce-uikit/src/SortOrders/SortOrdersQuery.tsx
@@ -1,12 +1,3 @@
-import gql from 'graphql-tag';
-import { Query } from '../Query/Query';
-
-export const SORT_ORDERS_QUERY = gql`
-  query SortOrdersQuery {
-    sortOrders @client
-  }
-`;
-
 export type SortOrderDirection = 'asc' | 'desc';
 
 export type SortOrderInput = {
@@ -22,12 +13,3 @@ export type SortOrder = {
 export type SortOrdersData = {
   sortOrders: SortOrder[];
 };
-
-export class SortOrdersQuery extends Query<SortOrdersData> {
-  static defaultProps = {
-    query: SORT_ORDERS_QUERY
-  };
-}
-
-export const AreSortOrderInputsEqual = (item1?: SortOrderInput, item2?: SortOrderInput): boolean =>
-  !!item1 && !!item2 && item1.field === item2.field && item1.direction === item2.direction;
