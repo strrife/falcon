22e0ef0165e539ac32cc62cf28c7de7a6bbf39fa
Nimo Beeren <nimo@deity.io>
2019-07-23 10:18:06 +0200

    fix: invalid payment method

--- examples/shop-with-blog/client/src/pages/shop/Account/Orders/Order.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/Orders/Order.js
@@ -106,7 +106,7 @@ const Order = ({ match }) => {
                 <Text fontWeight="bold">
                   <T id="order.paymentMethodLabel" />
                 </Text>
-                {order.shippingDescription}
+                {order.paymentMethodName}
               </Box>
             </GridLayout>
           </Box>
