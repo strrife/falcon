dd3aca7a144f5e7238fe8c28cfb45a97b756ac86
tutok <walas.marcin@gmail.com>
2019-07-24 10:58:35 +0200

    feat(lodash-vulnerability): fixing eslint config

--- packages/falcon-dev-tools/eslint-config-falcon/index.js
+++ packages/falcon-dev-tools/eslint-config-falcon/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['eslint-config-airbnb', 'plugin:prettier/recommended'],
-  plugins: ['react', 'import', 'prettier', 'jsdoc'],
+  plugins: ['react', 'import', 'prettier', 'jsdoc', 'jsx-a11y'],
   settings: {
     'import/parser': 'babel-eslint',
     'import/resolver': { node: true },
