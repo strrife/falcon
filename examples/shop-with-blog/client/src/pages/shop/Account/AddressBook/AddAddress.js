5d224db83136fcf793a6a2d6d12ef088090161d3
Nimo Beeren <nimo@deity.io>
2019-08-01 13:40:27 +0200

    improvement: use AddressForm in AddAddress

--- examples/shop-with-blog/client/src/pages/shop/Account/AddressBook/AddAddress.js
+++ examples/shop-with-blog/client/src/pages/shop/Account/AddressBook/AddAddress.js
@@ -1,90 +1,61 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { Formik } from 'formik';
 import { T } from '@deity/falcon-i18n';
-import { H1, Button, FlexLayout, GridLayout } from '@deity/falcon-ui';
-import { TwoColumnsLayout, TwoColumnsLayoutArea, CountriesQuery, CountrySelector } from '@deity/falcon-ecommerce-uikit';
-import { Form, FormField, CheckboxFormField, FormErrorSummary } from '@deity/falcon-ui-kit';
+import { H1, GridLayout } from '@deity/falcon-ui';
+import { CountriesQuery, Loader } from '@deity/falcon-ecommerce-uikit';
 import { AddAddressMutation } from '@deity/falcon-shop-data';
+import AddressForm from '../../components/AddressForm';
+import ErrorList from '../../components/ErrorList';
 
-const AddAddress = ({ history }) => (
-  <GridLayout mb="md" gridGap="md">
-    <H1>
-      <T id="addAddress.title" />
-    </H1>
-    <AddAddressMutation>
-      {(addAddress, { loading, error }) => (
-        <Formik
-          initialValues={{
-            firstname: '',
-            lastname: '',
-            street1: '',
-            street2: '',
-            postcode: '',
-            city: '',
-            countryId: '',
-            company: '',
-            telephone: '',
-            defaultBilling: false,
-            defaultShipping: false
-          }}
-          onSubmit={({ street1, street2, ...values }) =>
-            addAddress({
-              variables: {
-                input: {
-                  ...values,
-                  street: [street1, street2]
+const AddAddress = ({ history }) => {
+  const initialAddressValues = {
+    firstname: '',
+    lastname: '',
+    street1: '',
+    street2: '',
+    postcode: '',
+    city: '',
+    countryId: '',
+    company: '',
+    telephone: '',
+    defaultBilling: false,
+    defaultShipping: false
+  };
+
+  return (
+    <CountriesQuery>
+      {({ countries }) => (
+        <AddAddressMutation>
+          {(addAddress, { loading, error }) => (
+            <GridLayout position="relative" mb="md" gridGap="md">
+              {loading && <Loader variant="overlay" />}
+              <H1>
+                <T id="addAddress.title" />
+              </H1>
+              <Formik
+                initialValues={initialAddressValues}
+                onSubmit={
+                  ({ street1, street2, ...values }) =>
+                    addAddress({
+                      variables: {
+                        input: {
+                          ...values,
+                          street: [street1, street2]
+                        }
+                      }
+                    }).then(() => history.push('/account/address-book')) // TODO: render Redirect instead
                 }
-              }
-            }).then(() => history.push('/account/address-book'))
-          }
-        >
-          {() => (
-            <Form id="add-address" i18nId="addAddress">
-              <CheckboxFormField name="defaultBilling" />
-              <CheckboxFormField name="defaultShipping" />
-              <TwoColumnsLayout>
-                <GridLayout gridArea={TwoColumnsLayoutArea.left}>
-                  <FormField name="company" />
-                  <FormField name="firstname" required />
-                  <FormField name="lastname" required />
-                  <FormField name="telephone" />
-                </GridLayout>
-                <GridLayout gridArea={TwoColumnsLayoutArea.right}>
-                  <FormField name="street1" required />
-                  <FormField name="street2" />
-                  <FormField name="postcode" required />
-                  <FormField name="city" required />
-                  <FormField name="countryId" required>
-                    {({ form, field }) => (
-                      <CountriesQuery passLoading>
-                        {({ countries = { items: [] } }) => (
-                          <CountrySelector
-                            {...field}
-                            onChange={x => form.setFieldValue(field.name, x)}
-                            items={countries.items}
-                          />
-                        )}
-                      </CountriesQuery>
-                    )}
-                  </FormField>
-                </GridLayout>
-              </TwoColumnsLayout>
-              <FlexLayout justifyContent="flex-end" alignItems="center" mt="md">
-                <Button as={NavLink} to="/account/address-book" mr="md">
-                  <T id="addAddress.cancelButton" />
-                </Button>
-                <Button type="submit" variant={loading ? 'loader' : undefined}>
-                  <T id="addAddress.submitButton" />
-                </Button>
-              </FlexLayout>
-              <FormErrorSummary errors={error && [error.message]} />
-            </Form>
+              >
+                {/* TODO: form should ask if it should be a default address */}
+                {() => <AddressForm id="add-address" countries={countries.items} />}
+              </Formik>
+              <ErrorList errors={error ? [new Error(error)] : []} />
+            </GridLayout>
           )}
-        </Formik>
+        </AddAddressMutation>
       )}
-    </AddAddressMutation>
-  </GridLayout>
-);
+    </CountriesQuery>
+  );
+};
 
 export default AddAddress;
