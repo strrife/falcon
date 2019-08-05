9ea028231a197887720286232f703f1b3c1d3488
tutok <walas.marcin@gmail.com>
2019-08-05 11:55:02 +0200

    feat(front-kit-3): added support for arrays  into requiredValidator

--- packages/falcon-front-kit/src/Forms/validators.ts
+++ packages/falcon-front-kit/src/Forms/validators.ts
@@ -5,7 +5,13 @@ export type Validator = (value: string, label: string) => string | undefined;
 const validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const validPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
 
-export const requiredValidator: Validator = (value, label) => (!value ? `${label} is required` : undefined);
+export const requiredValidator: Validator = (value, label) => {
+  if (value === null || value === undefined || value === '' || (Array.isArray(value) && !(value as any[]).length)) {
+    return `${label} is required`;
+  }
+
+  return undefined;
+};
 
 export const emailValidator: Validator = value => {
   if (!value || !validEmailRegex.test(value.toLowerCase())) {
