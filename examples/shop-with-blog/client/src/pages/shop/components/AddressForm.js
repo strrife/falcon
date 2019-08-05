b5adf2ad8a1732dc9fa753b9d958c4a08f5566d6
Nimo Beeren <nimo@deity.io>
2019-08-05 10:13:49 +0200

    improvement: change address form layout to flexbox

--- examples/shop-with-blog/client/src/pages/shop/components/AddressForm.js
+++ examples/shop-with-blog/client/src/pages/shop/components/AddressForm.js
@@ -1,42 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, FormField, CountrySelector, toGridTemplate } from '@deity/falcon-ecommerce-uikit';
+import { Form, FormField, CountrySelector } from '@deity/falcon-ecommerce-uikit';
 import { Box, Button } from '@deity/falcon-ui';
-
-const AddressFormArea = {
-  firstName: 'firstName',
-  lastName: 'lastName',
-  street1: 'street1',
-  street2: 'street2',
-  number: 'number',
-  postCode: 'postCode',
-  city: 'city',
-  phone: 'phone',
-  region: 'region',
-  country: 'country',
-  submit: 'submit'
-};
 
 const addressFormLayout = {
   addressFormLayout: {
-    display: 'grid',
-    gridGap: 'sm',
-    my: 'xs',
-    fontSize: 'xs',
-    // prettier-ignore
-    gridTemplate:  toGridTemplate([
-      ['1fr'                      ],
-      [AddressFormArea.firstName  ],
-      [AddressFormArea.lastName   ],
-      [AddressFormArea.street1    ],
-      [AddressFormArea.street2    ],
-      [AddressFormArea.number     ],
-      [AddressFormArea.postCode   ],
-      [AddressFormArea.city       ],
-      [AddressFormArea.phone      ],
-      [AddressFormArea.country    ],
-      [AddressFormArea.submit     ]
-    ])
+    display: 'flex',
+    flexDirection: 'column'
   }
 };
 
@@ -44,45 +14,20 @@ const AddressForm = ({ countries = [], submitLabel = 'Save', id = '', autoComple
   const getAutoComplete = attribute => [autoCompleteSection, attribute].filter(x => x).join(' ');
 
   return (
-    <Form id={id} defaultTheme={addressFormLayout} i18nId="addressForm">
-      <FormField
-        name="firstname"
-        required
-        autoComplete={getAutoComplete('given-name')}
-        gridArea={AddressFormArea.firstName}
-      />
-      <FormField
-        name="lastname"
-        required
-        autoComplete={getAutoComplete('family-name')}
-        gridArea={AddressFormArea.lastName}
-      />
-      <FormField
-        name="street1"
-        required
-        autoComplete={getAutoComplete('address-line1')}
-        gridArea={AddressFormArea.street1}
-      />
-      <FormField name="street2" autoComplete={getAutoComplete('address-line2')} gridArea={AddressFormArea.street2} />
-      <FormField name="countryId" required autoComplete={getAutoComplete('country')} gridArea={AddressForm.country}>
+    <Form id={id} defaultTheme={addressFormLayout} i18nId="addressForm" height="400px">
+      <FormField name="firstname" required autoComplete={getAutoComplete('given-name')} mb="sm" />
+      <FormField name="lastname" required autoComplete={getAutoComplete('family-name')} mb="sm" />
+      <FormField name="street1" required autoComplete={getAutoComplete('address-line1')} mb="sm" />
+      <FormField name="street2" autoComplete={getAutoComplete('address-line2')} mb="sm" />
+      <FormField name="countryId" required autoComplete={getAutoComplete('country')} mb="sm">
         {({ form, field }) => (
           <CountrySelector {...field} items={countries} onChange={x => form.setFieldValue(field.name, x)} />
         )}
       </FormField>
-      <FormField
-        name="postcode"
-        required
-        autoComplete={getAutoComplete('postal-code')}
-        gridArea={AddressFormArea.postCode}
-      />
-      <FormField
-        name="city"
-        required
-        autoComplete={getAutoComplete('address-level2')}
-        gridArea={AddressFormArea.city}
-      />
-      <FormField name="telephone" required autoComplete={getAutoComplete('tel')} gridArea={AddressFormArea.phone} />
-      <Box gridArea={AddressFormArea.submit}>
+      <FormField name="postcode" required autoComplete={getAutoComplete('postal-code')} mb="sm" />
+      <FormField name="city" required autoComplete={getAutoComplete('address-level2')} mb="sm" />
+      <FormField name="telephone" required autoComplete={getAutoComplete('tel')} mb="sm" />
+      <Box mb="sm">
         <Button type="submit">{submitLabel}</Button>
       </Box>
     </Form>
@@ -92,6 +37,8 @@ const AddressForm = ({ countries = [], submitLabel = 'Save', id = '', autoComple
 AddressForm.propTypes = {
   id: PropTypes.string.isRequired,
   submitLabel: PropTypes.string,
+  // TODO: allow cancel
+  // TODO: ask default
   countries: PropTypes.arrayOf(
     PropTypes.shape({
       localName: PropTypes.string,
