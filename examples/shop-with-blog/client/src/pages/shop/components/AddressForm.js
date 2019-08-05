7488dcc2814777258f30edc3282af94a5961afab
Nimo Beeren <nimo@deity.io>
2019-08-05 10:21:30 +0200

    feature: support asking if it is a default address

--- examples/shop-with-blog/client/src/pages/shop/components/AddressForm.js
+++ examples/shop-with-blog/client/src/pages/shop/components/AddressForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CountrySelector } from '@deity/falcon-ecommerce-uikit';
+import { CheckboxFormField, CountrySelector } from '@deity/falcon-ecommerce-uikit';
 import { Form, FormField } from '@deity/falcon-ui-kit';
 import { Box, Button } from '@deity/falcon-ui';
 
@@ -11,11 +11,19 @@ const addressFormLayout = {
   }
 };
 
-const AddressForm = ({ countries = [], submitLabel = 'Save', id = '', autoCompleteSection }) => {
+const AddressForm = ({ countries = [], submitLabel = 'Save', askDefault, id = '', autoCompleteSection }) => {
   const getAutoComplete = attribute => [autoCompleteSection, attribute].filter(x => x).join(' ');
+
+  const askDefaultFields = askDefault ? (
+    <Box mb="sm">
+      <CheckboxFormField name="defaultShipping" />
+      <CheckboxFormField name="defaultBilling" />
+    </Box>
+  ) : null;
 
   return (
     <Form id={id} defaultTheme={addressFormLayout} i18nId="addressForm" height="400px">
+      {askDefaultFields}
       <FormField name="firstname" required autoComplete={getAutoComplete('given-name')} mb="sm" />
       <FormField name="lastname" required autoComplete={getAutoComplete('family-name')} mb="sm" />
       <FormField name="street1" required autoComplete={getAutoComplete('address-line1')} mb="sm" />
@@ -39,7 +47,7 @@ AddressForm.propTypes = {
   id: PropTypes.string.isRequired,
   submitLabel: PropTypes.string,
   // TODO: allow cancel
-  // TODO: ask default
+  askDefault: PropTypes.bool,
   countries: PropTypes.arrayOf(
     PropTypes.shape({
       localName: PropTypes.string,
