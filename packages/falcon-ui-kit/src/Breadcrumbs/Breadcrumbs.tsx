99b63ac642641c275b81b14d0f69c6738c7ed518
tutok <walas.marcin@gmail.com>
2019-08-06 09:28:01 +0200

    feat(front-kit-3): added themed props into Breadcrumbs

--- packages/falcon-ui-kit/src/Breadcrumbs/Breadcrumbs.tsx
+++ packages/falcon-ui-kit/src/Breadcrumbs/Breadcrumbs.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Breadcrumbs as FalconUiBreadcrumbs } from '@deity/falcon-ui';
+import PropTypes from 'prop-types';
+import { Breadcrumbs as FalconUiBreadcrumbs, ThemedComponentProps } from '@deity/falcon-ui';
 import { BreadcrumbItem, BreadcrumbItemProps } from './BreadcrumbItem';
 
 export type BreadcrumbsProps = {
   items: BreadcrumbItemProps[];
-};
+} & ThemedComponentProps<any>;
 
-export const Breadcrumbs: React.SFC<BreadcrumbsProps> = ({ items }) => (
-  <FalconUiBreadcrumbs>
+export const Breadcrumbs: React.SFC<BreadcrumbsProps> = ({ items, ...rest }) => (
+  <FalconUiBreadcrumbs {...(rest as any)}>
     {items.map(item => (
       <BreadcrumbItem key={item.name} {...item} />
     ))}
   </FalconUiBreadcrumbs>
 );
+Breadcrumbs.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object.isRequired)
+};
 Breadcrumbs.defaultProps = {
   items: []
 };
