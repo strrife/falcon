ef73412b6fa5d36e47fbfe0da253a0f96406af06
tutok <walas.marcin@gmail.com>
2019-08-06 16:26:01 +0200

    feat(front-kit-3): added typing for CategoryWithProductsQuery

--- packages/falcon-shop-data/src/Category/CategoryWithProductsQuery.tsx
+++ packages/falcon-shop-data/src/Category/CategoryWithProductsQuery.tsx
@@ -1,15 +1,16 @@
 import gql from 'graphql-tag';
-import { Query } from '@deity/falcon-data';
-import { Category } from '@deity/falcon-shop-extension';
+import { Query, Pagination, PaginationInput, Aggregation, SortOrderInput } from '@deity/falcon-data';
+import { Category, Product, FilterInput } from '@deity/falcon-shop-extension';
 
 export const GET_CATEGORY_WITH_PRODUCTS = gql`
   query CategoryWithProducts(
-    $categoryId: String!
+    $categoryId: ID
     $sort: SortOrderInput
     $pagination: PaginationInput
     $filters: [FilterInput]
   ) {
     category(id: $categoryId) {
+      id
       name
       products(sort: $sort, pagination: $pagination, filters: $filters) {
         items {
@@ -43,6 +44,21 @@ export const GET_CATEGORY_WITH_PRODUCTS = gql`
   }
 `;
 
+export type CategoryWithProductsResponse = Pick<Category, 'id' | 'name'> & {
+  products: {
+    items: Pick<Product, 'id' | 'name' | 'price' | 'thumbnail' | 'urlPath'>[];
+    pagination: Pick<Pagination, 'currentPage' | 'totalItems' | 'nextPage'>;
+    aggregations: Pick<Aggregation, 'field' | 'type' | 'title' | 'buckets'>;
+  };
+};
+
+export type CategoryWithProductsVariables = {
+  categoryId?: string;
+  sort?: SortOrderInput;
+  pagination?: PaginationInput;
+  filters?: FilterInput[];
+};
+
 const fetchMore = (data: any, apolloFetchMore: any) =>
   apolloFetchMore({
     variables: {
@@ -69,7 +85,7 @@ const fetchMore = (data: any, apolloFetchMore: any) =>
     }
   });
 
-export class CategoryWithProductsQuery extends Query<any> {
+export class CategoryWithProductsQuery extends Query<CategoryWithProductsResponse, CategoryWithProductsVariables> {
   static defaultProps = {
     query: GET_CATEGORY_WITH_PRODUCTS,
     fetchPolicy: 'cache-and-network',
