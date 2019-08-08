27b05c87f3c8837dc848a1f3cad15980918fa1da
Nimo Beeren <nimo@deity.io>
2019-08-08 15:23:25 +0200

    improvement: connect with formik to get submit state

--- packages/falcon-ecommerce-uikit/src/Forms/FormSubmit.tsx
+++ packages/falcon-ecommerce-uikit/src/Forms/FormSubmit.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { connect, FormikProps } from 'formik';
 import { Box, Button, ThemedComponentProps } from '@deity/falcon-ui';
 
-export const FormSubmit: React.SFC<{ submitting: boolean; value: string } & ThemedComponentProps> = ({
-  submitting,
+export type FormSubmitProps = {
+  value: string;
+} & InjectedProps;
+
+type InjectedProps = {
+  formik?: FormikProps<{}>;
+};
+
+const FormSubmitWithFormik: React.SFC<FormSubmitProps & ThemedComponentProps> = ({
   value,
+  formik,
   children,
   ...rest
 }) => (
-  <Box justifySelf="end" mt="md" {...rest as any}>
+  <Box justifySelf="end" mt="md" {...(rest as any)}>
     {children || (
-      <Button type="submit" variant={submitting ? 'loader' : undefined}>
+      <Button type="submit" variant={formik.isSubmitting ? 'loader' : undefined}>
         {value}
       </Button>
     )}
   </Box>
 );
+
+export const FormSubmit = connect(FormSubmitWithFormik);
