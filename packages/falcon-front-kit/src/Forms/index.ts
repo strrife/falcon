f4e82952137ed9b6af95ff9842234489f30de1e5
tutok <walas.marcin@gmail.com>
2019-08-09 08:24:18 +0200

    feat(front-kit-3): extracted common props type for formProviders

--- packages/falcon-front-kit/src/Forms/index.ts
+++ packages/falcon-front-kit/src/Forms/index.ts
@@ -1,5 +1,6 @@
 export * from './FormContext';
 export * from './Form';
+export * from './FormProvider';
 export * from './Field';
 export * from './validators';
 export * from './inputTypeToDefaultValidatorsMapper';
