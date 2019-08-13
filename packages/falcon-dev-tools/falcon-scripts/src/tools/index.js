41998c945a378d9b249bd8017183539951e5b2ce
tutok <walas.marcin@gmail.com>
2019-08-13 11:05:18 +0200

    feat(falcon-script-watch):  I think we need to create single place for config

--- packages/falcon-dev-tools/falcon-scripts/src/tools/index.js
+++ packages/falcon-dev-tools/falcon-scripts/src/tools/index.js
@@ -1,5 +1,11 @@
 const paths = require('./paths');
 
+const config = {
+  fileExtensions: ['.tsx', '.ts', '.jsx', '.js'],
+  babelConfigPath: require.resolve('../babel/babel.config')
+};
+
 module.exports = {
+  config,
   paths
 };
