130982411f0041188637e418827ddb215a104519
tutok <walas.marcin@gmail.com>
2019-08-23 12:14:02 +0200

    feat(front-kit-3): added todo

--- packages/falcon-front-kit/src/Forms/FormProvider.ts
+++ packages/falcon-front-kit/src/Forms/FormProvider.ts
@@ -1,6 +1,7 @@
 import { FormikConfig, FormikValues } from 'formik';
 
 export type FormProviderProps<Values = FormikValues> = {
+  /** Invoked when form is successfully submit, TODO: consider rename to `onSuccess` ? */
   onSubmit?: () => any | Promise<any>;
   initialValues?: Values;
 } & Pick<FormikConfig<Values>, 'children'>;
