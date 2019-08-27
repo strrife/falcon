2e6b73d504b8bd0698d5cfd332f2e62cfef4edf3
tutok <walas.marcin@gmail.com>
2019-08-27 16:34:40 +0200

    feat(front-kit-3): moved blog post into example

--- examples/shop-with-blog/client/src/pages/blog/Post.js
+++ examples/shop-with-blog/client/src/pages/blog/Post.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { BlogPostQuery } from '@deity/falcon-blog-data';
-import { BlogPost } from '@deity/falcon-ecommerce-uikit';
+import { Link as RouterLink } from 'react-router-dom';
+import { H1, Breadcrumbs, Breadcrumb, Link } from '@deity/falcon-ui';
+import { T } from '@deity/falcon-i18n';
+import { FormattedDate, BlogPostLayout } from '@deity/falcon-ui-kit';
+import { CMSContent } from './CmsContent';
 
 const Post = ({ path }) => (
-  <BlogPostQuery variables={{ path }}>{postProps => <BlogPost {...postProps} />}</BlogPostQuery>
+  <BlogPostQuery variables={{ path }}>
+    {({ blogPost }) => (
+      <BlogPostLayout>
+        <Breadcrumbs my="md" alignSelf="flex-start">
+          <Breadcrumb key="index">
+            <Link to="/blog" as={RouterLink}>
+              <T id="blog.title" />
+            </Link>
+          </Breadcrumb>
+          <Breadcrumb key="post">{blogPost.title}</Breadcrumb>
+        </Breadcrumbs>
+
+        <H1>{blogPost.title}</H1>
+        <FormattedDate mb="xl" value={blogPost.date} />
+        <CMSContent html={blogPost.content} />
+      </BlogPostLayout>
+    )}
+  </BlogPostQuery>
 );
 
 export default Post;
