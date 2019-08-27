c4c62633e032d96f8ca7e5dd069947fac158dd5d
tutok <walas.marcin@gmail.com>
2019-08-27 15:23:23 +0200

    feat(front-kit-3): added ts types for blog into blog extension

--- packages/falcon-blog-extension/src/types.ts
+++ packages/falcon-blog-extension/src/types.ts
@@ -1,1 +1,24 @@
-import { Pagination, Aggregation } from '@deity/falcon-data';
+import { Pagination } from '@deity/falcon-data';
+
+export type BlogPostList = {
+  items: BlogPost[];
+  pagination?: Pagination;
+};
+
+export type BlogPost = {
+  id: number;
+  date: string;
+  modified?: string;
+  title: string;
+  content: string;
+  excerpt?: string;
+  slug?: string;
+  status?: string;
+  image?: BlogPostImage;
+  related?: BlogPostList;
+};
+
+export type BlogPostImage = {
+  url: string;
+  description?: string;
+};
