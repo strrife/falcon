e0f5551ae6a15a51020e2a5842093361ce32decf
tutok <walas.marcin@gmail.com>
2019-09-06 16:15:39 +0200

    feat(501): improved the way of getting default validators

--- packages/falcon-front-kit/src/Forms/index.ts
+++ packages/falcon-front-kit/src/Forms/index.ts
@@ -3,6 +3,5 @@ export * from './Form';
 export * from './FormProvider';
 export * from './Field';
 export * from './validators';
-export * from './inputTypeToDefaultValidatorsMapper';
 export * from './getDefaultInputValidators';
 export * from './IValidator';
