1346013d3597190b9abfdfe9dd5f42fd4972ba0d
tutok <walas.marcin@gmail.com>
2019-09-09 10:17:27 +0200

    feat(501): extracted ValidatorProps

--- packages/falcon-front-kit/src/Forms/IValidator.ts
+++ packages/falcon-front-kit/src/Forms/IValidator.ts
@@ -1,5 +1,13 @@
 import { TranslationFunction } from '@deity/falcon-i18n';
 
+export type ValidatorProps = {
+  name: string;
+  label: string;
+  value?: any;
+  formI18nId?: string;
+  t: TranslationFunction;
+};
+
 export interface IValidator {
-  (props: { value?: any; label: string; t: TranslationFunction }): string | undefined;
+  (props: ValidatorProps): string | undefined;
 }
