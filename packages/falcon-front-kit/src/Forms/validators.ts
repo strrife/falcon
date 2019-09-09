ba3bf67daa543f7aaf58656f91402478296559e5
tutok <walas.marcin@gmail.com>
2019-09-09 12:38:59 +0200

    feat(501): simplified validator - removed translation responsibility from them

--- packages/falcon-front-kit/src/Forms/validators.ts
+++ packages/falcon-front-kit/src/Forms/validators.ts
@@ -1,4 +1,3 @@
-import { capitalize } from './string';
 import { IValidator } from './IValidator';
 
 const VALID_EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -6,38 +5,25 @@ const VALID_PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
 
 const isValueProvided = x => (x !== '' && x !== undefined && x !== null) || (Array.isArray(x) && (x as any[]).length);
 
-const getErrorI18nId: (error: string, props?: { formI18nId?: string; name: string }) => string | string[] = (
-  error,
-  props
-) => {
-  const DEFAULT = `formError.${error}`;
-
-  if (props && props.formI18nId) {
-    return [`${props.formI18nId}.${props.name}Field${capitalize(error)}`, DEFAULT];
-  }
-
-  return DEFAULT;
-};
-
-export const requiredValidator: IValidator = ({ value, name, label, formI18nId, t }) => {
+export const requiredValidator: IValidator = ({ value }) => {
   if (!isValueProvided(value)) {
-    return t(getErrorI18nId('required', { formI18nId, name }), { label });
+    return { errorI18nId: 'required' };
   }
 
   return undefined;
 };
 
-export const emailValidator: IValidator = ({ value, label, t }) => {
+export const emailValidator: IValidator = ({ value }) => {
   if (isValueProvided(value) && !VALID_EMAIL_REGEX.test(value.toLowerCase())) {
-    return t('formError.invalid', { label });
+    return { errorI18nId: 'invalid' };
   }
 
   return undefined;
 };
 
-export const passwordValidator: IValidator = ({ value, label, t }) => {
+export const passwordValidator: IValidator = ({ value }) => {
   if (isValueProvided(value) && !VALID_PASSWORD_REGEX.test(value)) {
-    return t('formError.uncomplexPassword', { label });
+    return { errorI18nId: 'uncomplexPassword' };
   }
 
   return undefined;
@@ -48,14 +34,14 @@ export function lengthValidator(min: number, max?: number): IValidator {
     throw new Error(`value of 'min' can not be grater that 'max'!`);
   }
 
-  return ({ value, label, t }) => {
+  return ({ value }) => {
     if (isValueProvided(value) && typeof value === 'string') {
       if (value.length < min) {
-        return t('formError.toShort', { label, min });
+        return { errorI18nId: 'toShort', min, ...(max ? { max } : {}) };
       }
 
       if (max && value.length > max) {
-        return t('formError.toLong', { label, min });
+        return { errorI18nId: 'toLong', min, max };
       }
     }
 
@@ -68,14 +54,14 @@ export function rangeValidator(min: number, max?: number): IValidator {
     throw new Error(`value of 'min' can not be grater that 'max'!`);
   }
 
-  return ({ value, label, t }) => {
+  return ({ value }) => {
     if (isValueProvided(value) && typeof value === 'number') {
       if (value < min) {
-        return t('formError.belowRange', { label, min });
+        return { errorI18nId: 'belowRange', min, ...(max ? { max } : {}) };
       }
 
       if (max && value > max) {
-        return t('formError.aboveRange', { label, max });
+        return { errorI18nId: 'aboveRange', min, max };
       }
     }
 
