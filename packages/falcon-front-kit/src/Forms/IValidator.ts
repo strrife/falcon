7f51eeea03c948afe87caca75ed84089031a1e77
tutok <walas.marcin@gmail.com>
2019-09-09 12:38:04 +0200

    feat(501): introduced I18nFieldValidationError

--- packages/falcon-front-kit/src/Forms/IValidator.ts
+++ packages/falcon-front-kit/src/Forms/IValidator.ts
@@ -2,12 +2,21 @@ import { TranslationFunction } from '@deity/falcon-i18n';
 
 export type ValidatorProps = {
   name: string;
-  label: string;
+  label?: string;
   value?: any;
   formI18nId?: string;
   t: TranslationFunction;
 };
 
 export interface IValidator {
-  (props: ValidatorProps): string | undefined;
+  (props: ValidatorProps): FieldValidationError;
+}
+
+export type FieldValidationError = undefined | string | I18nFieldValidationError;
+export type I18nFieldValidationError = {
+  errorI18nId: string;
+} & { [key in string]: any };
+
+export function isI18nFieldValidationError(error: FieldValidationError): error is I18nFieldValidationError {
+  return typeof error === 'object' && (error as I18nFieldValidationError).errorI18nId !== undefined;
 }
