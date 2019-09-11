54aa456df87c25366d81dd2c8130d4cc25094b0f
tutok <walas.marcin@gmail.com>
2019-09-11 18:14:37 +0200

    feat(sidebars-mutations): extracted form provider for reset pwd

--- packages/falcon-front-kit/src/Account/index.ts
+++ packages/falcon-front-kit/src/Account/index.ts
@@ -2,3 +2,4 @@ export * from './SignInFormProvider';
 export * from './SignOutProvider';
 export * from './SignUpFormProvider';
 export * from './ForgotPasswordFormProvider';
+export * from './ResetPasswordFormProvider';
