ae707466aac1191f35c8dc6d5a8925274f5d0bf8
tutok <walas.marcin@gmail.com>
2019-09-11 10:50:37 +0200

    feat(sidebars-mutations): aligned sidebarcontents

--- examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarContents.js
+++ examples/shop-with-blog/client/src/pages/shop/components/Sidebar/SidebarContents.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { I18n } from '@deity/falcon-i18n';
 import { MiniCartQuery } from '@deity/falcon-shop-data';
+import { Router } from '@deity/falcon-front-kit';
 import { Box, Divider } from '@deity/falcon-ui';
 import {
-  CloseSidebarMutation,
-  OpenSidebarMutation,
   SidebarLayout,
   NewAccount,
   SignInForm,
   SignUpForm,
+  EmptyMiniCart,
   MiniCart,
   ForgotPasswordForm
 } from '@deity/falcon-ui-kit';
+import { CloseSidebarMutation, OpenSidebarMutation } from 'src/components/Sidebar';
 import { SIDEBAR_CONTENT_TYPES } from './SidebarQuery';
 
 export default ({ contentType }) => {
@@ -29,40 +30,61 @@ export default ({ contentType }) => {
   // using hidden attribute will cause react to consider rendering it as low priority
   // (in version > 16.6) - https://github.com/oliviertassinari/react-swipeable-views/issues/453#issuecomment-417939459
   return (
-    <I18n>
-      {t => (
-        <React.Fragment>
-          <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.cart}>
-            <SidebarLayout title={t('miniCart.title')}>
-              <MiniCartQuery>{data => <MiniCart {...data} />}</MiniCartQuery>
-            </SidebarLayout>
-          </ContentBox>
-          <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.account}>
-            <SidebarLayout title={t('signIn.title')}>
-              <CloseSidebarMutation>
-                {closeSidebar => <SignInForm id="sign-in-sidebar" onSuccess={closeSidebar} />}
-              </CloseSidebarMutation>
-              <Divider my="lg" />
-              <NewAccount />
-            </SidebarLayout>
-          </ContentBox>
-          <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.signUp}>
-            <SidebarLayout title={t('signUp.title')}>
-              <OpenSidebarMutation>
-                {openSidebarMutation => (
-                  <SignUpForm onSuccess={() => openSidebarMutation({ variables: { contentType: 'account' } })} />
-                )}
-              </OpenSidebarMutation>
-            </SidebarLayout>
-          </ContentBox>
-          <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.forgotPassword}>
-            <SidebarLayout title={t('forgotPassword.title')}>
-              <ForgotPasswordForm />
-            </SidebarLayout>
-          </ContentBox>
-        </React.Fragment>
+    <Router>
+      {({ history }) => (
+        <CloseSidebarMutation>
+          {closeSidebar => (
+            <I18n>
+              {t => (
+                <React.Fragment>
+                  <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.cart}>
+                    <SidebarLayout title={t('miniCart.title')}>
+                      <MiniCartQuery>
+                        {({ cart = { items: [] } }) =>
+                          cart.items.length > 0 ? (
+                            <MiniCart
+                              items={cart.items}
+                              onCheckout={() => closeSidebar().then(() => history.push('/checkout'))}
+                            />
+                          ) : (
+                            <EmptyMiniCart
+                              onGoShopping={() => closeSidebar().then(() => history.push('/what-is-new.html'))}
+                            />
+                          )
+                        }
+                      </MiniCartQuery>
+                    </SidebarLayout>
+                  </ContentBox>
+                  <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.account}>
+                    <SidebarLayout title={t('signIn.title')}>
+                      <SignInForm id="sign-in-sidebar" onSuccess={closeSidebar} />
+                      <Divider my="lg" />
+                      <NewAccount />
+                    </SidebarLayout>
+                  </ContentBox>
+                  <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.signUp}>
+                    <SidebarLayout title={t('signUp.title')}>
+                      <OpenSidebarMutation>
+                        {openSidebarMutation => (
+                          <SignUpForm
+                            onSuccess={() => openSidebarMutation({ variables: { contentType: 'account' } })}
+                          />
+                        )}
+                      </OpenSidebarMutation>
+                    </SidebarLayout>
+                  </ContentBox>
+                  <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.forgotPassword}>
+                    <SidebarLayout title={t('forgotPassword.title')}>
+                      <ForgotPasswordForm />
+                    </SidebarLayout>
+                  </ContentBox>
+                </React.Fragment>
+              )}
+            </I18n>
+          )}
+        </CloseSidebarMutation>
       )}
-    </I18n>
+    </Router>
   );
 };
 
