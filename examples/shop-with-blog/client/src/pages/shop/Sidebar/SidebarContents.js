0634caa63066c49c45f26ec64871ebdb48e75c73
tutok <walas.marcin@gmail.com>
2019-09-11 18:49:01 +0200

    feat(sidebars-mutations): using SIDEBAR_CONTENT_TYPE

--- examples/shop-with-blog/client/src/pages/shop/Sidebar/SidebarContents.js
+++ examples/shop-with-blog/client/src/pages/shop/Sidebar/SidebarContents.js
@@ -12,12 +12,12 @@ import {
   MiniCart,
   ForgotPasswordForm
 } from '@deity/falcon-ui-kit';
-import { CloseSidebarMutation, OpenSidebarMutation, SIDEBAR_CONTENT_TYPES } from 'src/components/Sidebar';
+import { CloseSidebarMutation, OpenSidebarMutation, SIDEBAR_CONTENT_TYPE } from 'src/components/Sidebar';
 
 export default ({ contentType }) => {
   // if there is no content type provided it means that sidebar contents should be rendered as hidden
   // if unrecognized content type is provided add warning about it
-  if (contentType && !SIDEBAR_CONTENT_TYPES[contentType]) {
+  if (contentType && !SIDEBAR_CONTENT_TYPE[contentType]) {
     const message = `Unrecognized sidebar content type: ${contentType}`;
     if (process.env.NODE_ENV !== 'production') {
       throw new Error(message);
@@ -38,7 +38,7 @@ export default ({ contentType }) => {
                 <I18n>
                   {t => (
                     <React.Fragment>
-                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.cart}>
+                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.cart}>
                         <SidebarLayout title={t('miniCart.title')}>
                           <MiniCartQuery>
                             {({ cart = { items: [] } }) =>
@@ -56,31 +56,31 @@ export default ({ contentType }) => {
                           </MiniCartQuery>
                         </SidebarLayout>
                       </ContentBox>
-                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.account}>
+                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.account}>
                         <SidebarLayout title={t('signIn.title')}>
                           <SignInForm
                             id="sign-in-sidebar"
                             onSuccess={closeSidebar}
-                            onForgotPassword={() => openSidebar({ variables: { contentType: 'forgotPassword' } })}
+                            onForgotPassword={() =>
+                              openSidebar({ variables: { contentType: SIDEBAR_CONTENT_TYPE.forgotPassword } })
+                            }
                           />
                           <Divider my="lg" />
                           <NewAccount
-                            onCreateNewAccount={() => openSidebar({ variables: { contentType: 'signUp' } })}
+                            onCreateNewAccount={() =>
+                              openSidebar({ variables: { contentType: SIDEBAR_CONTENT_TYPE.signUp } })
+                            }
                           />
                         </SidebarLayout>
                       </ContentBox>
-                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.signUp}>
+                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.signUp}>
                         <SidebarLayout title={t('signUp.title')}>
-                          <OpenSidebarMutation>
-                            {openSidebarMutation => (
-                              <SignUpForm
-                                onSuccess={() => openSidebarMutation({ variables: { contentType: 'account' } })}
-                              />
-                            )}
-                          </OpenSidebarMutation>
+                          <SignUpForm
+                            onSuccess={() => openSidebar({ variables: { contentType: SIDEBAR_CONTENT_TYPE.account } })}
+                          />
                         </SidebarLayout>
                       </ContentBox>
-                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPES.forgotPassword}>
+                      <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.forgotPassword}>
                         <SidebarLayout title={t('forgotPassword.title')}>
                           <ForgotPasswordForm />
                         </SidebarLayout>
