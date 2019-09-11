c6ee0249b50057b56bd78e52a624bcedb7201ce3
tutok <walas.marcin@gmail.com>
2019-09-11 18:53:46 +0200

    feat(sidebars-mutation): extracted InvalidResetPasswordToken

--- packages/falcon-ui-kit/src/Account/index.ts
+++ packages/falcon-ui-kit/src/Account/index.ts
@@ -1,5 +1,6 @@
 export * from './ForgotPasswordForm';
 export * from './ResetPassword';
+export * from './InvalidResetPasswordToken';
 export * from './SignInIcon';
 export * from './SignInForm';
 export * from './AccountIcon';
