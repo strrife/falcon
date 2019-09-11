6f484409a76c60888aa1ff60b0a1974def00b75f
tutok <walas.marcin@gmail.com>
2019-09-11 10:36:00 +0200

    feat(sidebars-mutations): reworked minicart

--- packages/falcon-ui-kit/src/MiniCart/MiniCart.tsx
+++ packages/falcon-ui-kit/src/MiniCart/MiniCart.tsx
@@ -1,76 +1,39 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Icon, List, ListItem, Box, H3, DefaultThemeProps, Text, Divider, Button, FlexLayout } from '@deity/falcon-ui';
+import { Icon, List, ListItem, Box, Divider, Button } from '@deity/falcon-ui';
 import { LocaleProvider } from '@deity/falcon-front-kit';
 import { T } from '@deity/falcon-i18n';
 import { MiniCartResponse } from '@deity/falcon-shop-data';
-import { CloseSidebarMutation } from '../Sidebar';
-import { toGridTemplate, prettyScrollbars } from '../helpers';
+import { prettyScrollbars } from '../helpers';
 import { MiniCartProduct } from './MiniCartProduct';
+import { MiniCartLayout, MiniCartLayoutArea } from './MiniCartLayout';
 
-export const MiniCartLayoutArea = {
-  items: 'items',
-  cta: 'cta'
+export type MiniCartProps = {
+  onCheckout: Function;
+  items: MiniCartResponse['cart']['items'][0][];
 };
+export const MiniCart: React.SFC<MiniCartProps> = ({ onCheckout, items }) => (
+  <MiniCartLayout>
+    <Box gridArea={MiniCartLayoutArea.items} css={props => ({ ...prettyScrollbars(props.theme) })}>
+      <LocaleProvider>
+        <List>
+          {items.map((item, index: number) => {
+            const isLastItem = index === items.length - 1;
 
-const miniCartTheme: DefaultThemeProps = {
-  miniCart: {
-    display: 'grid',
-    gridRowGap: 'md',
-
-    // prettier-ignore
-    gridTemplate: toGridTemplate([
-      ['1fr'                          ],
-      [MiniCartLayoutArea.items, '1fr'],
-      [MiniCartLayoutArea.cta         ]
-    ]),
-    css: {
-      width: '100%',
-      height: '100%'
-    }
-  }
-};
-
-const MiniCartProducts: React.SFC<any> = ({ products }) => (
-  <List>
-    {products.map((product: any, index: number) => (
-      <ListItem pb="none" key={product.sku}>
-        <MiniCartProduct product={product} />
-        {index < products.length - 1 && <Divider my="md" />}
-      </ListItem>
-    ))}
-  </List>
-);
-
-export const MiniCart: React.SFC<MiniCartResponse> = ({ cart: { quoteCurrency, items } }) => (
-  <CloseSidebarMutation>
-    {closeSidebar => (
-      <LocaleProvider currency={quoteCurrency}>
-        <Box defaultTheme={miniCartTheme}>
-          <Box gridArea={MiniCartLayoutArea.items} css={props => ({ ...prettyScrollbars(props.theme) })}>
-            <MiniCartProducts products={items} />
-            {!items.length && (
-              <FlexLayout alignItems="center" flexDirection="column">
-                <Text fontSize="md">
-                  <T id="miniCart.empty" />
-                </Text>
-                <Button onClick={() => closeSidebar()} mt="lg">
-                  <T id="miniCart.goShoppingButton" />
-                </Button>
-              </FlexLayout>
-            )}
-          </Box>
-
-          {items.length > 0 && (
-            <Box gridArea={MiniCartLayoutArea.cta} py="sm" bgFullWidth="secondaryLight">
-              <Button as={RouterLink} to="/cart" onClick={() => closeSidebar()} css={{ width: '100%' }} height="xl">
-                <Icon stroke="white" size="md" mr="xs" src="lock" />
-                <T id="miniCart.checkout" />
-              </Button>
-            </Box>
-          )}
-        </Box>
+            return (
+              <ListItem key={item.sku} pb="none">
+                <MiniCartProduct product={item} />
+                {!isLastItem && <Divider my="md" />}
+              </ListItem>
+            );
+          })}
+        </List>
       </LocaleProvider>
-    )}
-  </CloseSidebarMutation>
+    </Box>
+    <Box gridArea={MiniCartLayoutArea.cta} py="sm" bgFullWidth="secondaryLight">
+      <Button onClick={() => onCheckout()} css={{ width: '100%' }} height="xl">
+        <Icon stroke="white" size="md" mr="xs" src="lock" />
+        <T id="miniCart.checkout" />
+      </Button>
+    </Box>
+  </MiniCartLayout>
 );
