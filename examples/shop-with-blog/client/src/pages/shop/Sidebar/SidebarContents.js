11b6f89b0e0f144ef59b4e95f24aacfc2a949253
tutok <walas.marcin@gmail.com>
2019-09-12 08:57:29 +0200

    feat(sidebars-mutations): using Deferred on SidebarContents

--- examples/shop-with-blog/client/src/pages/shop/Sidebar/SidebarContents.js
+++ examples/shop-with-blog/client/src/pages/shop/Sidebar/SidebarContents.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { I18n } from '@deity/falcon-i18n';
 import { MiniCartQuery } from '@deity/falcon-shop-data';
 import { Router } from '@deity/falcon-front-kit';
-import { Box, Divider } from '@deity/falcon-ui';
+import { Divider } from '@deity/falcon-ui';
 import {
   SidebarLayout,
   NewAccount,
@@ -10,7 +10,8 @@ import {
   SignUpForm,
   EmptyMiniCart,
   MiniCart,
-  ForgotPasswordForm
+  ForgotPasswordForm,
+  Deferred
 } from '@deity/falcon-ui-kit';
 import { SIDEBAR_CONTENT_TYPE } from 'src/components/Sidebar';
 
@@ -26,65 +27,65 @@ export default ({ contentType, open, close }) => {
     console.error(message);
   }
 
-  // using hidden attribute will cause react to consider rendering it as low priority
-  // (in version > 16.6) - https://github.com/oliviertassinari/react-swipeable-views/issues/453#issuecomment-417939459
   return (
     <Router>
       {({ history }) => (
         <I18n>
           {t => (
             <React.Fragment>
-              <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.cart}>
-                <SidebarLayout title={t('miniCart.title')}>
-                  <MiniCartQuery>
-                    {({ data: { cart = { items: [] } } }) =>
-                      cart.items.length > 0 ? (
-                        <MiniCart items={cart.items} onCheckout={() => close().then(() => history.push('/checkout'))} />
-                      ) : (
-                        <EmptyMiniCart onGoShopping={() => close().then(() => history.push('/what-is-new.html'))} />
-                      )
-                    }
-                  </MiniCartQuery>
-                </SidebarLayout>
-              </ContentBox>
-              <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.account}>
-                <SidebarLayout title={t('signIn.title')}>
-                  <SignInForm
-                    id="sign-in-sidebar"
-                    onSuccess={close}
-                    onForgotPassword={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.forgotPassword } })}
-                  />
-                  <Divider my="lg" />
-                  <NewAccount
-                    onCreateNewAccount={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.signUp } })}
-                  />
-                </SidebarLayout>
-              </ContentBox>
-              <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.signUp}>
-                <SidebarLayout title={t('signUp.title')}>
-                  <SignUpForm onSuccess={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.account } })} />
-                </SidebarLayout>
-              </ContentBox>
-              <ContentBox current={contentType} contentType={SIDEBAR_CONTENT_TYPE.forgotPassword}>
-                <SidebarLayout title={t('forgotPassword.title')}>
-                  <ForgotPasswordForm />
-                </SidebarLayout>
-              </ContentBox>
+              <Deferred
+                as={SidebarLayout}
+                title={t('miniCart.title')}
+                type={SIDEBAR_CONTENT_TYPE.cart}
+                until={contentType}
+              >
+                <MiniCartQuery>
+                  {({ data: { cart = { items: [] } } }) =>
+                    cart.items.length > 0 ? (
+                      <MiniCart items={cart.items} onCheckout={() => close().then(() => history.push('/checkout'))} />
+                    ) : (
+                      <EmptyMiniCart onGoShopping={() => close().then(() => history.push('/what-is-new.html'))} />
+                    )
+                  }
+                </MiniCartQuery>
+              </Deferred>
+              <Deferred
+                as={SidebarLayout}
+                title={t('signIn.title')}
+                type={SIDEBAR_CONTENT_TYPE.account}
+                until={contentType}
+              >
+                <SignInForm
+                  id="sign-in-sidebar"
+                  onSuccess={close}
+                  onForgotPassword={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.forgotPassword } })}
+                />
+                <Divider my="lg" />
+                <NewAccount
+                  onCreateNewAccount={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.signUp } })}
+                />
+              </Deferred>
+
+              <Deferred
+                as={SidebarLayout}
+                title={t('signUp.title')}
+                type={SIDEBAR_CONTENT_TYPE.signUp}
+                until={contentType}
+              >
+                <SignUpForm onSuccess={() => open({ variables: { contentType: SIDEBAR_CONTENT_TYPE.account } })} />
+              </Deferred>
+              <Deferred
+                as={SidebarLayout}
+                title={t('forgotPassword.title')}
+                type={SIDEBAR_CONTENT_TYPE.forgotPassword}
+                until={contentType}
+              >
+                <ForgotPasswordForm />
+              </Deferred>
             </React.Fragment>
           )}
         </I18n>
       )}
     </Router>
-  );
-};
-
-const ContentBox = ({ current, contentType, children, ...rest }) => {
-  const hidden = current !== contentType;
-  const key = contentType + hidden;
-
-  return (
-    <Box key={key} hidden={hidden} css={{ height: '100%' }} {...rest}>
-      {children}
-    </Box>
   );
 };
