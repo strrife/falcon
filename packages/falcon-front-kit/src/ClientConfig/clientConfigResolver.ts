32df8e116751bfce5431d6d8406ed86bff69a557
tutok <walas.marcin@gmail.com>
2019-09-13 12:25:12 +0200

    feat(557): improved types

--- packages/falcon-front-kit/src/ClientConfig/clientConfigResolver.ts
+++ packages/falcon-front-kit/src/ClientConfig/clientConfigResolver.ts
@@ -3,7 +3,7 @@ import { apolloStateToObject } from '../Apollo';
 
 const CLIENT_CONFIG_PATH = '$ROOT_QUERY.config';
 
-export const getClientConfig = (apolloState: any, key?: string) => {
+export const getClientConfig = (apolloState: object, key?: string) => {
   key = key ? `${CLIENT_CONFIG_PATH}.${key}` : CLIENT_CONFIG_PATH;
 
   return apolloStateToObject(apolloState, key);
