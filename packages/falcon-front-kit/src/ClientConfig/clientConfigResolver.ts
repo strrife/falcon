94174b69038f08592bcdc9ee40cee7265890333f
tutok <walas.marcin@gmail.com>
2019-09-13 11:44:16 +0200

    feat(557): cleanup

--- packages/falcon-front-kit/src/ClientConfig/clientConfigResolver.ts
+++ packages/falcon-front-kit/src/ClientConfig/clientConfigResolver.ts
@@ -3,8 +3,11 @@ import { apolloStateToObject } from '../Apollo';
 
 const CLIENT_CONFIG_PATH = '$ROOT_QUERY.config';
 
-export const getClientConfigResolver: IFieldResolver<{}, any, { key: string }> = (_source, { key }, { cache }) => {
+export const getClientConfig = (apolloState: any, key?: string) => {
   key = key ? `${CLIENT_CONFIG_PATH}.${key}` : CLIENT_CONFIG_PATH;
 
-  return apolloStateToObject(cache.data.data, key);
+  return apolloStateToObject(apolloState, key);
 };
+
+export const getClientConfigResolver: IFieldResolver<{}, any, { key: string }> = (_source, { key }, { cache }) =>
+  getClientConfig(cache.data.data, key);
