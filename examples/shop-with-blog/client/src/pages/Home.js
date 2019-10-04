92ae5135d02cf1ae7976cbb17f23e145b9cbc76d
tutok <walas.marcin@gmail.com>
2019-10-04 12:57:16 +0200

    feat(621): rendering errors on Home page

--- examples/shop-with-blog/client/src/pages/Home.js
+++ examples/shop-with-blog/client/src/pages/Home.js
@@ -3,28 +3,36 @@ import { GET_CATEGORY_WITH_PRODUCT_LIST } from '@deity/falcon-shop-data';
 import { useQuery } from '@apollo/react-hooks';
 import { T } from '@deity/falcon-i18n';
 import { H1 } from '@deity/falcon-ui';
-import { PageLayout, ProductList, Loader } from '@deity/falcon-ui-kit';
+import { PageLayout, ProductList, Loader, OperationError } from '@deity/falcon-ui-kit';
 
 const Home = () => {
-  const {
-    loading,
-    data: { category }
-  } = useQuery(GET_CATEGORY_WITH_PRODUCT_LIST, {
-    variables: {
-      categoryId: '25',
-      query: {
-        perPage: 1,
-        page: 20
+  const categoryProducts = () => {
+    const { data, loading, error } = useQuery(GET_CATEGORY_WITH_PRODUCT_LIST, {
+      variables: {
+        categoryId: '25',
+        query: {
+          perPage: 1,
+          page: 20
+        }
       }
+    });
+    if (error) {
+      return <OperationError {...error} />;
     }
-  });
+
+    if (loading) {
+      return <Loader />;
+    }
+
+    return <ProductList items={data.category.productList.items} />;
+  };
 
   return (
     <PageLayout>
       <H1 css={{ textAlign: 'center' }}>
         <T id="home.hotSellers" />
       </H1>
-      {loading ? <Loader /> : <ProductList items={category.productList.items} />}
+      {categoryProducts()}
     </PageLayout>
   );
 };
