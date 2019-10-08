66ccfd859f90ab2f7f8175711c0ca34d70706509
tutok <walas.marcin@gmail.com>
2019-10-08 09:25:32 +0200

    feat(621): fix: product PageLayout should be always rendered

--- examples/shop-with-blog/client/src/pages/shop/Product.js
+++ examples/shop-with-blog/client/src/pages/shop/Product.js
@@ -21,60 +21,64 @@ import {
 import { OpenSidebarMutation, SIDEBAR_TYPE } from 'src/components/Sidebar';
 
 const ProductPage = ({ id, path }) => (
-  <ProductQuery variables={{ id, path }}>
-    {({ data: { product } }) => (
-      <PageLayout>
-        <Breadcrumbs items={product.breadcrumbs} />
-        <OpenSidebarMutation>
-          {openSidebar => (
-            <AddToCartFormProvider
-              quantity={1}
-              product={product}
-              onSuccess={() => openSidebar({ variables: { contentType: SIDEBAR_TYPE.cart } })}
-            >
-              {({ isSubmitting, status = {} }) => (
-                <ProductLayout as={Form} id="add-to-cart" i18nId="product">
-                  <FlexLayout gridArea={ProductLayoutArea.gallery} alignItems="center" justifyContent="center">
-                    <ProductGallery items={product.gallery} />
-                  </FlexLayout>
-                  <Text fontSize="sm" gridArea={ProductLayoutArea.sku}>
-                    <T id="product.sku" sku={product.sku} />
-                  </Text>
-                  <H1 gridArea={ProductLayoutArea.title}>{product.name}</H1>
-                  <Box gridArea={ProductLayoutArea.price}>
-                    <ProductPrice {...product.price} fontSize="xl" />
-                    <ProductTierPrices items={product.tierPrices} />
-                  </Box>
-                  <ProductOptionList
-                    gridArea={ProductLayoutArea.options}
-                    name="options"
-                    items={product.options}
-                    disabled={isSubmitting}
-                  />
-                  <ProductDescription gridArea={ProductLayoutArea.description} html={product.description} />
-                  <FlexLayout alignItems="center" gridArea={ProductLayoutArea.cta} mt="md">
-                    <Field name="qty" required>
-                      {({ field, label, error }) => (
-                        <Box mr="md">
-                          <NumberInput {...field} min={1} disabled={isSubmitting} aria-label={label} />
-                          <FormFieldError>{field.invalid ? error : null}</FormFieldError>
-                        </Box>
-                      )}
-                    </Field>
-                    <Button type="submit" height="xl" disabled={isSubmitting} variant={isSubmitting && 'loader'}>
-                      {!isSubmitting && <Icon src="cart" stroke="white" size="md" mr="sm" />}
-                      <T id="product.addToCart" />
-                    </Button>
-                  </FlexLayout>
-                  <Box gridArea={ProductLayoutArea.error}>{status.error && <ErrorSummary errors={status.error} />}</Box>
-                </ProductLayout>
-              )}
-            </AddToCartFormProvider>
-          )}
-        </OpenSidebarMutation>
-      </PageLayout>
-    )}
-  </ProductQuery>
+  <PageLayout>
+    <ProductQuery variables={{ id, path }}>
+      {({ data: { product } }) => (
+        <React.Fragment>
+          <Breadcrumbs items={product.breadcrumbs} />
+          <OpenSidebarMutation>
+            {openSidebar => (
+              <AddToCartFormProvider
+                quantity={1}
+                product={product}
+                onSuccess={() => openSidebar({ variables: { contentType: SIDEBAR_TYPE.cart } })}
+              >
+                {({ isSubmitting, status = {} }) => (
+                  <ProductLayout as={Form} id="add-to-cart" i18nId="product">
+                    <FlexLayout gridArea={ProductLayoutArea.gallery} alignItems="center" justifyContent="center">
+                      <ProductGallery items={product.gallery} />
+                    </FlexLayout>
+                    <Text fontSize="sm" gridArea={ProductLayoutArea.sku}>
+                      <T id="product.sku" sku={product.sku} />
+                    </Text>
+                    <H1 gridArea={ProductLayoutArea.title}>{product.name}</H1>
+                    <Box gridArea={ProductLayoutArea.price}>
+                      <ProductPrice {...product.price} fontSize="xl" />
+                      <ProductTierPrices items={product.tierPrices} />
+                    </Box>
+                    <ProductOptionList
+                      gridArea={ProductLayoutArea.options}
+                      name="options"
+                      items={product.options}
+                      disabled={isSubmitting}
+                    />
+                    <ProductDescription gridArea={ProductLayoutArea.description} html={product.description} />
+                    <FlexLayout alignItems="center" gridArea={ProductLayoutArea.cta} mt="md">
+                      <Field name="qty" required>
+                        {({ field, label, error }) => (
+                          <Box mr="md">
+                            <NumberInput {...field} min={1} disabled={isSubmitting} aria-label={label} />
+                            <FormFieldError>{field.invalid ? error : null}</FormFieldError>
+                          </Box>
+                        )}
+                      </Field>
+                      <Button type="submit" height="xl" disabled={isSubmitting} variant={isSubmitting && 'loader'}>
+                        {!isSubmitting && <Icon src="cart" stroke="white" size="md" mr="sm" />}
+                        <T id="product.addToCart" />
+                      </Button>
+                    </FlexLayout>
+                    <Box gridArea={ProductLayoutArea.error}>
+                      {status.error && <ErrorSummary errors={status.error} />}
+                    </Box>
+                  </ProductLayout>
+                )}
+              </AddToCartFormProvider>
+            )}
+          </OpenSidebarMutation>
+        </React.Fragment>
+      )}
+    </ProductQuery>
+  </PageLayout>
 );
 ProductPage.propTypes = {
   id: PropTypes.string.isRequired
