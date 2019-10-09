5a356726e5a9c39c10d5bc80aa066917bfec1914
Daniel Ruf <mac1@daniel-ruf.de>
2019-10-09 22:26:08 +0200

    fix: remove unused React imports

--- packages/falcon-ui-kit/src/Order/OrderItemSummaryLayout.tsx
+++ packages/falcon-ui-kit/src/Order/OrderItemSummaryLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { themed, Box } from '@deity/falcon-ui';
 import { toGridTemplate } from '../helpers';
--- packages/falcon-ui-kit/src/Product/ProductListLayout.tsx
+++ packages/falcon-ui-kit/src/Product/ProductListLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { themed, List } from '@deity/falcon-ui';
 
 export const ProductListLayout = themed({
