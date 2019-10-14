8ff55ba41deb6cc37dd30d209a355db23e3356f9
tutok <walas.marcin@gmail.com>
2019-10-14 16:09:17 +0200

    feat(545): fix

--- packages/falcon-ui-kit/src/Locale/LocalePicker.tsx
+++ packages/falcon-ui-kit/src/Locale/LocalePicker.tsx
@@ -17,7 +17,7 @@ export type LocalePickerProps = ThemedComponentProps & {
 };
 
 export const LocalePickerInnerDOM: React.SFC<LocalePickerProps> = ({ items, value, ...rest }) => (
-  <Dropdown {...(rest as any)}>
+  <Dropdown {...rest}>
     <DropdownLabel>{value.name}</DropdownLabel>
     <DropdownMenu variant="above">
       {items.map(x => (
